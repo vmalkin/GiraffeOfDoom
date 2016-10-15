3c23aed644254df61c52128ba4cac26b79e4071e
vmalkin <vmalkin@es.co.nz>
2016-10-16 11:32:24 +1300

    HeatMapper
    Python script to collate a years data and create a heat map of auroral
    activity

---
+++ heatmap/heatmapper.py
@@ -0,0 +1,10 @@
+# create a list of available CSV files
+# sort into order
+
+# for each file in list
+
+  # find max/min H values for each hour
+
+  # append hour difference value to heatmap list
+
+# Normalise the final heatmap list
