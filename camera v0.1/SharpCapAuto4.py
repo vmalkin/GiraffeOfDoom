9c824e48caa2415f4c10110d5e8cc76157ca3fe6
vmalkin <meep_esq@hotmail.com>
2016-10-27 10:17:33 +1300

    Updated camera script
    includes auto/fixed exposure depending on time/month

--- camera v0.1/SharpCapAuto4.py
+++ camera v0.1/SharpCapAuto4.py
@@ -6,91 +6,55 @@ import System.Drawing
 capturedir = "c:\\temp\\"
 
 sundata = ((0,0,0,0),
-(4,5,21,22),
-(4,5,21,22),
-(5,6,21,22),
-(5,6,21,22),
-(5,6,21,22),
-(5,6,20,21),
-(5,6,20,21),
-(6,7,20,21),
-(6,7,20,21),
-(6,7,20,21),
-(6,7,20,21),
-(6,7,19,20),
-(6,7,19,20),
-(6,7,19,20),
-(6,7,18,19),
-(6,7,18,19),
-(6,7,17,18),
-(6,7,17,18),
-(6,7,17,18),
-(6,7,17,18),
-(6,7,17,18),
-(7,8,17,18),
-(7,8,17,18),
-(7,8,17,18),
-(7,8,16,17),
-(7,8,16,17),
-(7,8,17,18),
-(7,8,17,18),
-(7,8,17,18),
-(7,8,17,18),
-(7,8,17,18),
-(7,8,17,18),
-(7,8,17,18),
-(7,8,17,18),
-(6,7,17,18),
-(6,7,18,19),
-(6,7,18,19),
-(6,7,18,19),
-(5,6,18,19),
-(5,6,19,20),
-(5,6,19,20),
-(5,6,20,21),
-(5,6,20,21),
-(5,6,20,21),
-(5,6,20,21),
-(5,6,20,21),
-(5,6,20,21),
-(4,5,21,22),
-(4,5,21,22),
-(4,5,21,22),
-(4,5,21,22),
-(4,5,21,22))
+(5,22),
+(5,22),
+(5,22),
+(5,22),
+(5,22),
+(6,20),
+(6,20),
+(6,20),
+(6,21),
+(5,22),
+(5,22),
+(5,22))
 
 # Choose the ZWO camera. 
 def selectcam():
    pass
 
 SharpCap.SelectedCamera.Controls.OutputFormat.Value = 'PNG files (*.png)'
+capturemode = ""
 
 while True:
    dt = datetime.datetime.now()
-   nowhour = (dt.strftime('%H'))
-   weeknum = (dt.strftime('%W'))
+   nowhour = int(dt.strftime('%H'))
+   monthnum = int(dt.strftime('%m'))
+   tlr = sundata[int(monthnum)][0]   # Morning twilight starts
+   tls = sundata[int(monthnum)][1]   # Evening twilight ends
+   print("Month num: " + str(monthnum) + ". Dawn: " + str(tlr) + ". Dusk: " + str(tls))
 
-   tlr = sundata[int(weeknum)][0]   # Morning twilight starts
-   r = sundata[int(weeknum)][1]
-   s = sundata[int(weeknum)][2]
-   tls = sundata[int(weeknum)][3]   # Evening twilight ends
-
-   if nowhour <= tlr or nowhour >= tls:
+   if nowhour >= tlr and nowhour <= tls:
+      # Daytime
+      capturemode = "Daytime, Autoexposure"
+      print("Daytime: " + str(nowhour))
+      SharpCap.SelectedCamera.Controls.Exposure.Automatic = True
+      print("exposure is " + str(SharpCap.SelectedCamera.Controls.Exposure.Value))
+      
+   else:
       # Nighttime
+      capturemode = "Night-time, Fixed Exposure"
+      print("Night-time " + nowhour)
       SharpCap.SelectedCamera.Controls.Exposure.Automatic = False 
       SharpCap.SelectedCamera.Controls.Exposure.Value = 20  # Set the exposure to 1000ms (1s)  
       print("exposure is " + str(SharpCap.SelectedCamera.Controls.Exposure.Value))
-   else:
-      # Daytime
-      print("Daytime: " + str(nowhour))
-      SharpCap.SelectedCamera.Controls.Exposure.Automatic = True
 
    SharpCap.SelectedCamera.CaptureSingleFrameTo(capturedir + "capture.png")
    time.sleep(1)
    bm = System.Drawing.Bitmap(capturedir + "capture.png")
    g = System.Drawing.Graphics.FromImage(bm)
    f = System.Drawing.Font("Arial", 14)
-   stamp = "http://DunedinAurora.NZ \nSkyCam No 2\n FOV ~70deg, South \n Exposure " +  str(SharpCap.SelectedCamera.Controls.Exposure.Value)) + "\n" + System.DateTime.Now.ToString() + " NZST"
+   stamp = "http://DunedinAurora.NZ \nSkyCam No 2\n FOV ~70deg, South \n" + capturemode + " " +  str(SharpCap.SelectedCamera.Controls.Exposure.Value) + " seconds\n" + System.DateTime.Now.ToString() + " NZST"
    g.DrawString(stamp, f, System.Drawing.Brushes.Red, System.Drawing.Point(0,0))
    g.Dispose()
    f.Dispose()
