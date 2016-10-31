e82f02ee6bda2b35e627645380f59063b63cef0d
vmalkin <meep_esq@hotmail.com>
2016-11-01 07:59:30 +1300

    Data fusion
    added try/excepts to station creation

--- DataFusionProject/main.py
+++ DataFusionProject/main.py
@@ -7,18 +7,42 @@ getcontext().prec = 5
 
 while True:
     # create the magnetometer stations for this run
-    dalmore02 = Station.Station("Dalmore 02", "\\\\VICS-MACBOOK\Datalogger\graphing\Dalmore02.1minbins.csv")
-    dalmore01 = Station.Station("Dalmore 01", "D:\Transfer\My Documents\Magnetometer\pyDataReader\graphing\Dalmore01.1minbins.csv")
+    try:
+        corstorphine01 = Station.Station("Corstorphine 01", "D:\Transfer\My Documents\Magnetometer\DataFusionProject\Corstorphine01.1minbins.csv")
+    except:
+        print("Unable to create station")
+
+    try:
+        dalmore02 = Station.Station("Dalmore 02", "Z:/temp\pyDataReader v1.7/graphing/Dalmore02.1minbins.csv")
+    except:
+        print("Unable to create station")
+
+    try:
+        dalmore01 = Station.Station("Dalmore 01", "D:\Transfer\My Documents\Magnetometer\pyDataReader\graphing\Dalmore01.1minbins.csv")
+    except:
+        print("Unable to create station")
+
     # init the array of stations and append
     stationlist = []
-    stationlist.append(dalmore01)
-    stationlist.append(dalmore02)
+    try:
+        stationlist.append(dalmore01)
+    except:
+        print("Unable to create station")
+
+    try:
+        stationlist.append(dalmore02)
+    except:
+        print("Unable to create station")
+
+    try:
+        stationlist.append(corstorphine01)
+    except:
+        print("Unable to create station")
 
     # create a list of unique timestamps based on all available times from all stations, for this run.
     # make a list of all dates, duplicates and all...
     temp_data = []
     for magstation in stationlist:
-<<<<<<< HEAD
         for i in range(0, len(magstation.output_f)):
             datasplit = magstation.output_f[i].split(",")
             testdate = datasplit[0]
@@ -89,8 +113,18 @@ while True:
     merged_header = "Date/Time UTC, "
     for magstation in stationlist:
         merged_header = merged_header + magstation.station_name + ","
-    # strip off the last comma
-    merged_header = merged_header[:-1]
+
+    merged_header = merged_header + "Averaged Reading"
+
+    # We only want the most recent 1440 entries
+    if len(merged_data) > 1440:
+        chopvalue = len(merged_data) - 1440
+        try:
+            print("Merged array is " + str(len(merged_data)) + " records long: Trimming array")
+            merged_data = merged_data[chopvalue:]
+            print("Merged array is now " + str(len(merged_data)) + " records long\n")
+        except:
+            print("ERROR: Unable to trim array")
     # add to merged_data array
     merged_data.reverse()
     merged_data.append(merged_header)
@@ -98,67 +132,6 @@ while True:
 
     # write out to logfile
     logfile = "D:\Transfer\My Documents\Magnetometer\pyDataReader\publish\merged.csv"
-=======
-        # go thru each mag stations output list, find matching date and append the value to datastring
-        tempstring = datastring
-        for info in magstation.output_f:
-            info = info.split(",")
-            if info[0] == dateitem:
-                datastring = datastring + info[1] + ","
-
-        # we went thru the list of stations and there was no entry for this date/time
-        # CUSTOMISE the N/A string according to your graphing software so that N/A is not plotted
-        if tempstring == datastring:
-            datastring = datastring + "#N/A,"
-
-    merged_data.append(dateitem + "," + datastring)
-
-# for item in merged_data:
-#     print(item)
-
-print("Identified data...")
-
-temp_data= []
-# remove final trailing comma
-for item in merged_data:
-    item = item[:-1]
-    temp_data.append(item)
-merged_data = temp_data
-
-
-# # One final task - re-write the final merged data for values that do NOT have a zero in them.
-# temp_data = []
-# for i in range(0, len(merged_data)):
-#     writeflag = 1
-#     datasplit = merged_data[i].split(",")
-#     for dataitems in datasplit:
-#         if dataitems == str(0):
-#             writeflag = 0 # we cannot write this line...
-#     if writeflag == 1:
-#         temp_data.append(merged_data[i])
-# merged_data = temp_data
-
-
-# insert the headings as the first element of the merged_data[]
-merged_header = "Date/Time UTC, "
-for magstation in stationlist:
-    merged_header = merged_header + magstation.station_name + ","
-# strip off the last comma
-merged_header = merged_header[:-1]
-# add to merged_data array
-merged_data.reverse()
-merged_data.append(merged_header)
-merged_data.reverse()
-
-# write out to logfile
-logfile = "mergedoutput.csv"
-try:
-    os.remove(logfile)
-except OSError:
-    print("WARNING: could not delete " + logfile)
-
-for readings in merged_data:
->>>>>>> origin/master
     try:
         os.remove(logfile)
     except OSError:
