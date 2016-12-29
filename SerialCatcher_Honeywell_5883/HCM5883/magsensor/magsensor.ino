615d71d9643690c59ac1d926da69c2b865345faf
vmalkin <meep_esq@hotmail.com>
2016-12-29 17:09:31 +1300

    revision of blip-detection

--- SerialCatcher_Honeywell_5883/HCM5883/magsensor/magsensor.ino
+++ SerialCatcher_Honeywell_5883/HCM5883/magsensor/magsensor.ino
@@ -40,7 +40,7 @@
 /* Assign a unique ID to this sensor at the same time */
 Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
 
-int AVERAGE_ITERATIONS = 5000; /* Approx 1000 iterations per second */
+float AVERAGE_ITERATIONS = 5000; /* Approx 1000 iterations per second */
 
 
 void displaySensorDetails(void)
@@ -112,6 +112,5 @@ void loop(void)
   magX = 0;
   magY = 0;
   magZ = 0;
-  
-  delay(1);
+
 }
