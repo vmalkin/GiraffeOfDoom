d64988c86014c4c5d06f66a6cceaa90ef9f84d35
vmalkin <meep_esq@hotmail.com>
2016-12-30 15:56:18 +1300

    arduino code to produce blips via magnetometer

---
+++ SerialCatcher_fakedata/fakedata/fakedata.ino
@@ -0,0 +1,22 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("100,100,100");
+  delay(5000);
+
+  Serial.println("100,100,100");
+  delay(5000);
+
+  Serial.println("100,100,100");
+  delay(5000);
+
+  Serial.println("100,100,100");
+  delay(5000);
+
+  Serial.println("1000,1000,1000");
+  delay(5000);
+}
