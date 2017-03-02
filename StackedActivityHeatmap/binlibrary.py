3e9d04a1c28aa20837653368f90f6465b9f84414
vmalkin <vmalkin@es.co.nz>
2017-03-02 14:28:55 +1300

    revised bin library

--- StackedActivityHeatmap/binlibrary.py
+++ StackedActivityHeatmap/binlibrary.py
@@ -1,7 +1,7 @@
-import math
+import constants as k
 from datetime import datetime
 from time import mktime
-import os
+
 
 
 # #################################################################################
@@ -27,7 +27,7 @@ def binsimple(rawdata):
     threshold = 1
 
     # setup the binneddata array timestamps
-    # Most recent time is first
+    # Most recent time is first. Count back from current time in binwidths and append the time to the list
     timestamps = []
     for i in range(0, binnum):
         timestamps.append(currentdt)
@@ -35,11 +35,12 @@ def binsimple(rawdata):
 
     # array for final binned values
     binneddata = []
+
     # Now parse thru the rawdata array
     for i in range(0, len(timestamps) - 1):
         nowtime = timestamps[i]
         prevtime = timestamps[i + 1]
-        counter = float(1)
+        counter = float(0)
         datavalue = float(0.0)
 
         for j in range(0, len(rawdata)):
@@ -58,19 +59,21 @@ def binsimple(rawdata):
 
         # Calculate the average reading for the bin. If there is no data, we need to return an empty bin
         if counter > threshold:
+            print(counter)
             datavalue = datavalue / counter
-        else:
-            datavalue = ""
 
-        # Create the datapoint
-        dp = str(nowtime) + "," + str(datavalue)
+            # Create the datapoint
+            dp = str(nowtime) + "," + str(datavalue)
 
-        # append the datapoint to the binned data array
-        binneddata.append(dp)
+            # append the datapoint to the binned data array
+            binneddata.append(dp)
 
-    # the returned array has the most recent readings at index[0] Invert the
-    # array so it is like a conventional list
+        # If there is no data, then no datapoint will be created.
+
+        # the returned array has the most recent readings at index[0] Invert the
+        # array so it is like a conventional list
     binneddata.reverse()
+
     return binneddata
 
 
@@ -86,9 +89,10 @@ def utc2unix(arraylist):
 
     # convert array data times to unix time
     workingarray = []
-    for item in arraylist:
+    count = 0
+    for i in range(0, len(arraylist)):
         try:
-            itemsplit = item.split(",")
+            itemsplit = arraylist[i].print_values().split(",")
             newdatetime = datetime.strptime(itemsplit[0],dateformat)
             # convert to Unix time (Seconds)
             newdatetime = mktime(newdatetime.timetuple())
@@ -96,7 +100,8 @@ def utc2unix(arraylist):
             datastring = str(newdatetime) + "," + str(itemsplit[1])
             workingarray.append(datastring)
         except:
-            print("Problem with this entry: " + str(itemsplit))
+            count = count + 1
+            print("Problem with entry " + count)
 
     return workingarray
 
@@ -122,3 +127,19 @@ def unix2utc(dataarray):
 
     return temparray
 
+# #################################################################################
+# Save the raw datapoint array to the save file
+# #################################################################################
+def SaveRawArray(readings):
+    # export array to array-save file
+    try:
+        with open(k.FILE_BINNED_MINS, 'w') as w:
+            for dataObjects in readings:
+                w.write(dataObjects + '\n')
+    except IOError:
+        print("WARNING: There was a problem accessing " + k.FILE_BINNED_MINS)
+
+# typically we would have
+# array = utc2unix(array)
+# array = binsimple(array)
+# array = unix2utc(array)
