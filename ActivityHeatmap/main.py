2507eb507e6800168a7857a3deee57545fb55b72
vmalkin <vmalkin@es.co.nz>
2017-03-05 13:00:47 +1300

    refactored  heatmapper

--- ActivityHeatmap/main.py
+++ ActivityHeatmap/main.py
@@ -5,40 +5,28 @@ import os
 import time
 import binlibrary as binner
 
-# append output to a file.
-def createoutputfile(texttowrite, filename):
-    try:
-        with open(filename, 'a') as f:
-            f.write(texttowrite + "\n")
-    except IOError:
-        print("WARNING: There was a problem accessing " + k.OUTPUT_FILE)
-
-
-
 # ###############################################
 # Main Parts STarts Here
 # ###############################################
 
 while True:
-    try:
-        importarray = []
-        # Load up file into array
-        # print(k.FILE_BINNED_MINS)
-        importarray = am.CreateRawArray(k.FILE_BINNED_MINS)
+    importarray = []
+    # Load up file into array
+    # print(k.FILE_BINNED_MINS)
+    importarray = am.CreateRawArray(k.FILE_BINNED_MINS)
 
-        # remove the first line which may contain text header
-        importarray.pop(0)
+    # remove the first line which may contain text header
+    importarray.pop(0)
 
-        # create the bins for dh/dt
-        importarray = binner.utc2unix(importarray)
-        importarray = binner.bin_dh_dt(importarray)
-        importarray = binner.unix2utc(importarray)
+    # create the bins for dh/dt
+    importarray = binner.utc2unix(importarray)
+    importarray = binner.bin_dh_dt(importarray)
 
+    binner.SaveRawArray(importarray)
+    print("Binning complete.")
 
+    importarray.sort()
 
-
-
-    except:
-        print("The Binning Program has failed for some general reason")
+    print(importarray[int(len(importarray) / 2)])
 
     time.sleep(600)
