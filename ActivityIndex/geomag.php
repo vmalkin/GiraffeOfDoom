2e76a17470437ed5f30aee18a2421bfdd5865dab
vmalkin <vmalkin@es.co.nz>
2017-03-22 13:00:58 +1300

    dgdfgdfg

--- ActivityIndex/geomag.php
+++ ActivityIndex/geomag.php
@@ -1,75 +1,75 @@
 <table style=" width: 95%; font-size: 0.6em;">
 <tr>
-<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #b9e5ba">
-23 hr<b><br>0.30</b>
+<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #bee7bf">
+23 hr<b><br>0.28</b>
 </td>
-<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #d4efd5">
-22 hr<b><br>0.18</b>
+<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #c0e8c1">
+22 hr<b><br>0.27</b>
 </td>
-<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #c2e8c3">
-21 hr<b><br>0.26</b>
+<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #ffa666">
+21 hr<b><br>0.64</b>
 </td>
-<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #cceccd">
-20 hr<b><br>0.21</b>
+<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #ffb57f">
+20 hr<b><br>0.53</b>
 </td>
-<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #ffcda8">
-19 hr<b><br>0.36</b>
+<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #ffb884">
+19 hr<b><br>0.51</b>
 </td>
-<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #afe1b0">
-18 hr<b><br>0.34</b>
+<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #ffb47e">
+18 hr<b><br>0.54</b>
 </td>
-<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #ffb075">
-17 hr<b><br>0.57</b>
+<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #ffa05a">
+17 hr<b><br>0.69</b>
 </td>
-<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #fa0000">
-16 hr<b><br>0.99</b>
+<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #ffba87">
+16 hr<b><br>0.50</b>
 </td>
-<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #fb4b4b">
-15 hr<b><br>0.70</b>
+<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #b8e5ba">
+15 hr<b><br>0.30</b>
 </td>
-<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #ffac6f">
-14 hr<b><br>0.60</b>
+<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #afe2b0">
+14 hr<b><br>0.34</b>
 </td>
-<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #ffa562">
-13 hr<b><br>0.65</b>
+<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #bde7be">
+13 hr<b><br>0.28</b>
 </td>
-<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #fa1f1f">
-12 hr<b><br>0.87</b>
+<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #d1eed2">
+12 hr<b><br>0.19</b>
 </td>
-<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #ffcaa3">
+<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #ffcaa4">
 11 hr<b><br>0.38</b>
 </td>
-<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #ffc9a2">
-10 hr<b><br>0.38</b>
-</td>
-<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #aee1b0">
-9 hr<b><br>0.34</b>
-</td>
-<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #c9ebca">
-8 hr<b><br>0.23</b>
-</td>
-<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #bce6bd">
-7 hr<b><br>0.28</b>
-</td>
-<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #ffbe8e">
-6 hr<b><br>0.47</b>
-</td>
-<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #fb3939">
-5 hr<b><br>0.77</b>
-</td>
-<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #ffb177">
-4 hr<b><br>0.56</b>
-</td>
-<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #fb4444">
-3 hr<b><br>0.73</b>
+<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #ffceaa">
+10 hr<b><br>0.35</b>
 </td>
 <td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #ffaa6b">
-2 hr<b><br>0.61</b>
+9 hr<b><br>0.61</b>
 </td>
-<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #ffa35f">
-now<b><br>0.66</b>
+<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #fa0000">
+8 hr<b><br>1.0</b>
+</td>
+<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #ffb885">
+7 hr<b><br>0.51</b>
+</td>
+<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #ffba87">
+6 hr<b><br>0.50</b>
+</td>
+<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #ffcda9">
+5 hr<b><br>0.35</b>
+</td>
+<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #ffc296">
+4 hr<b><br>0.43</b>
+</td>
+<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #cdedce">
+3 hr<b><br>0.21</b>
+</td>
+<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #b2e3b3">
+2 hr<b><br>0.33</b>
+</td>
+<td style="border-radius: 3px; text-align: center; padding: 2px; background-color: #ffc396">
+now<b><br>0.43</b>
 </td>
 <td style="border-radius: 3px; text-align: center; padding: 2px;"><h4>Dalmore <br> No 1</h4></td>
 </tr>
 </table>
-<div style="font-size: 0.7em;">Last updated at March 21 2017 - 19:23 UTC.   <i> <br>Best min: March 21 2017 - 19:18 UTC. <br>Best max: March 21 2017 - 19:18 UTC. </i>  <div>
+<div style="font-size: 0.7em;">Last updated at March 21 2017 - 23:55 UTC.   <i> <br>Best min: March 21 2017 - 19:18 UTC. <br>Best max: March 21 2017 - 23:55 UTC. </i>  <div>
