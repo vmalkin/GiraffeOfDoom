6a83558c592ee5cbbba61e7e3f56a4c2fabb3d6a
vmalkin <meep_esq@hotmail.com>
2018-03-17 13:49:14 +1300

    latest image

--- Coronal Hole Suite/parse_solar_image.py
+++ Coronal Hole Suite/parse_solar_image.py
@@ -10,14 +10,22 @@ img = cv2.imread(imgfolder + 'latest_512_0193.jpg')
 mask = cv2.imread(imgfolder + 'mask_black.bmp', 0)
 #mask_red = cv2.imread(imgfolder + 'mask.bmp',0)
 
-maskedsun = cv2.bitwise_and(img,img,mask = mask)
-
 # converting an Image to grey scale...
-greyimg = cv2.cvtColor(maskedsun, cv2.COLOR_BGR2GRAY)
+greyimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Identify dark coronal hole areas from the solar surface...
 # This is crude at the moment, but it basically works
-ret, outputimg = cv2.threshold(greyimg, 30,255, cv2.THRESH_BINARY)
+ret, outputimg = cv2.threshold(greyimg, 25,255, cv2.THRESH_BINARY)
+
+# Erode and Dilate the image to clear up noise
+# Erosion will trim away pixels (noise)
+# dilation puffs out edges
+kernel = np.ones((5,5),np.uint8)
+outputimg = cv2.erode(outputimg,kernel,iterations = 2)
+outputimg = cv2.dilate(outputimg,kernel,iterations = 1)
+
+# Mask off the blowout due to the corona
+outputimg = cv2.bitwise_and(outputimg,outputimg,mask = mask)
 
 # Save adjusted image
 cv2.imwrite(imgfolder + 'saved.bmp', outputimg)
