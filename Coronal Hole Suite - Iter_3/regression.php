8ebd2e019893990c8f96391cb380925666d44767
Vaughn Malkin <vmalkin@es.co.nz>
2018-04-17 09:23:22 +1200

    testing forecast

--- Coronal Hole Suite - Iter_3/regression.php
+++ Coronal Hole Suite - Iter_3/regression.php
@@ -1,1 +1,1 @@
-<p style = "color: orange;">Pearsons correlation = 0.4116<br style = "color: orange;">Average Windspeed is 418.54km/s<br style = "color: orange;">Coronal Hole effects will be felt in 4.1 days
+<p>Pearsons correlation: |r| = 0.4116<br>Average Windspeed is 418.54km/s<br>Coronal Hole effects will be felt in 4.1 days
