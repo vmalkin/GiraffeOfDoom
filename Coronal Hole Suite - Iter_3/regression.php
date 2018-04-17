5ce93cedf222f3b7f92a73aad28d50e4a2fb12c1
vmalkin <meep_esq@hotmail.com>
2018-04-17 21:25:19 +1200

    still grinding thru minor bugs...
    why did i start...

--- Coronal Hole Suite - Iter_3/regression.php
+++ Coronal Hole Suite - Iter_3/regression.php
@@ -1,4 +1,4 @@
-<p style = "color: orange;">Linear approximation is: Predicted windspeed = 351.78 + 783.74 * coronal hole area on meridian     <br>Pearsons correlation = 0.3966
-<br>Average Windspeed is 396.37km/s
-<br>Coronal Hole effects will be felt in 4.3 days
+<p style = "color: orange;">Linear approximation is: Predicted windspeed = 361.93 + 714.29 * coronal hole area on meridian     <br>Pearsons correlation = 0.4172
+<br>Average Windspeed is 402.72km/s
+<br>Coronal Hole effects will be felt in 4.2 days
 
