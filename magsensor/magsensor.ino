8ff1e66d874ff10962184501e3117586d0fb9453
vmalkin <meep_esq@hotmail.com>
2018-06-14 18:21:19 +1200

    updates to Arduino code - added Watchdog Timer

---
+++ magsensor/magsensor.ino
@@ -0,0 +1,119 @@
+#include <Adafruit_Sensor.h>
+#include <Adafruit_HMC5883_U.h>
+#include <avr/wdt.h>
+/***************************************************************************
+  This is a library example for the HMC5883 magnentometer/compass
+
+  Designed specifically to work with the Adafruit HMC5883 Breakout
+  http://www.adafruit.com/products/1746
+ 
+  *** You will also need to install the Adafruit_Sensor library! ***
+
+  These displays use I2C to communicate, 2 pins are required to interface.
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit andopen-source hardware by purchasing products
+  from Adafruit!
+
+  Written by Kevin Townsend for Adafruit Industries with some heading example from
+  Love Electronics (loveelectronics.co.uk)
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the version 3 GNU General Public License as
+ published by the Free Software Foundation.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ ***************************************************************************/
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_HMC5883_U.h>
+
+/* Assign a unique ID to this sensor at the same time */
+Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
+
+double AVERAGE_ITERATIONS = 2000; /* Approx 1000 iterations per second */
+
+
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  mag.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" uT");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" uT");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" uT");  
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
+
+void setup(void) 
+{
+  // Enable the watchdog timer
+  wdt_enable(WDTO_8S);
+  Serial.begin(9600);
+  Serial.println("Ruru Test Station");
+  
+  /* Initialise the sensor */
+  if(!mag.begin())
+  {
+    /* There was a problem detecting the HMC5883 ... check your connections */
+    Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
+    while(1);
+  }
+  
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
+}
+
+void loop(void) 
+{
+  /* Get a new sensor event */ 
+  // sensors_event_t event; 
+  // mag.getEvent(&event);
+  double magX = 0;
+  double magY = 0; 
+  double magZ = 0;
+  
+  /* Display the results (magnetic vector values are in micro-Tesla (uT)) */
+  //Serial.print("X: "); Serial.print(event.magnetic.x); Serial.print("  ");
+  //Serial.print("Y: "); Serial.print(event.magnetic.y); Serial.print("  ");
+  //Serial.print("Z: "); Serial.print(event.magnetic.z); Serial.print("  ");Serial.println("uT");
+  
+  for (int i=0; i < AVERAGE_ITERATIONS; i++)
+  {
+    sensors_event_t event; 
+    mag.getEvent(&event);
+    magX = magX + (double)event.magnetic.x;
+    magY = magY + (double)event.magnetic.y;
+    magZ = magZ + (double)event.magnetic.z;
+  }
+  
+  magX = magX/AVERAGE_ITERATIONS;  
+  magY = magY/AVERAGE_ITERATIONS; 
+  magZ = magZ/AVERAGE_ITERATIONS; 
+
+  Serial.println(magX,3);
+//  Serial.print(",");
+//  Serial.print(magY,3);
+//  Serial.print(",");
+//  Serial.println(magZ,3);
+  
+  magX = 0;
+  magY = 0;
+  magZ = 0;
+
+  // Pat the watchdog - good boy!
+  wdt_reset();
+}
