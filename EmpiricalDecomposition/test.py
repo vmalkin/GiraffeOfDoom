5b7a24d95d035498879505d9c40e3e58b914b439
Vaughn Malkin <vmalkin@es.co.nz>
2018-07-13 15:18:33 +1200

    EMD - Created functin that identifies Min and Max values to create the envelope

--- EmpiricalDecomposition/test.py
+++ EmpiricalDecomposition/test.py
@@ -13,6 +13,10 @@ class DataPoint:
         self.datavalue = datavalue
         self.maxvalue = self.null
         self.minvalue = self.null
+
+    def print_data(self):
+        datastring = str(self.posixdate) + "," + str(self.minvalue) + "," + str(self.datavalue) + "," + str(self.maxvalue)
+        return datastring
         
 # Function to load CSV. Returns a list    
 def loadcsv(csvfilename):
@@ -25,6 +29,17 @@ def loadcsv(csvfilename):
             line = line.strip()
             returnlist.append(line)
     return returnlist
+
+# Function to load CSV. Returns a list
+def savecsv(csvfilename, object_list):
+    """
+    Save a CSV file.
+    """
+    returnlist = []
+    with open(csvfilename, "w") as f:
+        for item in object_list:
+            datastring = str(item.print_data()) + "\n"
+            f.write(datastring)
 
 # refine the list to the format [posix_date, datavalue]
 def refine_magnetometer_data(datalist):
@@ -44,9 +59,45 @@ def refine_magnetometer_data(datalist):
         returnlist.append(dp)
     return returnlist
 
+def list_to_object(datalist):
+    returnvalue = []
+    for item in datalist:
+        datasplit = item.split(",")
+        posixdate = datasplit[0]
+        datavalue = datasplit[1]
+
+        dp = DataPoint(posixdate, datavalue)
+        returnvalue.append(dp)
+    return returnvalue
+
+
+def data_minmax(object_list):
+    for i in range(1, len(object_list) - 1):
+        prev = object_list[i-1].datavalue
+        now = object_list[i].datavalue
+        next = object_list[i + 1].datavalue
+
+        if now > prev and now > next:
+            object_list[i].maxvalue = now
+
+        if now < prev and now < next:
+            object_list[i].minvalue = now
+
+
 if __name__ == "__main__":
+    # load the data
     magnetometer_data = loadcsv("2018-06-18.csv")
-    print(magnetometer_data[0])
+
+    # refine data down to [posixdate, datavalue]
     magnetometer_data = refine_magnetometer_data(magnetometer_data)
-    print(magnetometer_data[0])
+
+    # Convert datalist to object list
+    magnetometer_data = list_to_object(magnetometer_data)
+
+    # Identify the high and low points
+    data_minmax(magnetometer_data)
+
+    # save out the CSV for diagnostic purposes
+    savecsv("savefile.csv", magnetometer_data)
+
 
