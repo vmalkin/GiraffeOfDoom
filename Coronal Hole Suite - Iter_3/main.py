761d67479c720f06af093500eb51f4c7e73f3f17
vmalkin <vaughnmalkin@gmail.com>
2018-08-25 11:08:25 +1200

    change the looping to happen via BASH script rather than internally

--- Coronal Hole Suite - Iter_3/main.py
+++ Coronal Hole Suite - Iter_3/main.py
@@ -18,46 +18,46 @@ forecaster = mgr_forecast.Forecaster()
 common_data.report_string = ""
 
 if __name__ == "__main__":
-    while True:
-        # reset the resport string
-        common_data.report_string = ""
+#    while True:
+    # reset the resport string
+    common_data.report_string = ""
 
-        # get the wind data and coronal hole coverage. In cases of no information, the the
-        # returned values will be ZERO!
-        # Get the satellite data
-        discovr.get_data()
+    # get the wind data and coronal hole coverage. In cases of no information, the the
+    # returned values will be ZERO!
+    # Get the satellite data
+    discovr.get_data()
 
-        # process latest solar image
-        sun.get_meridian_coverage()
+    # process latest solar image
+    sun.get_meridian_coverage()
 
-        # get current posix time and create the datapoint to append the the main data
-        posixtime = int(time.time())   # sun.coverage  discovr.wind_speed  discovr.wind_density
-        dp = mgr_data.DataPoint(posixtime, sun.coverage, discovr.wind_speed, discovr.wind_density)
-        print(dp.return_values())
+    # get current posix time and create the datapoint to append the the main data
+    posixtime = int(time.time())   # sun.coverage  discovr.wind_speed  discovr.wind_density
+    dp = mgr_data.DataPoint(posixtime, sun.coverage, discovr.wind_speed, discovr.wind_density)
+    print(dp.return_values())
 
-        # append the new datapoint and process the master datalist
-        data_manager.append_datapoint(dp)
-        data_manager.process_new_data()
+    # append the new datapoint and process the master datalist
+    data_manager.append_datapoint(dp)
+    data_manager.process_new_data()
 
-        # Calculate if enough time has elapsed to start running the forecasting.
-        startdate = int(data_manager.master_data[0].posix_date)
-        nowdate = int(data_manager.master_data[len(data_manager.master_data) - 1].posix_date)
-        elapsedtime = nowdate - startdate
-        timeleft = (WAITPERIOD - elapsedtime) / (60 * 60 * 24)
+    # Calculate if enough time has elapsed to start running the forecasting.
+    startdate = int(data_manager.master_data[0].posix_date)
+    nowdate = int(data_manager.master_data[len(data_manager.master_data) - 1].posix_date)
+    elapsedtime = nowdate - startdate
+    timeleft = (WAITPERIOD - elapsedtime) / (60 * 60 * 24)
 
-        if elapsedtime >= WAITPERIOD:
-            # create the forecast
-            forecaster.calculate_forecast(data_manager.master_data)
+    if elapsedtime >= WAITPERIOD:
+        # create the forecast
+        forecaster.calculate_forecast(data_manager.master_data)
 
-            # Instantiate the prediction plotter, this will load it with the lates values. Plot the final data
-            prediction_plotter = mgr_plotter.Plotter()
-            prediction_plotter.plot_data()
-        else:
-            common_data.report_string = common_data.report_string + ("<br>Insufficient time has passed to begin forecasting. " + str(timeleft)[:5] + " days remaining" + "\n")
-            print(common_data.report_string)
-            with open("regression.php", 'w') as w:
-                w.write(common_data.report_string + '\n')
+        # Instantiate the prediction plotter, this will load it with the lates values. Plot the final data
+        prediction_plotter = mgr_plotter.Plotter()
+        prediction_plotter.plot_data()
+    else:
+        common_data.report_string = common_data.report_string + ("<br>Insufficient time has passed to begin forecasting. " + str(timeleft)[:5] + " days remaining" + "\n")
+        print(common_data.report_string)
+        with open("regression.php", 'w') as w:
+            w.write(common_data.report_string + '\n')
 
         # Pause for an hour
-        time.sleep(3600)
+#        time.sleep(3600)
 
