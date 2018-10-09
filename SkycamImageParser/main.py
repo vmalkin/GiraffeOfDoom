75d82f0b2fa89c7e36613583e42a7e968172179d
vmalkin <meep_esq@hotmail.com>
2018-10-09 23:00:55 +1300

    skycam image parser

--- SkycamImageParser/main.py
+++ SkycamImageParser/main.py
@@ -14,14 +14,24 @@ class PictureToProcess:
     def _return_pixelvalue(self, img):
         img_height = img.shape[0]
         img_width = img.shape[1]
-        pxl = img[int(img_height / 2),int(img_width / 2)]
-        # array in format [blue, green, red]. closer to zero, closer to black
-        return pxl
+        # bl = str(img[int(img_height / 2),int(img_width / 2), 0])
+        # gr = str(img[int(img_height / 2), int(img_width / 2), 1])
+        # rd = str(img[int(img_height / 2), int(img_width / 2), 2])
+        # # array in format [blue, green, red]. closer to zero, closer to black
+        # returnstring = rd + "," + gr + "," + bl
+        # return returnstring
+
+        pxl_array = str(img[int(img_height / 2),int(img_width / 2)])
+        return pxl_array
+
+
+
 
 if __name__ == "__main__":
     filelist = ["00-20-49-66utc.png",
                 "01-06-54-31utc.png",
                 "07-38-27-44utc.png",
+                "10-57-31-98utc.png",
                 "10-57-31-98utc.png",
                 "11-04-31-59utc.png",
                 "11-33-30-38utc.png",
@@ -38,4 +48,6 @@ if __name__ == "__main__":
 
     for pic in picturelist:
         print(pic.pixelvalues)
+
+
 
