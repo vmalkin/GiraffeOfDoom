27d129f17100506d527ddcdf119f410bd5c13197
vmalkin <meep_esq@hotmail.com>
2019-01-01 23:06:37 +1300

    added averaging and deblip for magnetic noise values

--- FrankenCoilParser/main.py
+++ FrankenCoilParser/main.py
@@ -1,11 +1,12 @@
 import datetime
 import os
 import shutil
-spectrumlab_data = "z://temp//test.csv"
+spectrumlab_data = "c://temp//test.csv"
 frankencoil_data = "working.csv"
 running_avg_window = 20
 null_value = ""
 reading_threshold = -115
+noise_threshold = -108.1
 
 # raw data should have the format "utcdate, avg noise, avg reading"
 
@@ -16,16 +17,18 @@ class Datapoint:
         self.noise = avg_noise
         self.reading = avg_reading
         self.reading_db = null_value
-        self.running_average = null_value
+        self.noise_db = null_value
+        self.average_reading = null_value
+        self.average_noise = null_value
         
     def print_labels(self):
-        labelstring = "UTC Date/Time,Magnetic noise,Reading (dB),Average Amplitude"
+        labelstring = "UTC Date/Time,Magnetic noise,Reading (dB),Average Magnetic Amplitude,Average Magnetic Noise"
         # labelstring = "UTC Date/Time,Magnetic noise,Reading (dB),deblipped, Average Amplitude"
         return labelstring
         
     def print_values(self):
-        returnstring = str(self.datetime) + "," + str(self.noise) + "," + str(self.reading) + "," + str(self.running_average)
-        # returnstring = str(self.datetime) + "," + str(self.noise) + "," + str(self.reading) + "," + str(self.reading_db) + "," + str(self.running_average)
+        returnstring = str(self.datetime) + "," + str(self.noise) + "," + str(self.reading) + "," + str(self.average_reading) + "," + str(self.average_noise)
+        # returnstring = str(self.datetime) + "," + str(self.noise) + "," + str(self.reading) + "," + str(self.reading_db) + "," + str(self.average_reading)
         return returnstring
 
 
@@ -34,9 +37,13 @@ def deblip_readings(object_list):
         if float(object_list[i].reading) < float(reading_threshold):
             object_list[i].reading_db = object_list[i].reading
 
+def deblip_noise(object_list):
+    for i in range(0, len(object_list)):
+        if float(object_list[i].noise) < float(noise_threshold):
+            object_list[i].noise_db = object_list[i].noise
 
 # uses deblipped readings
-def running_average(object_list):
+def average_reading(object_list):
     half_window = int(round((running_avg_window / 2), 0))
     
     for i in range(half_window, len(object_list) - half_window):
@@ -48,9 +55,22 @@ def running_average(object_list):
                 divider = divider + 1
 
         avg_value = round((avg_value / divider), 4)
-        object_list[i].running_average = avg_value
+        object_list[i].average_reading = avg_value
 
 
+def average_noise(object_list):
+    half_window = int(round((running_avg_window / 2), 0))
+
+    for i in range(half_window, len(object_list) - half_window):
+        avg_value = float(0)
+        divider = 0
+        for j in range(half_window * -1, half_window):
+            if object_list[i + j].noise_db != null_value:
+                avg_value = avg_value + float(object_list[i + j].noise_db)
+                divider = divider + 1
+
+        avg_value = round((avg_value / divider), 4)
+        object_list[i].average_noise = avg_value
 
 if __name__ == "__main__":
     if os.path.isfile(spectrumlab_data):
@@ -72,8 +92,10 @@ if __name__ == "__main__":
         
         # perform the running average and update the datapoint property
         deblip_readings(storage_array)
+        deblip_noise(storage_array)
         if len(storage_array) > running_avg_window:
-            running_average(storage_array)
+            average_reading(storage_array)
+            average_noise(storage_array)
         
         # create the daily CSV logfile
         with open(frankenCoil_current_datafile, "w") as f:
