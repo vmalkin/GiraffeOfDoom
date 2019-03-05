0149ec1a2106df886c1c16a8c4106bc20192cbbc
vmalkin <meep_esq@hotmail.com>
2019-03-05 20:17:47 +1300

    revision of DNA Core.
    fleshing out instrument methods

--- DnACore/instrument_manager.py
+++ DnACore/instrument_manager.py
@@ -10,17 +10,21 @@ CRITICAL
 """
 from instruments import MagnetometerLocalCSV, MagnetometerURL
 import time
+import constants as k
 import logging
+
+errorloglevel = logging.DEBUG
+logging.basicConfig(filename=k.errorfile, format='%(asctime)s %(message)s', level=errorloglevel)
+logging.info("Created error log for this session")
 
 UPDATE_DELAY = 300
 
-errorloglevel = logging.DEBUG
-logging.basicConfig(filename="errors.log", format='%(asctime)s %(message)s', level=errorloglevel)
-logging.info("Created error log for this session")
-
-rapid_run = MagnetometerLocalCSV("Ruru Rapidrun", "Vaughn", "Dunedin", "NULL", "C://test.csv")
+# Set up the list of sensors here
+logging.debug("Setting up magnetometer stations")
+rapid_run = MagnetometerLocalCSV("Ruru_Rapidrun", "Vaughn", "Dunedin", "NULL", "C://test.csv")
 goes = MagnetometerURL("GOES", "Vaughn", "Dunedin", "NULL", "http://www.test.com")
 
+logging.debug("appending to list")
 instrument_list = []
 instrument_list.append(rapid_run)
 instrument_list.append(goes)
@@ -28,14 +32,9 @@ instrument_list.append(goes)
 if __name__ == "__main__":
     while True:
         for instrument in instrument_list:
-            instrument.get_data()
-            instrument.append_new_data()
-            instrument.prune_current_data()
-            instrument.save_logfile()
-            # instrument.calculate_dynamic_parameters()
-            # instrument.save_dynamic_parameters()
+            instrument.process_data()
 
-        print("Update completed...")
+        print("\nUpdate completed...")
         for i in range(0, UPDATE_DELAY):
             print(str(UPDATE_DELAY - i) + " seconds until next pass")
             time.sleep(1)
--- DnACore/instruments.py
+++ DnACore/instruments.py
@@ -3,6 +3,23 @@ Instrumentation. This file contains  parent/child objects to deal with the getti
 in multiple ways. Instrument is the abstract class. Each child class is contains the unique methods for getting
 data from each different kind of device eg: local file, http, COM port, etc.
 """
+import constants as k
+import logging
+import os
+import datetime,time
+
+errorloglevel = logging.DEBUG
+logging.basicConfig(filename=k.errorfile, format='%(asctime)s %(message)s', level=errorloglevel)
+logging.info("Created error log for this session")
+
+class Datapoint:
+    def __init__(self, posix_time, data):
+        self.posix_time = posix_time
+        self.data = data
+
+    def print_values(self):
+        returnstring = self.posix_time + "," + self.data
+        return returnstring
 
 
 class Instrument:
@@ -14,7 +31,50 @@ class Instrument:
         self.date_regex_string = date_regex_string
         self.datasource = datasource
 
+        self.logfile_dir = self.name + "_logfiles"
+        self.running_array_savefile = self.name + ".csv"
+
         self.load_dynamic_parameters()
+        self.runningarray = self.running_array_load(self.running_array_savefile)
+        self.setup_paths()
+
+    def setup_paths(self):
+        # setup file paths
+        # Set up file structure for Data logs. Linux systems might need use of the mode arg to set correct permissions.
+        if not os.path.exists(self.logfile_dir):
+            try:
+                os.makedirs(self.logfile_dir)
+                print("Logfile directory created.")
+            except:
+                if not os.path.isdir(self.logfile_dir):
+                    print("Unable to create log directory")
+                    logging.critical("CRITICAL ERROR: Unable to create logs directory")
+
+    def running_array_load(self, savefile):
+        returnarray = []
+        if os.path.exists(savefile):
+            with open(savefile, "r") as s:
+                for line in s:
+                    line = line.strip()
+                    line = line.split(",")
+                    dp = Datapoint(line[0], line[1])
+                    returnarray.append(dp)
+                logging.debug("Running array loaded for " + self.name)
+        return returnarray
+
+    def running_array_save(self, runningarray):
+        with open(self.running_array_savefile, "w") as w:
+            for data_point in runningarray:
+                w.write(data_point.print_values() + "\n")
+
+    def utc_2_posic(self, utctime, dateformat, regex):
+        # set date time format for strptime()
+        # dateformat = "%Y-%m-%d %H:%M:%S.%f"
+        # dateformat = '"%Y-%m-%d %H:%M:%S"'
+        # dateformat = '%Y-%m-%d %H:%M'
+        posixtime = datetime.strptime(utctime, dateformat)
+        posixtime = time.mktime(posixtime.timetuple())
+        return str(int(posixtime))
 
     def load_dynamic_parameters(self):
         """An instrument can load it's unique parameters"""
@@ -30,7 +90,7 @@ class Instrument:
 
     def get_data(self):
         """load current data"""
-        raise NotImplementedError
+        pass
 
     def append_new_data(self):
         """Aggregate to internal list"""
@@ -44,15 +104,18 @@ class Instrument:
         """Save the current data to CSV logfile"""
         pass
 
+    def process_data(self):
+        """Wrapper function to process this instruments data gathering and parameter updating"""
+        raise NotImplementedError
+
 
 class MagnetometerLocalCSV(Instrument):
     """Child class of Instrument"""
     def __init__(self, name, location, owner, date_regex_string, datasource):
         Instrument.__init__(self, name, location, owner, date_regex_string, datasource)
 
-    def get_data(self):
-        returnvalue = "Data from local CSV file"
-        return returnvalue
+    def process_data(self):
+        print("Processing local magnetometer")
 
 
 class MagnetometerGOES(Instrument):
@@ -60,6 +123,14 @@ class MagnetometerGOES(Instrument):
     def __init__(self, name, location, owner, date_regex_string, datasource):
         Instrument.__init__(self, name, location, owner, date_regex_string, datasource)
 
-    def get_data(self):
-        returnvalue = "Data from local the Internet"
-        return returnvalue
+    def process_data(self):
+        print("Processing GOES satellite magnetometer")
+
+
+class MagnetometerURL(Instrument):
+    """Child class of Instrument - data from URL"""
+    def __init__(self, name, location, owner, date_regex_string, datasource):
+        Instrument.__init__(self, name, location, owner, date_regex_string, datasource)
+
+    def process_data(self):
+        print("Processing URL data")
