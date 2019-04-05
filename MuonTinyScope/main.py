5855f9f17e3a77fd5c8718c435bb780430afe8e5
Vaughn Malkin <vmalkin@es.co.nz>
2019-04-05 13:18:13 +1300

    muon tinyscope - camera connection and image capture

--- MuonTinyScope/main.py
+++ MuonTinyScope/main.py
@@ -1,17 +1,22 @@
 import cv2
 import numpy
+import time
+
+cap = cv2.VideoCapture(0)
 
 
-if __name__ == ("__main__"):
-    # capture from camera at location 0
-    cap = cv2.VideoCapture(0)
+def setup_cam():
     # Change the camera setting using the set() function
-    cap.set(cv2.CAP_PROP_EXPOSURE, -6.0)
     cap.set(cv2.CAP_PROP_GAIN, 4.0)
     cap.set(cv2.CAP_PROP_BRIGHTNESS, 144.0)
     cap.set(cv2.CAP_PROP_CONTRAST, 27.0)
     cap.set(cv2.CAP_PROP_HUE, 13.0)  # 13.0
     cap.set(cv2.CAP_PROP_SATURATION, 28.0)
+    cap.set(cv2.CAP_PROP_EXPOSURE, 0.99)
+
+if __name__ == ("__main__"):
+    setup_cam()
+
     # Read the current setting from the camera
     test = cap.get(cv2.CAP_PROP_POS_MSEC)
     ratio = cap.get(cv2.CAP_PROP_POS_AVI_RATIO)
@@ -23,7 +28,8 @@ if __name__ == ("__main__"):
     saturation = cap.get(cv2.CAP_PROP_SATURATION)
     hue = cap.get(cv2.CAP_PROP_HUE)
     gain = cap.get(cv2.CAP_PROP_GAIN)
-    # exposure = cap.get(cv2.CAP_PROP_EXPOSURE)
+    exposure = cap.get(cv2.CAP_PROP_EXPOSURE)
+
     print("Test: ", test)
     print("Ratio: ", ratio)
     print("Frame Rate: ", frame_rate)
@@ -34,3 +40,9 @@ if __name__ == ("__main__"):
     print("Saturation: ", saturation)
     print("Hue: ", hue)
     print("Gain: ", gain)
+    print("Exposure: ", exposure)
+
+    ret, img = cap.read()
+    # cv2.imshow("Image", img)
+    cv2.imwrite("detect.png", img)
+    time.sleep(5)
