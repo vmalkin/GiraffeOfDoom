95c877269233d9d682ba21a61471b380a5f53f87
vmalkin <meep_esq@hotmail.com>
2019-04-07 19:06:45 +1200

    muon tinyscope experiments

--- MuonTinyScope/main.py
+++ MuonTinyScope/main.py
@@ -1,26 +1,26 @@
 import cv2
+import math
 import numpy
 
 
 cap = cv2.VideoCapture(0)
 
+
 def setup_cam():
     # Change the camera setting using the set() function
     print(cv2.CAP_PROP_XI_DEVICE_SN)
-    cap.set(cv2.CAP_PROP_GAIN, 4.0)
-    cap.set(cv2.CAP_PROP_BRIGHTNESS, 140.0)
-    cap.set(cv2.CAP_PROP_CONTRAST, 27.0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1600)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1200)
+    cap.set(cv2.CAP_PROP_GAIN, 255)
+    cap.set(cv2.CAP_PROP_BRIGHTNESS, 188.0)
+    cap.set(cv2.CAP_PROP_CONTRAST, 120)
     cap.set(cv2.CAP_PROP_HUE, 13)  # 13.0
-    cap.set(cv2.CAP_PROP_SATURATION, 28.0)
-    cap.set(cv2.CAP_PROP_EXPOSURE, 1)
+    cap.set(cv2.CAP_PROP_SATURATION, 128)
+    cap.set(cv2.CAP_PROP_EXPOSURE, -7)
 
 if __name__ == ("__main__"):
     setup_cam()
-
-    # Read the current setting from the camera
-    test = cap.get(cv2.CAP_PROP_POS_MSEC)
-    ratio = cap.get(cv2.CAP_PROP_POS_AVI_RATIO)
-    frame_rate = cap.get(cv2.CAP_PROP_FPS)
+    # # Read the current setting from the camera
     width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
     brightness = cap.get(cv2.CAP_PROP_BRIGHTNESS)
@@ -30,9 +30,11 @@ if __name__ == ("__main__"):
     gain = cap.get(cv2.CAP_PROP_GAIN)
     exposure = cap.get(cv2.CAP_PROP_EXPOSURE)
 
-    print("Test: ", test)
-    print("Ratio: ", ratio)
-    print("Frame Rate: ", frame_rate)
+    total_pixels = width * height
+
+    # print("Test: ", test)
+    # print("Ratio: ", ratio)
+    # print("Frame Rate: ", frame_rate)
     print("Height: ", height)
     print("Width: ", width)
     print("Brightness: ", brightness)
@@ -42,11 +44,27 @@ if __name__ == ("__main__"):
     print("Gain: ", gain)
     print("Exposure: ", exposure)
 
-    while True:
-        ret, img = cap.read()
-        greyimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        count_b = cv2.countNonZero(greyimg)
-        print("Non black pixels: " + str(count_b))
-        # cv2.imwrite("image.png", greyimg)
 
-    cap.release()
+    # if cap.isOpened() == True:
+    #     count_b_old = 0
+    #     while True:
+    #         ret, img = cap.read()
+    #         if ret == True:
+    #             greyimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #             count_b = cv2.countNonZero(greyimg)
+    #
+    #             k = 0.05
+    #             count_b_old = (k * count_b) + ((1 - k) * count_b_old)
+    #
+    #             # if count_b > threshold:
+    #             print((count_b_old / total_pixels))
+    #                 # cv2.imwrite("image.png", img)
+
+    if cap.isOpened() == True:
+        ret, img = cap.read()
+        if ret == True:
+            greyimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            count_b = cv2.countNonZero(greyimg)
+            cv2.imwrite("image.png", img)
+        cap.release()
+        cv2.destroyAllWindows()
