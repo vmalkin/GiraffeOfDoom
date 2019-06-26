2a9b8669f274e949ff7ded617ac704c23f594a0e
Vaughn Malkin <vmalkin@es.co.nz>
2019-06-26 15:18:20 +1200

    Update fgm-3_v1.ino
    Arduino Sketch to read from a FGM-3 style fluxgate sensor. Returns the number of pulses in a set interval

--- fgm-3_v1/fgm-3_v1.ino
+++ fgm-3_v1/fgm-3_v1.ino
@@ -1,13 +1,33 @@
 // FGM-3 Fluxgate Sensor Sketch
 #define FGM 8
+unsigned long sensor_read_interval = 2000000;
+unsigned long sensor_read_total;
+unsigned long current_timer = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(FGM, INPUT_PULLUP);
+  delay(5000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println(pulseIn(FGM, LOW));
+  sensor_read_total = 0;
+  while (sensor_read_total <= sensor_read_interval)
+  {
+    sensor_read_total = sensor_read_total + sensor_reading();
+    }
+  if (micros() > current_timer)
+  {
+    Serial.println(micros() - current_timer);
+    current_timer = micros();
+    }
+
 }
+
+int sensor_reading()
+{
+  int reading;
+  reading = pulseIn(FGM, LOW);
+  return reading;
+  }
