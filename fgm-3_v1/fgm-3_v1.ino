97190da6f75dc5fb7a83a87c8c5d1bd201e5f940
vmalkin <meep_esq@hotmail.com>
2019-06-27 20:05:48 +1200

    Update fgm-3_v1.ino
    setup for 2 second recording interval. added watchdog timer

--- fgm-3_v1/fgm-3_v1.ino
+++ fgm-3_v1/fgm-3_v1.ino
@@ -1,18 +1,21 @@
 // FGM-3 Fluxgate Sensor Sketch
 // FGM sensor is powered off Arduino 5V. Output pin from FGM to pin 8 on Uno.
-
+#include <avr/wdt.h>
 #define FGM 8
 unsigned long sensor_read_interval = 1000000;  // Approx 200000 per second
 unsigned long current_timer = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  wdt_enable(WDTO_8S);
   Serial.begin(9600);
   pinMode(FGM, INPUT_PULLUP);
 }
 
 void loop() {
+  wdt_reset();
   Serial.println(sensor_freq());
+  delay(2000);
 }
 
 // This method counts pulses from the FGM for an interval of time and returns the frequency
