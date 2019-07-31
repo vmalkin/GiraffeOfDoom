ced96bf6a964d0fba6f11cfe892c233e6e45848f
Vaughn Malkin <vmalkin@es.co.nz>
2019-07-31 12:58:32 +1200

    median filter with adjustable window

--- pyDataReader v3.0/test.py
+++ pyDataReader v3.0/test.py
@@ -6,8 +6,8 @@ import constants as k
 filemanager = mgr_files.FileManager()
 datamanager = mgr_data.DataList()
 
-# templist = mgr_grapher.median_filter(datamanager.data_array)
-templist = mgr_grapher.median_window_filter(datamanager.data_array, 5)
+templist = mgr_grapher.median_filter(datamanager.data_array)
+# templist = mgr_grapher.median_window_filter(datamanager.data_array, 5)
 templist = mgr_grapher.recursive_filter(templist)
 
 binlist = mgr_grapher.BinBinlist(60, templist, k.publish_folder + "/dna_fgm1.csv")
