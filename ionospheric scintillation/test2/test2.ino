918a8ff223a247637ba5fac44cc254157b165850
vmalkin <vaughnmalkin@gmail.com>
2019-09-08 12:07:37 +1200

    testing manipulation of nmea sentences

---
+++ ionospheric scintillation/test2/test2.ino
@@ -0,0 +1,66 @@
+/*
+  Read NMEA sentences over I2C using Ublox module SAM-M8Q, NEO-M8P, etc
+  By: Nathan Seidle
+  SparkFun Electronics
+  Date: August 22nd, 2018
+  License: MIT. See license file for more information but you can
+  basically do whatever you want with this code.
+
+  This example reads the NMEA characters over I2C and pipes them to MicroNMEA
+  This example will output your current long/lat and satellites in view
+ 
+  Feel like supporting open source hardware?
+  Buy a board from SparkFun!
+  ZED-F9P RTK2: https://www.sparkfun.com/products/15136
+  NEO-M8P RTK: https://www.sparkfun.com/products/15005
+  SAM-M8Q: https://www.sparkfun.com/products/15106
+
+  For more MicroNMEA info see https://github.com/stevemarple/MicroNMEA
+
+  Hardware Connections:
+  Plug a Qwiic cable into the GPS and a BlackBoard
+  If you don't have a platform with a Qwiic connection use the SparkFun Qwiic Breadboard Jumper (https://www.sparkfun.com/products/14425)
+  Open the serial monitor at 115200 baud to see the output
+  Go outside! Wait ~25 seconds and you should see your lat/long
+*/
+
+#include <Wire.h> //Needed for I2C to GPS
+#include "SparkFun_Ublox_Arduino_Library.h" //http://librarymanager/All#SparkFun_Ublox_GPS
+SFE_UBLOX_GPS gps;
+
+String nmea_sentence;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("SparkFun Ublox Example");
+
+  Wire.begin();
+
+  if (gps.begin() == false)
+  {
+    Serial.println(F("Ublox GPS not detected at default I2C address. Please check wiring. Freezing."));
+    while (1);
+  }
+}
+
+void loop()
+{
+  gps.checkUblox(); //See if new data is available. Process bytes as they come in.
+  delay(1000); //Don't pound too hard on the I2C bus
+}
+
+//This function gets called from the SparkFun Ublox Arduino Library
+//As each NMEA character comes in you can specify what to do with it
+//Useful for passing to other libraries like tinyGPS, MicroNMEA, or even
+//a buffer, radio, etc.
+void SFE_UBLOX_GPS::processNMEA(char incoming)
+{
+  nmea_sentence = nmea_sentence + incoming;
+  //Serial.print(incoming);
+  if (incoming == '*')
+  {
+    Serial.println(nmea_sentence);
+    String nmea_sentence;
+    }
+}
