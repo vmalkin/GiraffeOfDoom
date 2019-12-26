f6c9b52becc3eed6b70ad1ea98536d120dcfcfdf
vmalkin <meep_esq@hotmail.com>
2019-12-26 17:26:48 +1300

    fgfg

---
+++ DnACore V2/runSys.bat
@@ -0,0 +1,6 @@
+python station_Bz.py
+python station_GIC.py
+python station_GOES.py
+python station_rururapid.py
+python station_solarwind.py
+pause
--- DnACore V2/runSys.sh
+++ DnACore V2/runSys.sh
@@ -1,6 +1,10 @@
-#! bin/bash
-python3 station_Bz.py
-python3 station_GIC.py
-python3 station_GOES.py
-python3 station_rururapid.py
-python3 station_solarwind.py
+#!/bin/bash
+while :
+do
+	python3 station_Bz.py
+	python3 station_GIC.py
+	python3 station_GOES.py
+	python3 station_rururapid.py
+	python3 station_solarwind.py
+	sleep 300
+done
