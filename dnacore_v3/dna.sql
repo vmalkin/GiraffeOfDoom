8d40b36dfdb0ec813295269e8ae9e443e4347c26
Vaughn Malkin <vaughnmalkin@gmail.com>
2020-02-03 16:36:09 +1300

    test DB setup

---
+++ dnacore_v3/dna.sql
@@ -0,0 +1,20 @@
+drop table if exists station;
+drop table if exists station_data;
+PRAGMA foreign_keys = ON;
+
+create table station (
+station_id text not null primary key
+);
+
+create table station_data(
+station_id text,
+data_value text,
+foreign key (station_id) references station(station_id)
+);
+
+insert into station(station_id) values ("Ruru_Obs");
+insert into station(station_id) values ("Dn_Aurora");
+insert into station(station_id) values ("GOES_16");
+insert into station(station_id) values ("Geomag_Bz");
+insert into station(station_id) values ("SW_speed");
+insert into station(station_id) values ("SW_Density");
