20e19a51a7017e5163115ba677587ccc714a5f54
vmalkin <vaughnmalkin@gmail.com>
2020-02-16 13:38:22 +1300

    experimenting with state machine

--- dnacore_v3/get_ruru.py
+++ dnacore_v3/get_ruru.py
@@ -1,9 +1,46 @@
 import sqlite3
 import constants as k
 
-if __name__ == "__main__":
-    dna_core = sqlite3.connect(k.dbfile)
-    db = dna_core.cursor()
 
-    dna_core.commit()
-    db.close()
+class State:
+    """State Class - a series of states for this FSM"""
+    data_append = "data_append"
+    data_connect = "data_connect"
+    initialise = "initialise"
+    exit = "exit"
+
+dna_core = sqlite3.connect(k.dbfile)
+db = dna_core.cursor()
+state = State()
+machine_state = state.initialise
+
+def exit():
+    pass
+
+
+if __name__ == "__main__":
+    while True:
+        # perform state action
+        if machine_state == state.initialise:
+            print(machine_state)
+
+        if machine_state == state.exit:
+            print(machine_state)
+
+        # test to see if we can change state
+        if machine_state == state.initialise:
+            print("init --> Data connect")
+            machine_state = state.data_connect
+
+        if machine_state == state.data_connect:
+            machine_state = state.exit
+
+        if machine_state == state.exit:
+            print(machine_state)
+            db.close()
+            break
+
+    # dna_core = sqlite3.connect(k.dbfile)
+    # db = dna_core.cursor()
+    # dna_core.commit()
+    # db.close()
