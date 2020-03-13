69870d6ed4e6748bcae2cd10e7f726bc197f8b52
vmalkin <meep_esq@hotmail.com>
2020-03-13 15:28:53 +1300

    Update get_generic.py

--- dnacore_v3/get_generic.py
+++ dnacore_v3/get_generic.py
@@ -24,47 +24,27 @@ class State:
     """
     Define states.
     have a do_method() for each state. The do_method returns success or fail.
+    each states fails to the error stae or falls thru to the next one
+    initialise
+    get data
+    parse into list: posix, data
+    get most recent date from DB, discard old data
+    append new data to DB
+    exit
     """
     def __init__(self):
         """State Class - a series of states for this FSM"""
-        self.s_initialise = "initialise"
-        self.s_get_date = "get_date"
-        self.s_convert_data = "convert_data"
-        self.s_parse_data = "parse_data"
-        self.s_data_append = "data_append"
-        self.s_error = "error"
-        self.s_exit = "exit"
+        self.s_initialise = 1
+        self.s_get_date = 1
+        self.s_parse_data = 1
+        self.s_recent_date = 1
+        self.s_data_append = 1
+        self.s_error = 1
+        self.s_exit = 1
+
         self.mag_data = ""
 
-    def do_initialise(self):
-        result = "success"
-        return result
 
-    def do_get_date(self):
-        result = "fail"
-        return result
-
-    def do_convert_data(self):
-        result = "success"
-        return result
-
-    def do_parse_data(self):
-        result = "success"
-        return result
-
-    def do_data_append(self):
-        result = "success"
-        return result
-
-    def do_error(self, error_statement):
-        logging.error(error_statement)
-        result = "success"
-        return result
-
-    def do_exit(self):
-        result = "success"
-        print("Exiting FSM")
-        return result
 
 state = State()
 machine_state = state.s_initialise
@@ -76,34 +56,7 @@ if __name__ == "__main__":
         counter = counter + 1
         # #######################################
         # test to see if we can change state
-        if machine_state == state.s_initialise:
-            machine_state = state.s_get_date
-
-        if machine_state == state.s_get_date:
-            result = state.do_get_date()
-            if result == "success":
-                machine_state = state.do_parse_data()
-            elif result == "fail":
-                machine_state = state.s_error
-                print("ERROR: unable to get data")
-                state.do_error("ERROR: unable to get data")
-            else:
-                machine_state = state.s_error
-                print("ERROR: Serious failure of data get")
-                state.do_error("ERROR: Serious failure of data get")
-
-
-        if machine_state == state.s_get_date:
-            pass
-
-        if machine_state == state.s_parse_data:
-            pass
-
-        if machine_state == state.s_data_append:
-            pass
-
-        if machine_state == state.s_error:
-            machine_state = state.s_exit
+        state.s_
 
         if counter == 100:
             print("Counter Triggered")
