94c55e809e8cfdf4f1523b7b03803dce088e08d7
vmalkin <meep_esq@hotmail.com>
2020-04-04 15:14:34 +1300

    Plot data as bins created

--- dnacore_v3/get_data.bat
+++ dnacore_v3/get_data.bat
@@ -3,6 +3,7 @@ python get_ruru.py
 python get_goes16.py
 python get_SW_speed.py
 python get_SW_density.py
-python plot_data.py
+python get_bz.py
+rem python plot_data.py
 timeout 300
 goto start
--- dnacore_v3/plot_bins.py
+++ dnacore_v3/plot_bins.py
@@ -20,37 +20,36 @@ logging.info("Created error log for this session")
 dna_core = sqlite3.connect(k.dbfile)
 db = dna_core.cursor()
 timeformat = '%Y-%m-%d %H:%M:%S'
+# stations = ["Ruru_Obs", "GOES_16", "Geomag_Bz", "SW_speed", "SW_Density"]
 stations = k.stations
-
 finish_time = int(time.time())
 start_time = finish_time - (60 * 60 * 24)
-binsize = 60  # in seconds
+binsize = 60
+number_bins = int((finish_time - start_time) / binsize) + 2
+null_value = " "
 
-class Bins:
-    def __init__(self, posixtime):
+class DataPoint():
+    def __init__(self, timevalue):
         self.datalist = []
-        self.posixtime = posixtime
+        self.timevalue = timevalue
 
-    def return_avg(self):
-        return mean(self.datalist)
+    def avg_data(self):
+        if len(self.datalist) > 0:
+            return mean(self.datalist)
+        else:
+            return null_value
 
-    def return_posix(self):
-        return self.posixtime
-
-binlist = []
-for i in range(0, 86400, binsize):
-    b = Bins(start_time)
-    binlist.append(bin)
-    start_time = start_time + i
-
-def binindex(posixdate):
-    indexnumber = int((posixdate - start_time) / binsize)
-    return indexnumber
 
 def posix2utc(posixvalue):
     # utctime = datetime.datetime.fromtimestamp(int(posixvalue)).strftime('%Y-%m-%d %H:%M:%S')
     utctime = datetime.datetime.utcfromtimestamp(int(posixvalue)).strftime(timeformat)
     return utctime
+
+
+def bin_indexvalue(nowtime):
+    returnvalue = int((int(nowtime) - start_time) / binsize)
+    return returnvalue
+
 
 def check_create_folders():
     for station in stations:
@@ -63,14 +62,23 @@ def check_create_folders():
         except Exception:
             print("Some kind of error happened creating the directory for " + station)
 
+
 def save_logfiles():
     # result = db.execute("select station_data.posix_time, station_data.data_value from station_data")
     # print(result.fetchall())
     for station in stations:
+        datapoint_list = []
+        timestamp = start_time
+        for i in range(0, number_bins):
+            dp = DataPoint(timestamp)
+            datapoint_list.append(dp)
+            timestamp = timestamp + binsize
+
         result = db.execute("select station_data.posix_time, station_data.data_value from station_data "
                             "where station_data.station_id = ? and station_data.posix_time > ?", [station, start_time])
 
         current_stationdata = result.fetchall()
+
         # Setup for saving basic log files
         savefile = station + "//" + datetime.datetime.fromtimestamp(int(time.time())).strftime('%Y-%m-%d') + ".csv"
         nowfile = station + ".csv"
@@ -90,16 +98,25 @@ def save_logfiles():
         s.close()
 
         # Bin the data into one minute bins then save
-        print("Bin the data into one minute bins then save")
         for item in current_stationdata:
-            date = item[0]
-            data = item[1]
-            index = binindex(date)
-            print(date)
+            try:
+                index = bin_indexvalue(item[0])
+                data = float(item[1])
+                datapoint_list[index].datalist.append(data)
+            except IndexError:
+                print("Index error in datapoint list")
 
+        binned_data = []
+        header = station + ", data"
+        binned_data.append(header)
+        for item in datapoint_list:
+            timevalue = item.timevalue
+            datavalue = item.avg_data()
+            dp = str(posix2utc(timevalue)) + ", " + str(datavalue)
+            binned_data.append(dp)
 
         with open(nowfile, "w") as n:
-            for item in tempfile:
+            for item in binned_data:
                 n.write(item + "\n")
         n.close()
 
