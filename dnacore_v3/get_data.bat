e8e888fdd59b7ab50a74128978db167cbad5d176
vmalkin <meep_esq@hotmail.com>
2020-04-09 16:55:23 +1200

    serer

--- dnacore_v3/get_data.bat
+++ dnacore_v3/get_data.bat
@@ -1,9 +1,14 @@
 :start
+rem Get basic data and plot the basic csv files
 python get_ruru.py
 python get_goes16.py
 python get_SW_speed.py
 python get_SW_density.py
 python get_bz.py
-rem python plot_data.py
+python plot_bins.py
+
+rem specialised plotting files.
+rem python plot_dxdt.py
+
 timeout 300
 goto start
