eb10d47d3fc4d4dffcaefe469ec11adfa7cf16dd
Vaughn Malkin <vaughnmalkin@gmail.com>
2020-09-21 13:01:53 +1200

    hide sidebar

--- dnacore_v3/plot_spark_ruru.py
+++ dnacore_v3/plot_spark_ruru.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 
 minvalue = 0
-maxvalue = 3
+maxvalue = 5
 data = []
 hours = []
 title = "DnA No 1"
@@ -41,7 +41,7 @@ ax.annotate('Now', xy=(0,0.5), xytext=(0.5, 0.5), color ="white")
 ax.annotate('24 hours ago', xy=(0,23), xytext=(0.5, 23), color ="white")
 
 b = ax.imshow(data, cmap='viridis', interpolation="hanning", vmin=minvalue, vmax=maxvalue, extent=(0,5,0,24))
-cbar = ax.figure.colorbar(b, ax=ax)
+# cbar = ax.figure.colorbar(b, ax=ax)
 # cbar.ax.set_ylabel("x background level")
 
 # plt.colorbar(b)
