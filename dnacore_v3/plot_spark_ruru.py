03b0546714556bfc278cb32596fe2683500d0096
Vaughn Malkin <vaughnmalkin@gmail.com>
2020-09-23 16:39:01 +1200

    adjusting position of y axis ticks for plotters

--- dnacore_v3/plot_spark_ruru.py
+++ dnacore_v3/plot_spark_ruru.py
@@ -40,7 +40,7 @@ ax.set_title(title)
 ax.annotate('Now', xy=(0,0.5), xytext=(0.5, 0.5), color ="white")
 ax.annotate('24 hours ago', xy=(0,23), xytext=(0.5, 23), color ="white")
 
-b = ax.imshow(data, cmap='viridis', interpolation="hanning", vmin=minvalue, vmax=maxvalue, extent=(0,5,0,24))
+b = ax.imshow(data, cmap='viridis', interpolation="hanning", vmin=minvalue, vmax=maxvalue, extent=(0,5,-0.5,23.5))
 # cbar = ax.figure.colorbar(b, ax=ax)
 # cbar.ax.set_ylabel("x background level")
 # plt.colorbar(b)
