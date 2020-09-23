b7d8865faf7253ece2ebb4f9500d98a47b66353d
vmalkin <meep_esq@hotmail.com>
2020-09-23 20:21:18 +1200

    working on new Bz plotter

---
+++ dnacore_v3/plot_spark_bz.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from datetime import datetime
+
+minvalue = 0
+maxvalue = 3
+data = []
+hours = []
+title = "IMF Bz"
+savefile = "bz.png"
+
+tempdata = []
+
+def convert_datetime_to_hour(datetimestring):
+    timeformat = "%Y-%m-%d %H:%M:%S"
+    dateobject = datetime.strptime(datetimestring, timeformat)
+    hr = datetime.strftime(dateobject, "%H:%M")
+    return hr
+
+with open("Geomag_Bz_spark.csv", "r") as f:
+    dd = []
+    for line in f:
+        d = line.strip("\n")
+        d = d.split(",")
+        hr = d[0]
+        da = float(d[1])
+        dd.append(da)
+        hr = convert_datetime_to_hour(hr)
+        hours.append(hr)
+        data.append(dd)
+
+
+# draw the heatmap
+
+# fig, ax = plt.subplots(figsize=(10,3))
+fig, ax = plt.subplots()
+ax.set_xticks(range(len(hours)))
+
+tick_space = 1
+ax.set_xticklabels(hours)
+ax.xaxis.set_major_locator(ticker.MultipleLocator(tick_space))
+
+ax.set_yticks([-9,-9,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9])
+ax.set_xlabel("UTC Hour")
+ax.set_title(title)
+ax.bar(x=hours, height=dd)
+plt.grid(color='#95a5a6', linestyle='-', linewidth=1, axis='y', alpha=0.7)
+
+fig.tight_layout()
+plt.xticks(rotation=90)
+
+plt.show()
+# plt.savefig(savefile)
+plt.close('all')
