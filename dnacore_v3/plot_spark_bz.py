7beafcbc4e400b6e8b6705642fb713d98454aa69
vmalkin <meep_esq@hotmail.com>
2020-09-28 14:22:17 +1300

    first working iteration of Bz

--- dnacore_v3/plot_spark_bz.py
+++ dnacore_v3/plot_spark_bz.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 minvalue = 0
 maxvalue = 3
-null_value = ""
+null_value = 0
 hours = []
 title = "IMF Bz"
 savefile = "bz.png"
@@ -25,12 +25,13 @@ with open("Geomag_Bz_spark.csv", "r") as f:
         d = d.split(",")
         hr = d[0]
         da = float(d[1])
+        d = []
         if da > 0:
-            d_hi.append(str(da))
+            d_hi.append((da))
             d_lo.append(null_value)
         if da <= 0:
             d_hi.append(null_value)
-            d_lo.append(str(da))
+            d_lo.append((da))
         hr = convert_datetime_to_hour(hr)
         hours.append(hr)
 
@@ -47,8 +48,8 @@ ax.set_xticklabels(hours)
 ax.set_yticks([-9,-9,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9])
 ax.set_xlabel("UTC Hour")
 ax.set_title(title)
-ax.bar(x=hours, height=d_hi, color='black')
-# ax.bar(x=hours, height=d_lo, color='red')
+ax.bar(x=hours, height=d_hi, color='green')
+ax.bar(x=hours, height=d_lo, color='orange')
 plt.grid(color='#95a5a6', linestyle='-', linewidth=1, axis='y', alpha=0.7)
 
 # fig.tight_layout()
