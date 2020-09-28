8059d4e092fe044f727c866be650e2079cefb42e
vmalkin <meep_esq@hotmail.com>
2020-09-28 15:33:09 +1300

    image size set. Still a problem with setting the spacing of the x-axis labels

--- dnacore_v3/plot_spark_bz.py
+++ dnacore_v3/plot_spark_bz.py
@@ -5,6 +5,7 @@ from datetime import datetime
 minvalue = 0
 maxvalue = 3
 null_value = 0
+hour_label_spacing = 2
 hours = []
 title = "IMF Bz"
 savefile = "bz.png"
@@ -20,6 +21,7 @@ def convert_datetime_to_hour(datetimestring):
 with open("Geomag_Bz_spark.csv", "r") as f:
     d_hi = []
     d_lo = []
+    i = 0
     for line in f:
         d = line.strip("\n")
         d = d.split(",")
@@ -33,17 +35,14 @@ with open("Geomag_Bz_spark.csv", "r") as f:
             d_hi.append(null_value)
             d_lo.append((da))
         hr = convert_datetime_to_hour(hr)
-        hours.append(hr)
+        i = i + 1
+        if i % hour_label_spacing == 0:
+            hours.append(hr)
+        else:
+            hours.append("")
 
-# draw the heatmap
-
-# fig, ax = plt.subplots(figsize=(10,3))
 fig, ax = plt.subplots(figsize=(10, 4))
-ax.set_xticks(range(len(hours)))
-
-tick_space = 30
-ax.set_xticklabels(hours)
-ax.xaxis.set_major_locator(ticker.MultipleLocator(tick_space))
+# ax.set_xticks(range(len(hours)))
 
 ax.set_yticks([-9,-9,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9])
 ax.set_xlabel("UTC Hour")
@@ -51,10 +50,8 @@ ax.set_title(title)
 ax.bar(x=hours, height=d_hi, color='#509050')
 ax.bar(x=hours, height=d_lo, color='red')
 plt.grid(color='#95a5a6', linestyle='-', linewidth=1, axis='y', alpha=0.7)
-
 fig.tight_layout()
-plt.xticks(rotation=90)
-
+plt.xticks(hours, rotation=90)
 # plt.show()
 plt.savefig(savefile)
 plt.close('all')
