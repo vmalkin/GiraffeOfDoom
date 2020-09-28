be9fa247c9c1618af8e3822da4fb69967a09c397
vmalkin <meep_esq@hotmail.com>
2020-09-28 13:58:44 +1300

    modifying Bz to get multi colour columns

--- dnacore_v3/plot_spark_bz.py
+++ dnacore_v3/plot_spark_bz.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 minvalue = 0
 maxvalue = 3
-data = []
+null_value = ""
 hours = []
 title = "IMF Bz"
 savefile = "bz.png"
@@ -18,17 +18,21 @@ def convert_datetime_to_hour(datetimestring):
     return hr
 
 with open("Geomag_Bz_spark.csv", "r") as f:
-    dd = []
+    d_hi = []
+    d_lo = []
     for line in f:
         d = line.strip("\n")
         d = d.split(",")
         hr = d[0]
         da = float(d[1])
-        dd.append(da)
+        if da > 0:
+            d_hi.append(str(da))
+            d_lo.append(null_value)
+        if da <= 0:
+            d_hi.append(null_value)
+            d_lo.append(str(da))
         hr = convert_datetime_to_hour(hr)
         hours.append(hr)
-        data.append(dd)
-
 
 # draw the heatmap
 
@@ -38,15 +42,16 @@ ax.set_xticks(range(len(hours)))
 
 tick_space = 1
 ax.set_xticklabels(hours)
-ax.xaxis.set_major_locator(ticker.MultipleLocator(tick_space))
+# ax.xaxis.set_major_locator(ticker.MultipleLocator(tick_space))
 
 ax.set_yticks([-9,-9,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9])
 ax.set_xlabel("UTC Hour")
 ax.set_title(title)
-ax.bar(x=hours, height=dd)
+ax.bar(x=hours, height=d_hi, color='black')
+# ax.bar(x=hours, height=d_lo, color='red')
 plt.grid(color='#95a5a6', linestyle='-', linewidth=1, axis='y', alpha=0.7)
 
-fig.tight_layout()
+# fig.tight_layout()
 plt.xticks(rotation=90)
 
 plt.show()
