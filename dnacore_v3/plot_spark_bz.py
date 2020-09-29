24a7b7e04f11e2fb443b99a8bd91290ee84eadbd
vmalkin <meep_esq@hotmail.com>
2020-09-29 20:10:34 +1300

    Update plot_spark_bz.py

--- dnacore_v3/plot_spark_bz.py
+++ dnacore_v3/plot_spark_bz.py
@@ -51,8 +51,8 @@ d_hi.reverse()
 
 fig, ax = plt.subplots(figsize=(4, 6))
 
-ax.set_xticks([-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9])
-ax.set_xlim(-9,9)
+# ax.set_xticks([-10, -9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9, 10])
+ax.set_xlim(-10,10)
 ax.set_ylabel("UTC Hour")
 ax.set_xlabel("Bz - nT")
 ax.set_title(title)
