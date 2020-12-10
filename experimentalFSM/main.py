9da333617d9dd89b2ddae138a343a7b9f8a11d24
vmalkin <meep_esq@hotmail.com>
2020-12-10 13:46:13 +1300

    ultra compact FSM

--- experimentalFSM/main.py
+++ experimentalFSM/main.py
@@ -1,28 +1,57 @@
+from time import sleep
+
 class State():
-    def on_event(self):
+    def transition(self, event):
         """Events that can cause a transition out"""
         pass
 
-    def __str__(self):
-        return self.__class__.__name__
 
 
 class StateDrive(State):
-    def on_event(self, event):
+    def __init__(self):
+        # do something when in this state
+        print("Driving")
+
+    def transition(self, event):
         if event == 'obst_left':
             return StateRight()
-        if event == 'obst_right':
+        elif event == 'obst_right':
             return StateLeft()
+        else:
+            return StateDrive()
 
 class StateRight(State):
-    def on_event(self, event):
+    def __init__(self):
+        # do something when in this state
+        print("Turning Right")
+
+    def transition(self, event):
         if event == 'obst_null':
             return StateDrive()
+        else:
+            return StateRight()
 
 class StateLeft(State):
-    def on_event(self, event):
+    def __init__(self):
+        # do something when in this state
+        print("Turning Left")
+
+    def transition(self, event):
         if event == 'obst_null':
             return StateDrive()
+        else:
+            return StateLeft()
 
+class Robot:
+    def __init__(self):
+        self.state = StateDrive()
 
+    def transition(self, event):
+        self.state = self.state.transition(event)
 
+robby = Robot()
+
+if __name__ == "__main__":
+    while True:
+        robby.transition('obst_right')
+        sleep(1)
