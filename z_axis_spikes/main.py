88e946c54bed3056e3d1bed936fc8c20957ac0b2
Vaughn Malkin <vaughnmalkin@gmail.com>
2021-05-03 15:14:20 +1200

    New Frankencoil script

---
+++ z_axis_spikes/main.py
@@ -0,0 +1,21 @@
+z_data = [12,13,14,13,12,1,14,1,6,14,13,134,13,17,18,19,18,17,16,14,14,15,16]
+threshold = 4
+
+def parse_data(z_data):
+    pass
+
+
+def create_diffs(data):
+    pass
+
+
+def remove_negative_values(data):
+    pass
+
+
+if __name__ == '__main__':
+    data =  parse_data(z_data)
+    data = create_diffs(data)
+    data = remove_negative_values(data)
+
+    
