cca2a9fe7c74f1529cea401df5b2563349578951
vmalkin <meep_esq@hotmail.com>
2021-08-04 22:56:55 +1200

    completed polar to rectangular coordinate function

--- CME_Detector/test2.py
+++ CME_Detector/test2.py
@@ -7,9 +7,12 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from statistics import median
 from PIL import Image, ImageDraw
+from math import sin, cos, radians
 
-offset_x = -10
-offset_y = -7
+offset_x = 0
+offset_y = 0
+image_size = 512
+imagecentre = image_size / 2
 
 def image_load(file_name):
     img = cv2.imread(file_name)
@@ -21,36 +24,69 @@ def image_save(file_name, image_object):
 
 
 def polar_to_rectangular(angle, distance):
-    image_size = 512
-    imagecentre = image_size / 2
     """
-    With our image, we have a line at an angle radiating from the centre. We want
-    the pixel value at the end. THis method will return the [x,y] co-ords accounting
+    With our image, we have a line at an angle , radiating from
+    the centre. We want the pixel value at the end. THis method will return the [x,y] co-ords accounting
     for the offset the actual centre point from the geometric centre of the image
+
     Angle: in degrees measured clockwise from North/top
     Distance: in pixels, as a radius measured from the centre.
     """
     if angle == 0 or angle == 360:
         x = imagecentre
-        y = distance
+        y = imagecentre - distance
+
+    if angle > 0:
+        if angle < 90:
+            delta_x = distance * sin(radians(angle))
+            delta_y = distance * cos(radians(angle))
+            x = imagecentre + delta_x
+            y = imagecentre - delta_y
 
     if angle == 90:
-        x = distance
+        x = imagecentre + distance
         y = imagecentre
+
+    if angle > 90:
+        if angle < 180:
+            angle = angle - 90
+            delta_y = distance * sin(radians(angle))
+            delta_x = distance * cos(radians(angle))
+            x = imagecentre + delta_x
+            y = imagecentre + delta_y
 
     if angle == 180:
         x = imagecentre
         y = imagecentre + distance
 
+    if angle > 180:
+        if angle < 270:
+            angle = angle - 180
+            delta_x = distance * sin(radians(angle))
+            delta_y = distance * cos(radians(angle))
+            x = imagecentre - delta_x
+            y = imagecentre + delta_y
+
     if angle == 270:
         x = imagecentre - distance
         y = imagecentre
 
+    if angle > 270:
+        if angle < 360:
+            angle = angle - 270
+            delta_x = distance * sin(radians(angle))
+            delta_y = distance * cos(radians(angle))
+            x = imagecentre - delta_x
+            y = imagecentre - delta_y
+
     # finally add the offsets and return
-    x = x + offset_x
-    y = y + offset_y
+    x = int(x + offset_x)
+    y = int(y + offset_y)
     return [x,y]
 
 
-
 test = image_load("part1.jpg")
+
+for i in range(88, 93):
+    coords = polar_to_rectangular(i, 100)
+    print(i, coords)
