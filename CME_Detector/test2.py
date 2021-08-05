393d4fd3ca79e5373c6b9bb60d47ad9fd26bd8d4
vmalkin <meep_esq@hotmail.com>
2021-08-05 22:51:21 +1200

    preliminary and wrong test of conversions!

--- CME_Detector/test2.py
+++ CME_Detector/test2.py
@@ -1,12 +1,6 @@
-import datetime
-import time
 import cv2
 import numpy as np
-import calendar
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-from statistics import median
-from PIL import Image, ImageDraw
+from PIL import Image as im
 from math import sin, cos, radians
 
 offset_x = 0
@@ -85,8 +79,24 @@ def polar_to_rectangular(angle, distance):
     return [x,y]
 
 
-test = image_load("part1.jpg")
+test = image_load("part2.jpg")
+greyimg = cv2.cvtColor(test, cv2.COLOR_BGR2GRAY)
+pic = cv2.split(greyimg)
+pic = pic[0]
 
-for i in range(88, 93):
-    coords = polar_to_rectangular(i, 100)
-    print(i, coords)
+t = []
+for i in range(0, 201):
+    for j in range(0, 361):
+        coords = polar_to_rectangular(j, 1)
+        t.append(pic[coords[0], coords[1]])
+
+# https://www.geeksforgeeks.org/convert-a-numpy-array-to-an-image/
+array = np.arange(0, 72000, 1, np.uint8)
+print(array.shape)
+array = np.reshape(array, (200, 360))
+testpic = im.fromarray(array)
+testpic.save('testpic.png')
+
+cv2.imshow("Test", testpic)
+cv2.waitKey(0) # waits until a key is pressed
+cv2.destroyAllWindows() # destroys the window showing image
