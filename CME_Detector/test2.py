918500c1ccf76cd9293e98e3274612c2ea360c0f
vmalkin <meep_esq@hotmail.com>
2021-08-06 16:49:50 +1200

    update

--- CME_Detector/test2.py
+++ CME_Detector/test2.py
@@ -27,22 +27,26 @@ def polar_to_rectangular(angle, distance):
     Distance: in pixels, as a radius measured from the centre.
     """
     if angle == 0 or angle == 360:
+        # print("a == 0")
         x = imagecentre
         y = imagecentre - distance
 
     if angle > 0:
         if angle < 90:
+            # print("0 < a < 90")
             delta_x = distance * sin(radians(angle))
             delta_y = distance * cos(radians(angle))
             x = imagecentre + delta_x
             y = imagecentre - delta_y
 
     if angle == 90:
+        # print("a == 90")
         x = imagecentre + distance
         y = imagecentre
 
     if angle > 90:
         if angle < 180:
+            # print("90 < a < 180")
             angle = angle - 90
             delta_y = distance * sin(radians(angle))
             delta_x = distance * cos(radians(angle))
@@ -50,11 +54,13 @@ def polar_to_rectangular(angle, distance):
             y = imagecentre + delta_y
 
     if angle == 180:
+        # print("a == 180")
         x = imagecentre
         y = imagecentre + distance
 
     if angle > 180:
         if angle < 270:
+            # print("180 < a < 270")
             angle = angle - 180
             delta_x = distance * sin(radians(angle))
             delta_y = distance * cos(radians(angle))
@@ -62,11 +68,13 @@ def polar_to_rectangular(angle, distance):
             y = imagecentre + delta_y
 
     if angle == 270:
+        # print("a == 270")
         x = imagecentre - distance
         y = imagecentre
 
     if angle > 270:
         if angle < 360:
+            # print("270 < a < 360")
             angle = angle - 270
             delta_y = distance * sin(radians(angle))
             delta_x = distance * cos(radians(angle))
@@ -79,7 +87,7 @@ def polar_to_rectangular(angle, distance):
     return [x,y]
 
 
-test = image_load("part1.jpg")
+test = image_load("part5.jpg")
 greyimg = cv2.cvtColor(test, cv2.COLOR_BGR2GRAY)
 pic = cv2.split(greyimg)
 pic = pic[0]
@@ -88,9 +96,9 @@ t = []
 u = []
 
 # the y direction
-for i in range(0, 200):
+for i in range(200, 0, -1):
     # the x direction
-    for j in range(0, 360):
+    for j in range(360, 0, -1):
         coords = polar_to_rectangular(j, i)
         pixelvalue = pic[coords[0], coords[1]]
         u.append(pixelvalue)
