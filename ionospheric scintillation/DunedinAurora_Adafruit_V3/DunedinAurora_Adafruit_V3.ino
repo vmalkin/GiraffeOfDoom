8cff43ae38232bba58ee35aec060fab5240e1c35
vmalkin <meep_esq@hotmail.com>
2021-09-09 21:29:23 +1200

    This sketch sets the GPS to 10hz! Finally!!

---
+++ ionospheric scintillation/DunedinAurora_Adafruit_V3/DunedinAurora_Adafruit_V3.ino
@@ -0,0 +1,58 @@
+// Test code for Adafruit GPS modules using MTK3329/MTK3339 driver
+//
+// This code shows how to listen to the GPS module in an interrupt
+// which allows the program to have more 'freedom' - just parse
+// when a new NMEA sentence is available! Then access data when
+// desired.
+//
+// Tested and works great with the Adafruit Ultimate GPS module
+// using MTK33x9 chipset
+//    ------> http://www.adafruit.com/products/746
+// Pick one up today at the Adafruit electronics shop
+// and help support open source hardware & software! -ada
+
+#include <Adafruit_GPS.h>
+#include <SoftwareSerial.h>
+
+// Connect the GPS Power pin to 5V
+// Connect the GPS Ground pin to ground
+// Connect the GPS TX (transmit) pin to Digital 8
+// Connect the GPS RX (receive) pin to Digital 7
+
+// you can change the pin numbers to match your wiring:
+SoftwareSerial mySerial(8, 7);
+Adafruit_GPS GPS(&mySerial);
+
+// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
+// Set to 'true' if you want to debug and listen to the raw GPS sentences
+#define GPSECHO  true
+#define PMTK_SET_BAUD_38400 "$PMTK251,38400*27"
+#define PMTK_SET_BAUD_19200 "$PMTK251,19200*22"
+
+void setup()
+{
+
+  // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
+  // also spit it out
+  Serial.begin(115200);
+  delay(5000);
+  Serial.println("Adafruit GPS library basic test!");
+
+// Default speed is 9600.
+  GPS.begin(19200);
+//  GPS.sendCommand(PMTK_SET_BAUD_19200);
+
+  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_GSVONLY);
+  GPS.sendCommand(PGCMD_NOANTENNA); // OFF
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_10HZ);
+}
+
+uint32_t timer = millis();
+void loop()                     // run over and over again
+{
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+  if ((c) && (GPSECHO))
+    Serial.write(c);
+}
