4c5addc87451e7448bc782a372c250d9e0c12a37
Vaughn Malkin <vaughnmalkin@gmail.com>
2021-09-17 15:44:07 +1200

    cleaned up old comments

--- Arduino/variance/variance.ino
+++ Arduino/variance/variance.ino
@@ -1,30 +1,3 @@
-/*
-  Getting time and date using u-blox commands
-  By: Nathan Seidle
-  SparkFun Electronics
-  Date: April 16th, 2019
-  License: MIT. See license file for more information but you can
-  basically do whatever you want with this code.
-
-  This example shows how to use the Millisecond and Nanosecond output as well as increase the
-  I2C speed (100 to 400kHz), and serial output (115200 to 500kbps).
-
-  Note: you will need to set your Serial Monitor to 500000 Baud to see the output
-
-  Leave NMEA parsing behind. Now you can simply ask the module for the datums you want!
-
-  Feel like supporting open source hardware?
-  Buy a board from SparkFun!
-  ZED-F9P RTK2: https://www.sparkfun.com/products/15136
-  NEO-M8P RTK: https://www.sparkfun.com/products/15005
-  SAM-M8Q: https://www.sparkfun.com/products/15106
-
-  Hardware Connections:
-  Plug a Qwiic cable into the GNSS and a BlackBoard
-  If you don't have a platform with a Qwiic connection use the SparkFun Qwiic Breadboard Jumper (https://www.sparkfun.com/products/14425)
-  Open the serial monitor at 115200 baud to see the output
-*/
-
 #include <Wire.h> //Needed for I2C to GNSS
 
 #include <SparkFun_u-blox_GNSS_Arduino_Library.h> //http://librarymanager/All#SparkFun_u-blox_GNSS
