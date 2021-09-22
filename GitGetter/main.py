837f0953f4ad71af377e28131398fa0f85137561
Vaughn Malkin <vaughnmalkin@gmail.com>
2021-09-23 11:53:25 +1200

    New Project - GIT getter
    Project to parse git repos for student/user stats, to help with marking, etc

---
+++ GitGetter/main.py
@@ -0,0 +1,17 @@
+# This script is designed to use the Github api to extract information about a series of repos, their contributors
+# and their work
+# Data will be displayed graphically.
+
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
