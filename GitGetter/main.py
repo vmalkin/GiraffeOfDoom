ada5104dd3f5bd5ac921911f53619438cd7e447a
Vaughn Malkin <vaughnmalkin@gmail.com>
2021-09-23 17:06:43 +1200

    github API tool created

--- GitGetter/main.py
+++ GitGetter/main.py
@@ -2,16 +2,30 @@
 # and their work
 # Data will be displayed graphically.
 
+from github import Github
+import requests
+from pprint import pprint
+
+git_token = "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"
+g = Github(git_token)
 
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+repos = [
+    "BIT-Studio-2/project-21s2-buddy-on-the-beach",
+    "BIT-Studio-2/project-21s2-ark-tech",
+    "BIT-Studio-2/project-21s2-beach-boys",
+    "BIT-Studio-2/project-21s2-beach-buddy",
+    "BIT-Studio-2/project-21s2-jackal",
+    "BIT-Studio-2/project-21s2-paw-patrol",
+    "BIT-Studio-2/project-21s2-sea-dogs",
+    "BIT-Studio-2/project-21s2-walkeez"
+    ]
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    for item in repos:
+        r = g.get_repo(item)
+        pprint(r.get_contributors().get_page(0))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
