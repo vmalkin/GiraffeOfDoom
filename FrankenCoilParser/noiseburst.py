93dd138c7b61e0d46caf3f856c7162375be570ed
Vaughn Malkin <vaughnmalkin@gmail.com>
2021-11-10 16:09:31 +1300

    refining parsing of initial data

--- FrankenCoilParser/noiseburst.py
+++ FrankenCoilParser/noiseburst.py
@@ -18,7 +18,10 @@ begin_time = time.time() - (86400 * 1)
 def recursive_smooth(x_now, smoothed_prev):
     # Exponential smoothing
     k = 0.5
-    return (k * x_now) + ((1 - k) * smoothed_prev)
+    x_n = float(x_now)
+    x_p = float(smoothed_prev)
+    x_new = (k * x_n) + ((1 - k) * x_p)
+    return x_new
 
 
 def utc_to_posix(utc_time):
@@ -29,7 +32,7 @@ def utc_to_posix(utc_time):
 
 if __name__ == "__main__":
     # open master csv data, load into array
-    initial_datalist = []
+    raw_datalist = []
     refined_datalist = []
 
     # ***********************************************************************************
@@ -40,21 +43,38 @@ if __name__ == "__main__":
             line.strip("\n")
             line_s = line.split(",")
             datething = line_s[0]
-
+            #  Get only the last 24 hours of data
             if re.match(regex, datething):
                 dt = utc_to_posix(datething)
                 if dt > begin_time:
-                    initial_datalist.append(line)
+                    raw_datalist.append(line)
+
+    initialvalue = raw_datalist[0]
+    refined_datalist.append(initialvalue)
+
+    for i in range(1, len(raw_datalist)):
+        # The new row to be appended to the refined data
+        dp = []
+        date = raw_datalist[i][0]
+        dp.append(date)
+        # recursive smooth the data and build up the datapoint
+        for j in range(1, len(raw_datalist[i])):
+            x_now = raw_datalist[i][j]
+            smoothed_prev = refined_datalist[i-1][j]
+            print(x_now, smoothed_prev)
+            d = recursive_smooth(x_now, smoothed_prev)
+            dp.append(d)
+        #append the smoothed data
+        refined_datalist.append(dp)
+
 
     # create the display file for upload to DunedinAurora.NZ
     #  Smooth the data
     with open(graphing_file, "w") as g:
         g.write("UTC Datetime,125hz,240hz,410hz,760hz,1800hz,4300hz,9000hz \n")
-        for i in range(1, len(initial_datalist)):
-            data = initial_datalist[i].split(",")
-            data_string = data[0] + ","
-            for j in range(1, len(data)):
-
+        for i in range(1, len(refined_datalist)):
+            d = refined_datalist[i] + "\n"
+            g.write(d)
     g.close()
     print("Data files created")
 
