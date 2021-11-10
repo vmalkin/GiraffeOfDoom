f0baaa0f4a2282a0a6c1ba7a9c4ff28af95a9087
vmalkin <vaughnmalkin@gmail.com>
2021-11-10 21:19:37 +1300

    Implementing recursive smoothing of frequency data

--- FrankenCoilParser/noiseburst.py
+++ FrankenCoilParser/noiseburst.py
@@ -11,17 +11,17 @@ datafile = "hiss.csv"
 regex = r"\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d"
 dt_format = "%Y-%m-%d %H:%M:%S"
 stationname = "dna_hiss"
-graphing_file = stationname + "_graph.csv"
+graphing_file = stationname + "_smoothed.csv"
 begin_time = time.time() - (86400 * 1)
 
 
 def recursive_smooth(x_now, smoothed_prev):
     # Exponential smoothing
-    k = 0.5
+    k = 0.1
     x_n = float(x_now)
     x_p = float(smoothed_prev)
     x_new = (k * x_n) + ((1 - k) * x_p)
-    return x_new
+    return round(x_new, 3)
 
 
 def utc_to_posix(utc_time):
@@ -47,7 +47,7 @@ if __name__ == "__main__":
             if re.match(regex, datething):
                 dt = utc_to_posix(datething)
                 if dt > begin_time:
-                    raw_datalist.append(line)
+                    raw_datalist.append(line_s)
 
     initialvalue = raw_datalist[0]
     refined_datalist.append(initialvalue)
@@ -55,21 +55,23 @@ if __name__ == "__main__":
     for i in range(1, len(raw_datalist)):
         # The new row to be appended to the refined data
         dp = []
-        data_r = raw_datalist[i].strip("\n")
-        data_s = refined_datalist[i - 1].strip("\n")
-        data_r = data_r.split(",")
-        data_s = data_s.split(",")
+        data_r = raw_datalist[i]
+        data_s = refined_datalist[i - 1]
+
+        # data_r = data_r.split(",")
+        # data_s = data_s.split(",")
 
         # recursive smooth the data and build up the datapoint
-        for length in range(0, len(raw_datalist[i])):
+        # for length in range(0, len(raw_datalist[i])):
+        for length in range(0, 8):
             if length == 0:
                 dp.append(data_r[0])
             else:
                 x_now = data_r[length]
                 smoothed_prev = data_s[length]
-                print(x_now, smoothed_prev)
                 d = recursive_smooth(x_now, smoothed_prev)
                 dp.append(d)
+
         # append the smoothed data
         refined_datalist.append(dp)
 
@@ -77,8 +79,11 @@ if __name__ == "__main__":
     #  Smooth the data
     with open(graphing_file, "w") as g:
         g.write("UTC Datetime,125hz,240hz,410hz,760hz,1800hz,4300hz,9000hz \n")
-        for i in range(1, len(refined_datalist)):
-            d = refined_datalist[i] + "\n"
+        for line in refined_datalist:
+            d = line[0]
+            for i in range(1, len(line)):
+                d = d + "," + str(line[i])
+            d = d + "\n"
             g.write(d)
     g.close()
     print("Data files created")
