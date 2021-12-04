7f1c3a94794a073e428767116434efb7a927818f
vmalkin <vaughnmalkin@gmail.com>
2021-12-04 13:09:16 +1300

    Set up database manager

--- Coronal Hole Suite - Version 4/mgr_sqlite.py
+++ Coronal Hole Suite - Version 4/mgr_sqlite.py
@@ -1,29 +1,42 @@
 import sqlite3
 
 database = "solarwind.db"
-
+"""
+ [satellite] 1..* [data] *..1 [data_type]
+"""
 
 def db_create():
     print("Creating database...")
     data_b = sqlite3.connect(database)
     db = data_b.cursor()
-    db.execute("drop table if exists sat")
-    db.execute("drop table if exists data")
-    db.execute("drop table if exists type")
+    db.execute("drop table if exists sat;")
+    db.execute("drop table if exists data;")
+    db.execute("drop table if exists type;")
 
-    db.execute("create table sat(sat_name text primary key)")
-    db.execute("create table type(data_type text primary key)")
+    db.execute("create table sat(sat_name text primary key);")
+    db.execute("create table type(data_type text primary key);")
     db.execute("create table data("
                "posixtime integer,"
                "data real,"
                "sat_name text,"
                "data_type text,"
-               "foreign key sat_name references sat(sat_name),"
-               "foreign key data_type references type(data_type)"
-               ")")
+               "foreign key (sat_name) references sat(sat_name),"
+               "foreign key (data_type) references type(data_type)"
+               ");")
     data_b.commit()
     db.close()
 
 
 def db_populate():
-    pass
+    data_b = sqlite3.connect(database)
+    db = data_b.cursor()
+    db.execute("insert into sat (sat_name) values (?);", ["discovr"])
+    db.execute("insert into sat (sat_name) values (?);", ["goes_primary"])
+    db.execute("insert into sat (sat_name) values (?);", ["sdo"])
+
+    db.execute("insert into data (data_type) values (?);", ["speed"])
+    db.execute("insert into data (data_type) values (?);", ["density"])
+    db.execute("insert into data (data_type) values (?);", ["forecast speed"])
+
+    data_b.commit()
+    db.close()
