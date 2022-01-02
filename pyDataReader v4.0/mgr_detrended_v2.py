095de110aaecf92992dea087d023a9ef570cff7c
vmalkin <vaughnmalkin@gmail.com>
2022-01-02 20:04:08 +1300

    detrender for data logger

---
+++ pyDataReader v4.0/mgr_detrended_v2.py
@@ -0,0 +1,21 @@
+# The number of readings that equates to one and a half hours of time.
+half_window = 90
+
+def calc_start(datalist):
+    pass
+
+
+def calc_end(datalist):
+    pass
+
+
+def calc_middle(datalist):
+    pass
+
+
+def parse_dates(datalist):
+    pass
+
+
+def wrapper(datalist, publishdir):
+    pass
