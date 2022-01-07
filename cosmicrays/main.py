b0a720e1dc825c0878005cf121220bb68f037665
vmalkin <vaughnmalkin@gmail.com>
2022-01-08 00:15:22 +1300

    experimenting with camera parameters

--- cosmicrays/main.py
+++ cosmicrays/main.py
@@ -1,40 +1,36 @@
 import cv2
+import numpy as np
+
+def camera_setup_c270(cam):
+    """
+    https://physicsopenlab.org/2016/05/18/diy-webcam-particle-detector/
+    Resolution = 640 x 480
+    Exposure = -7 (corresponding to 1/10 s)
+    Gain = 255
+    Sharp = 255
+    """
+    cam.set(cv2.CAP_PROP_GAIN, 255)
+    cam.set(cv2.CAP_PROP_BRIGHTNESS, 130)
+    # # No
+    # camera.set(cv2.CAP_PROP_GAMMA, 128)
+    # Can set. 255 max value
+    cam.set(cv2.CAP_PROP_SATURATION, 32)
+    # camera.set(cv2.CAP_PROP_HUE, -1)
+    cam.set(cv2.CAP_PROP_CONTRAST, 32)
+    cam.set(cv2.CAP_PROP_SHARPNESS, 255)
+    # Set to zero for auto exposure. Set to 1 for manual exposure
+    # cam.set(cv2.CAP_PROP_AUTO_EXPOSURE, -12)
+    # max of 10000 manually
+    camera.set(cv2.CAP_PROP_EXPOSURE, 120)
+
+
 
 if __name__ == '__main__':
     camera = cv2.VideoCapture(2)
+    camera_setup_c270(camera)
+    print("Exposure: ", camera.get(cv2.CAP_PROP_EXPOSURE))
 
 
-    # Can set - no change
-    # camera.set(cv2.CAP_PROP_GAIN, 255)
-
-    # camera.set(cv2.CAP_PROP_BRIGHTNESS, 255)
-    #
-    # # No
-    # camera.set(cv2.CAP_PROP_GAMMA, 1)
-
-    # Can set. 255 max value
-    camera.set(cv2.CAP_PROP_SATURATION, 100)
-    camera.set(cv2.CAP_PROP_HUE, -1)
-    camera.set(cv2.CAP_PROP_CONTRAST, 100)
-    # Set to zero for auto exposure. Set to 1 for manual exposure
-    camera.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0)
-    # max of 10000 manually
-    camera.set(cv2.CAP_PROP_EXPOSURE, 10)
-
-    # showing values of the properties
-    print("CV_CAP_PROP_FRAME_WIDTH: '{}'".format(camera.get(cv2.CAP_PROP_FRAME_WIDTH)))
-    print("CV_CAP_PROP_FRAME_HEIGHT : '{}'".format(camera.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-    print("CAP_PROP_FPS : '{}'".format(camera.get(cv2.CAP_PROP_FPS)))
-    print("CAP_PROP_POS_MSEC : '{}'".format(camera.get(cv2.CAP_PROP_POS_MSEC)))
-    print("CAP_PROP_FRAME_COUNT  : '{}'".format(camera.get(cv2.CAP_PROP_FRAME_COUNT)))
-    print("CAP_PROP_BRIGHTNESS : '{}'".format(camera.get(cv2.CAP_PROP_BRIGHTNESS)))
-    print("CAP_PROP_CONTRAST : '{}'".format(camera.get(cv2.CAP_PROP_CONTRAST)))
-    print("CAP_PROP_SATURATION : '{}'".format(camera.get(cv2.CAP_PROP_SATURATION)))
-    print("CAP_PROP_HUE : '{}'".format(camera.get(cv2.CAP_PROP_HUE)))
-    print("CAP_PROP_GAIN  : '{}'".format(camera.get(cv2.CAP_PROP_GAIN)))
-    print("CAP_PROP_CONVERT_RGB : '{}'".format(camera.get(cv2.CAP_PROP_CONVERT_RGB)))
-    print("CAP_PROP_GAMMA : '{}'".format(camera.get(cv2.CAP_PROP_GAMMA)))
-    print("CAP_PROP_EXPOSURE : '{}'".format(camera.get(cv2.CAP_PROP_EXPOSURE)))
 
     while True:
         ret, frame = camera.read()
@@ -46,4 +42,4 @@ if __name__ == '__main__':
             break
 
     camera.release()
-    cv2.destroyAllWindows()
+    # cv2.destroyAllWindows()
