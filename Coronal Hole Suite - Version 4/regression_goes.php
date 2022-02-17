135891e0f1fe89ca8b2ec9439a5d52d5349796bb
vmalkin <vaughnmalkin@gmail.com>
2022-02-17 20:29:23 +1300

    Added Empirical Mode Decomposition module to look for patterns in cosmic ray data

--- Coronal Hole Suite - Version 4/regression_goes.php
+++ Coronal Hole Suite - Version 4/regression_goes.php
@@ -1,2 +1,2 @@
-<br>Insufficient time has passed to begin forecasting. 0.996 days remaining
+<br>Insufficient time has passed to begin forecasting. 1.0 days remaining
 
--- cosmicrays/mgr_emd.py
+++ cosmicrays/mgr_emd.py
@@ -1,5 +1,5 @@
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+# from plotly.subplots import make_subplots
 import matplotlib.pyplot as plt
 import datetime
 import emd
@@ -51,44 +51,15 @@ def wrapper(datafile, plotname):
             data.append(da)
             data_subset.pop(0)
 
-    print(len(dates), len(data))
-
     n = np.array(data, dtype='float')
 
     sample_rate = len(n)
     imf = emd.sift.sift(n)
-    print(imf.shape)
-    IP, IF, IA = emd.spectra.frequency_transform(imf, sample_rate, 'hilbert')
-    # Define frequency range (low_freq, high_freq, nsteps, spacing)
-    freq_range = (1, 20, 10, 'linear')
-    f, hht = emd.spectra.hilberthuang(IF, IA, freq_range, sum_time=False)
 
-    fig = plt.figure(figsize=(10, 6))
-    emd.plotting.plot_imfs(imf, scale_y=True, cmap=True, fig=fig)
-    fig.show()
-
-    # fig = plt.figure(figsize=(10, 6))
-    # emd.plotting.plot_hilberthuang(hht, len(dates), f,
-    #                                time_lims=(2, 4), freq_lims=(0.1, 15),
-    #                                fig=fig, log_y=True)
-
+    # fig = go.Figure(go.Scatter(x=dates, y=imf))
+    # fig.show()
 
     print("data is " + str(len(imf)) + " records long")
-
-    # # Create the plotly subsplots
-    # fig = make_subplots(rows=len(imf[0])+1, cols=1)
-    # fig.update_xaxes(nticks=24, tickangle=45)
-    # fig.add_trace(go.Scatter(x=dates, y=data, mode="lines", line=dict(width=2, color="#ff0000")), row=1, col=1)
-    # for i in range(0, len(imf[0])):
-    #     data = imf[:, i]
-    #     r = i+2
-    #     # fig.add_trace(go.Scatter(x=dt, y=data, mode="lines", line=dict(width=2, color="#000000")), row=r, col=1)
-    #     fig.add_trace(go.Scatter(x=dates, y=data, mode="lines", line=dict(width=1)), row=r, col=1)
-    # title = "Instrinsic Mode functions - " + plotname
-    # fig.update_layout(height=1000, width=1000, title_text=title)
-    # # fig.show()
-    # filename = "imd_" + plotname + ".svg"
-    # fig.write_image(file=filename, format="svg")
     print("Plot finished")
 
 
