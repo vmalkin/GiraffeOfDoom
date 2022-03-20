dfe86bd162e3e9d1e86bf68223a548c629820788
vmalkin <vaughnmalkin@gmail.com>
2022-03-20 22:54:04 +1300

    Ionospheric plotter - cumulative S4 tracker

--- ionospheric scintillation/test.py
+++ ionospheric scintillation/test.py
@@ -1,20 +1,47 @@
-import mgr_s4_count_stats
+import mgr_polar_noise_tracks
+import mgr_s4_stats_json
+import mgr_s4_count
 
-x = [3,3,3,3,4,4,5,4,3,2]
-#
-# print(mgr_s4_count_stats.mean(x))
-# print(mgr_s4_count_stats.median(x))
-# print(mgr_s4_count_stats.calc_stdev(x))
+import sqlite3
+import time
+
+# This is the query output that will be used to generate graphs and plots etc.
+querydata_24 = []
+querydata_48 = []
+current_stats = []
+sat_database = "gps_satellites.db"
+optimum_altitude = 25
+
+
+def database_parse(hourduration):
+    starttime = int(time.time()) - (60 * 60 * hourduration)
+    print("Parsing database...")
+    gpsdb = sqlite3.connect(sat_database)
+    db = gpsdb.cursor()
+
+    result = db.execute('select sat_id, posixtime, alt, az, s4, snr from satdata where posixtime > ? and alt > ? order by posixtime asc', [starttime, optimum_altitude])
+    returnlist = []
+    for item in result:
+        dp = (item[0], item[1], item[2], item[3], item[4], item[5])
+        returnlist.append(dp)
+    print("current query " + str(len(returnlist)) + " records long")
+    gpsdb.commit()
+    db.close()
+    return returnlist
+
+
+querydata_24 = database_parse(24)
+querydata_72 = database_parse(72)
+timeformat = '%Y-%m-%d %H:%M:%S'
+sat_database = "gps_satellites.db"
+integration_time = 30
+optimum_altitude = 25
+
+print("***************************** Start Query Processor")
+
+mgr_polar_noise_tracks.wrapper(querydata_24)
+mgr_s4_count.wrapper(querydata_72)
 
 
 
-testcount = mgr_s4_count_stats.load_values("test.pkl")
-print("current list is ", testcount)
-
-testcount = mgr_s4_count_stats.append_value(testcount, 3)
-
-print("Appended value. Mean list is ", meanlist)
-
-print("median value of list ", mgr_s4_count_stats.calc_median(meanlist))
-mgr_s4_count_stats.save_values(meanlist, "test.pkl")
 
