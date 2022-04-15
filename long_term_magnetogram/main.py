9d36bdacdb82dd9a2cad3336d6bd8ef2321bafa3
vmalkin <vaughnmalkin@gmail.com>
2022-04-15 14:26:50 +1200

    implementing methods to process magnetometer data

--- long_term_magnetogram/main.py
+++ long_term_magnetogram/main.py
@@ -1,11 +1,13 @@
 import standard_stuff as k
 from os import path
+from statistics import median, mean
 import re
 
 # Index positions of UTC date and data in each logfile. This could be different...
 index_utcdate = 0
 index_data = 1
 regex_filename = "\d\d\d\d-\d\d-\d\d.csv"
+
 
 # Create a bin object. A bin:
 # Covers a time period
@@ -27,9 +29,52 @@ class Bin:
         pass
 
 
+def median_filter(list_to_parse):
+    # Each element in the list has the format [datetime, data]
+    returnlist = []
+    for i in range(1, len(list_to_parse) - 1):
+        t = []
+        t.append(list_to_parse[1][i - 1])
+        t.append(list_to_parse[1][i])
+        t.append(list_to_parse[1][i + 1])
+        medvalue = median(t)
+        dt = list_to_parse[i][0]
+        # the new datetime and median values
+        newdp = [dt, medvalue]
+        returnlist.append(newdp)
+    return returnlist
+
+
+def h_to_dhdt(array_time_data):
+    returnlist = []
+    for i in range(1, len(array_time_data)):
+        tt = array_time_data[i][0]
+        dh = array_time_data[i][1] - array_time_data[i - 1][1]
+        dp = [tt, dh]
+        returnlist.append(dp)
+    return returnlist
+
+
+def smooth_data(array_time_data):
+    # Each element in the list has the format [datetime, data]
+    # The half window value should cover 10 minutes of data
+    returnlist = []
+    halfwindow = 10 * 4
+    for i in range(halfwindow, len(array_time_data) - halfwindow):
+        d = []
+        for j in range(0 - halfwindow, halfwindow):
+            if j == 0:
+                tt = array_time_data[i + j][0]
+            d.append(array_time_data[i + j][1])
+        dd = mean(d)
+        dp = [tt, dd]
+        returnlist.append(dp)
+    return returnlist
+
+
 if __name__ == '__main__':
     regex_data = "[0-9][.][0-9]"
-    masterlist = []
+    array_time_data = []
 
     # does files.txt exist? if not, abort
     if path.exists(k.file_list):
@@ -49,13 +94,17 @@ if __name__ == '__main__':
                                 posixtime = k.utc2posix(newcsv[0], '%Y-%m-%d %H:%M:%S')
                                 data = newcsv[index_data]
                                 dp = [posixtime, data]
-                                masterlist.append(dp)
-        # Sort by date
-        masterlist.sort()
-
+                                array_time_data.append(dp)
 
         # Remove any spikes in data with a median filter.
+        array_time_data = median_filter(array_time_data)
+
         # smooth the data
+        array_time_data = smooth_data(array_time_data)
+
+        # Convert the data into dh/dt
+        array_time_data = h_to_dhdt(array_time_data)
+
         # Create a series of 365 dated bins for the previous 365 days
         # parse thru the list and allocate data values to bins (each bin will have a list of data for the day)
         # open the sightings file. allocate the dates of sightings to each bin.
