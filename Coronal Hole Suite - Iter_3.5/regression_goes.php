df0bf0cf4b608aa309c79b089b92966c08f17dc5
vmalkin <vaughnmalkin@gmail.com>
2022-04-21 10:50:12 +1200

    refactor of CH Suite

---
+++ Coronal Hole Suite - Iter_3.5/regression_goes.php
@@ -0,0 +1,2 @@
+<br>Linear approximation is: Predicted windspeed = 430.03 + 622.56 * coronal hole area on meridian     <br>Pearsons correlation |r| = 0.3130
+<br>Average Windspeed is 439.6km/s<br>Space weather transit time is 3.9 days
