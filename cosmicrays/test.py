e72aff04739424b55bd7207affd5b36c27d3b92e
vmalkin <vaughnmalkin@gmail.com>
2022-04-22 12:01:25 +1200

    adjusting cosmic ray detector date range

--- cosmicrays/test.py
+++ cosmicrays/test.py
@@ -1,9 +1,7 @@
-import mgr_plot_simple_avg
-import mgr_plot_avg_hits
 import sqlite3
 import time
 import datetime
-import mgr_emd
+from plotly import graph_objects as go
 
 database = "events.db"
 
@@ -35,23 +33,40 @@ def database_get_data(hours_duration):
     db.close()
     return tempdata
 
-# data = []
-# file = "dr01_24hr.csv"
-# with open(file, "r") as f:
-#     for line in f:
-#         line = line.rstrip("\n")
-#         l = line.split(",")
-#         dt = utc2posix(l[0])
-#         da = l[1]
-#         dp = str(dt) + "," + str(da)
-#         data.append(dp)
+def plot(dates, data):
+    fig = go.Figure(go.Bar(x=dates, y=data,
+                           marker=dict(color='#340059', line=dict(width=0.5, color='#340059'))))
+    fig.update_xaxes(ticks='outside', tickangle=45)
+    # fig.update_yaxes(range=[0, 1],  nticks=2)
+    # fig.update_layout(font=dict(size=14), title_font_size=21)
+    # fig.update_layout(plot_bgcolor="#a0a0a0", paper_bgcolor="#a0a0a0")
+    # fig.update_layout(width=1400, height=400,
+    #                   title="Muons - Average Hits " + str(hrs) + " hour avg.",
+    #                   xaxis_title="Date/time UTC<br><sub>http://DunedinAurora.nz</sub>")
+    # title = "muons_avg_" + str(hrs) + "_hr.jpg"
+    fig.show()
 
-data = database_get_data(24*30)
 
-with open("analysis.csv", "w") as a:
-    for item in data:
-        tp = item
-        tt = posix2utc(tp, "%Y-%m-%d %H:%M:%S")
-        dp = tt + "," + str(tp)
-        a.write(dp + "\n")
-a.close()
+data = database_get_data(24*60)
+data_counts = []
+data_times = []
+tmp = []
+for i in range(0, len(data) - 1):
+    if posix2utc(data[i + 1], "%d") == posix2utc(data[i], "%d"):
+        tmp.append(1)
+
+    if posix2utc(data[i + 1], "%d") != posix2utc(data[i], "%d"):
+        tt = posix2utc(data[i], "%Y-%m-%d")
+        dd = sum(tmp)
+        data_counts.append(dd)
+        data_times.append(tt)
+        tmp = []
+
+    if i == len(data) - 1:
+        tt = posix2utc(data[i], "%Y-%m-%d")
+        dd = sum(tmp)
+        data_counts.append(dd)
+        data_times.append(tt)
+
+print(data_times, data_counts)
+# plot(data_times, data_counts)
