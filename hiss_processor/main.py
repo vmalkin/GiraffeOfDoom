1473d3ce50c6e18d283f0ddeb29247d8fce9bdd3
vmalkin <vaughnmalkin@gmail.com>
2022-05-18 20:32:56 +1200

    New processor for frankencoil hiss data

---
+++ hiss_processor/main.py
@@ -0,0 +1,10 @@
+# YYYY-MM-DD hh:mm:ss,125hz,240hz,410hz,760hz,1800hz,4300hz,9000hz
+# 2022-01-15 04:13:34,36.243,33.482,23.342,21.961,17.429,3.902,-3.328
+hissfile = "hiss.csv"
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
