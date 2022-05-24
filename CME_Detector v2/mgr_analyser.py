bug fixing on finding the average image

CME_Detector v2/mgr_analyser.py
@@ -350,16 +350,27 @@ def wrapper(storage_folder, analysis_folder):
             #  of finding the residual.
             # Pic is used for comparisons and must be float64
             pic = cv2.erode(img_g, kernel1, iterations=1)
-            pic = np.array(pic, np.float64)
+            # pic = cv2.normalize(src=img_g, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+            pic = cv2.normalize(src=pic, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
             avg_array.append(pic)
+
+
 
             # 100 images is about a day. Start comparing individual images against an "average" image
             if len(avg_array) >= 100:
                 # ALWAYS POP
                 avg_array.pop(0)
                 avg_img = np.mean(avg_array, axis=0)
-                detrended_img = cv2.subtract(pic, avg_img)
+                avg_img = cv2.normalize(src=avg_img, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+                print(pic.dtype, avg_img.dtype)
+
+                # detrended_img = cv2.subtract(pic, avg_img)
+                detrended_img = pic - avg_img
                 ret, detrended_img = cv2.threshold(detrended_img, 3, 255, cv2.THRESH_BINARY)
+
+                cv2.imshow('graycsale image', avg_img)
+                # waitKey() waits for a key press to close the window and 0 specifies indefinite loop
+                cv2.waitKey(0)
 
                 #  convolve the returned residuals image from polar to rectangular co-ords. the data is appended to
                 #  an array