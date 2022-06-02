80bd94c43119c70ab59d0c0edb786c6b5ce25e66
vmalkin <vaughnmalkin@gmail.com>
2022-06-02 23:03:26 +1200

    New processor for frankencoil hiss data

--- hiss_processor/main.py
+++ hiss_processor/main.py
@@ -1,9 +1,11 @@
 # YYYY-MM-DD hh:mm:ss,125hz,240hz,410hz,760hz,1800hz,4300hz,9000hz
 # 2022-01-15 04:13:34,36.243,33.482,23.342,21.961,17.429,3.902,-3.328
 hissfile = "hiss.csv"
+sightingsfile = "sightings.csv"
 
 class DataPoint:
     def __init__(self, data_csv):
+        self.sightings = 0
         self.d = data_csv.strip()
         self.dd = self.d.split(",")
         self.utc = self.dd[0]
@@ -37,3 +39,5 @@ if __name__ == '__main__':
     for i in range(1, len(datapoint_array)):
         for j in range(0, 7):
             datapoint_array[i].diff_data[j] = datapoint_array[i].diff_data[j] - datapoint_array[i - 1].diff_data[j]
+
+# 6 readings a minute
