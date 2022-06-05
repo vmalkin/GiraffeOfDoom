8453e2e33bfa0c89e5062f3c6d45306d69c8890f
vmalkin <vaughnmalkin@gmail.com>
2022-06-06 10:11:02 +1200

    New processor for frankencoil hiss data

--- hiss_processor/main.py
+++ hiss_processor/main.py
@@ -2,7 +2,7 @@ from statistics import mean
 
 # YYYY-MM-DD hh:mm:ss,125hz,240hz,410hz,760hz,1800hz,4300hz,9000hz
 # 2022-01-15 04:13:34,36.243,33.482,23.342,21.961,17.429,3.902,-3.328
-hissfile = "hiss.csv"
+hissfile = "hiss1.csv"
 sightingsfile = "sightings.csv"
 frequencies = ["125hz","240hz","410hz","760hz","1800hz","4300hz","9000hz"]
 
