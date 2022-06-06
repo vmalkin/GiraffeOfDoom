a6eb0bcee7473550dd06192603724ace6efddd61
vmalkin <vaughnmalkin@gmail.com>
2022-06-06 14:10:10 +1200

    added utility file of standard functions

--- hiss_processor/main.py
+++ hiss_processor/main.py
@@ -1,8 +1,9 @@
 from statistics import mean
+import standard_stuff
 
 # YYYY-MM-DD hh:mm:ss,125hz,240hz,410hz,760hz,1800hz,4300hz,9000hz
 # 2022-01-15 04:13:34,36.243,33.482,23.342,21.961,17.429,3.902,-3.328
-hissfile = "hiss1.csv"
+hissfile = "hiss.csv"
 sightingsfile = "sightings.csv"
 frequencies = ["125hz","240hz","410hz","760hz","1800hz","4300hz","9000hz"]
 
@@ -11,11 +12,11 @@ class DataPoint:
         self.sightings = 0
         self.d = data_csv.strip()
         self.dd = self.d.split(",")
+
         self.utc = self.dd[0]
         self.data = self.dd[1:]
 
         self.hz_data = [0, 0, 0, 0, 0, 0, 0]
-        self.diff_data = [0, 0, 0, 0, 0, 0, 0]
 
         # Add data if exists to data array
         if self.utc != 'YYYY-MM-DD hh:mm:ss':
@@ -32,11 +33,18 @@ if __name__ == '__main__':
             dp = DataPoint(line)
             datapoint_array.append(dp)
 
-    # Calculate dh/dt and populate the diffs array in each datapoint
-    for i in range(1, len(datapoint_array)):
-        for j in range(0, 7):
-            datapoint_array[i].diff_data[j] = float(datapoint_array[i].hz_data[j]) - float(datapoint_array[i - 1].hz_data[j])
+    # Add sightings to datapoints
+    with open(sightingsfile, "r") as s:
+        for line in s:
+            pass
 
+
+    # # Calculate dh/dt and populate the diffs array in each datapoint
+    # for i in range(1, len(datapoint_array)):
+    #     for j in range(0, 7):
+    #         datapoint_array[i].diff_data[j] = float(datapoint_array[i].hz_data[j]) - float(datapoint_array[i - 1].hz_data[j])
+
+    # Save out averaged readings
     # 6 readings a minute
     interval = 6 * 60
 
@@ -45,10 +53,11 @@ if __name__ == '__main__':
 
     for i in range(0, len(datapoint_array)):
         for j in range(0, 7):
-            dp = datapoint_array[i].diff_data[j]
+            dp = float(datapoint_array[i].hz_data[j])
             t[j].append(dp)
 
         if i % interval == 0:
+            # print(t)
             dt = datapoint_array[i].utc
             for j in range(0, 7):
                 dd = round(mean(t[j]), 5)
