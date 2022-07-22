aaf6b9eee0cf6b73069ad1a22c1ab090f9f0a700
vmalkin <vaughnmalkin@gmail.com>
2022-07-22 19:46:35 +1200

    experimenting with partical drawing on canvas

--- particle_sim/particles.py
+++ particle_sim/particles.py
@@ -14,19 +14,19 @@ mycanvas.pack()
 
 px_dead = 0
 px_active = 1
-px_quantity = 500
+px_quantity = 4000
 
 class Particle:
-    def __init__(self):
+    def __init__(self, pos_x=grid_x, pos_y=grid_y ):
         self.visible = True
         self.colour = "orange"
-        self.pos_x = random.randrange(0, grid_x)
-        self.pos_y = random.randrange(0, grid_y)
-        self.move_x = random.randrange(-6, 6)
-        self.move_y = random.randrange(-6, 6)
+        self.pos_x = random.randrange(0, pos_x)
+        self.pos_y = random.randrange(0, pos_y)
+        self.move_x = random.randrange(1, 6)
+        self.move_y = random.randrange(-2, 2)
 
     def draw_pixel(self):
-        mycanvas.create_rectangle(self.pos_x, self.pos_y, self.pos_x + 2, self.pos_y + 2, fill=self.colour, width=0)
+        mycanvas.create_rectangle(self.pos_x, self.pos_y, self.pos_x + 1, self.pos_y + 1, fill=self.colour, width=0)
 
     def movement_x(self):
         return self.pos_x + self.move_x
@@ -35,14 +35,16 @@ class Particle:
         return self.pos_y + self.move_y
 
     def am_i_visible(self):
+        returnvalue = True
         if self.pos_x > grid_x:
-            self.visible == False
-        if self.pos_x < 0:
-            self.visible == False
+            returnvalue = False
+        if self.pos_x < 1:
+            returnvalue = False
         if self.pos_y > grid_y:
-            self.visible == False
-        if self.pos_y < 0:
-            self.visible == False
+            returnvalue = False
+        if self.pos_y < 1:
+            returnvalue = False
+        return returnvalue
 
 
 def create_blank_grid():
@@ -66,19 +68,25 @@ if __name__ == "__main__":
         particles.append(Particle())
 
     while True:
-        time.sleep(0.05)
+        time.sleep(0.01)
         clear_canvas()
         drawing_array = []
-        print(len(drawing_array))
+
         for p in particles:
-            p.am_i_visible()
-            if p.visible == True:
+            p.visible = p.am_i_visible()
+            if p.visible:
                 p.pos_x = p.movement_x()
                 p.pos_y = p.movement_y()
                 drawing_array.append(p)
-                p.draw_pixel()
+
+        for p in drawing_array:
+            p.draw_pixel()
 
         particles = copy.deepcopy(drawing_array)
+        if len(particles) < px_quantity:
+            for i in range(0, 100):
+                particles.append(Particle(100, grid_y))
+
         mywindow.update()
     mywindow.mainloop()
 
