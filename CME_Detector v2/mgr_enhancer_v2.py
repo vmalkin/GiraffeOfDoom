97c057330532bacdaf8f430067101883615506e6
Vaughn Malkin <vaughnmalkin@gmail.com>
2023-01-08 14:45:05 +1300

    improve contrast for processed CME images

--- CME_Detector v2/mgr_enhancer_v2.py
+++ CME_Detector v2/mgr_enhancer_v2.py
@@ -1,8 +1,12 @@
 
-# get image list of LASCO files
+# get image list of LASCO files for the last x-hours.
 # if time difference between img_x, ing_y < time threshold
-# Convert img_x, img_y to greyscale
-# Convert img_x, img_y to single channel
-# New savefile = img_y name
-# for img_x, img_y:
-# for same pixel location in imag_x, img_y
+#   Convert img_x, img_y to greyscale
+#   Convert img_x, img_y to single channel
+#   New savefile = img_y name
+#   for img_x, img_y:
+#       for same pixel location in img_x, img_y:
+#           if diff between px_img_x and px_img_y greater than pixel_threshold:
+#               new_pixel = median pixel value
+#               else new_pixel = old_pixel
+#       save new image, img_z
