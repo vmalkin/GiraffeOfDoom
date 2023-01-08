61a43e2ca4ee35ae2eb9a4463ddb6e2c46935019
Vaughn Malkin <vaughnmalkin@gmail.com>
2023-01-08 14:33:13 +1300

    improve contrast for processed CME images

---
+++ CME_Detector v2/mgr_enhancer_v2.py
@@ -0,0 +1,8 @@
+
+# get image list of LASCO files
+# if time difference between img_x, ing_y < time threshold
+# Convert img_x, img_y to greyscale
+# Convert img_x, img_y to single channel
+# New savefile = img_y name
+# for img_x, img_y:
+# for same pixel location in imag_x, img_y
