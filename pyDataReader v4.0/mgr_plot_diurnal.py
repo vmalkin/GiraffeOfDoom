17208750da254b8532e6b936e45ea6f3bcca18eb
Vaughn Malkin <vaughnmalkin@gmail.com>
2023-01-11 16:43:53 +1300

    refactor of serial datalogger

--- pyDataReader v4.0/mgr_plot_diurnal.py
+++ pyDataReader v4.0/mgr_plot_diurnal.py
@@ -8,51 +8,6 @@ from plotly import graph_objects as go
 # The number of readings that equates to one and a half hours of time.
 half_window = 10
 
-def calc_start(datalist):
-    returnlist = []
-    data_start = float(datalist[0])
-    data_end = float(datalist[half_window - 1])
-    rate = (data_end - data_start) / half_window
-    d = data_start
-    returnlist.append(data_start)
-
-    for i in range(0, half_window - 1):
-        d = d + rate
-        returnlist.append(round(d,3))
-        # print(i, datalist[i], returnlist[i])
-    return returnlist
-
-
-def calc_end(datalist):
-    returnlist = []
-    data_start = float(datalist[len(datalist) - half_window])
-    data_end = float(datalist[len(datalist) - 1])
-    rate = (data_end - data_start) / half_window
-    d = data_start
-    returnlist.append(data_start)
-
-    for i in range(len(datalist) - half_window, len(datalist) - 1):
-        d = d + rate
-        returnlist.append(round(d,3))
-    return returnlist
-
-
-def calc_middle(datalist):
-    returnlist = []
-
-    for i in range(half_window, len(datalist) - half_window):
-        t = []
-        for j in range(0 - half_window, half_window):
-            t.append(float(datalist[i + j]))
-
-        if len(t) > 0:
-            d = mean(t)
-        else:
-            d = 0
-        returnlist.append(round(d,3))
-
-    return returnlist
-
 
 def plot(dt_dates, dt_detrend, savefile_name):
     width = 1500
