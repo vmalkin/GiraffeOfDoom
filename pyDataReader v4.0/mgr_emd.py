6076fb702f3e819560ba3741108a8badaa09e891
Vaughn Malkin <vaughnmalkin@gmail.com>
2023-01-12 10:10:51 +1300

    tweaks to plots

--- pyDataReader v4.0/mgr_emd.py
+++ pyDataReader v4.0/mgr_emd.py
@@ -48,7 +48,7 @@ def plot_data(imf, dates, filename):
         fig.add_trace(go.Scatter(x=dates, y=imf[:, i], mode="lines"), row=i+1, col=1)
     fig.update_layout(height=2000, width=1400, title_text="Muon Counts 356 Days - Empirical Mode Decomposition")
     # fig.show()
-    fig.write_html("emd.html")
+    # fig.write_html("emd.html")
     fig.write_image(filename)
 
 def wrapper(database, publishdirectory):
@@ -63,7 +63,7 @@ def wrapper(database, publishdirectory):
         dt_readings.append(item[1])
 
     n = np.array(dt_readings, dtype='float')
-    # sample_rate = len(n)
+    sample_rate = len(n)
     # imf = emd.sift.iterated_mask_sift(n)
     imf = emd.sift.sift(n, max_imfs=12)
 
