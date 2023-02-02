df447db4e3ea8bd2ee70bcd101b3cedbe23346b9
vmalkin <vaughnmalkin@gmail.com>
2023-02-02 20:25:04 +1300

    Refactor of GPS satellite processor

--- ionospheric scintillation v2/main.py
+++ ionospheric scintillation v2/main.py
@@ -58,17 +58,6 @@ class Satellite:
             returnarray.append(intensity)
         return returnarray
 
-    def get_s4(self):
-        # http://mtc-m21b.sid.inpe.br/col/sid.inpe.br/mtc-m21b/2017/08.25.17.52/doc/poster_ionik%20%5BSomente%20leitura%5D.pdf
-        if len(self.snr) > 2:
-            intensity = self.calc_intensity(self.snr)
-            avg_intensity = mean(intensity)
-            sigma = stdev(intensity)
-            returnvalue = round(((sigma / avg_intensity) * 100), 5)
-        else:
-            returnvalue = 0
-        return returnvalue
-
     def get_alt_avg(self):
         x = 0
         if len(self.alt) > 0:
