8070d8536b808f7c61208f3e32b2c63f10f2f8af
vmalkin <vaughnmalkin@gmail.com>
2023-03-15 22:28:23 +1300

    Refactor of GPS satellite processor

--- ionospheric scintillation v2/test.py
+++ ionospheric scintillation v2/test.py
@@ -8,29 +8,47 @@ import datetime
 import logging
 from statistics import mean, stdev
 from threading import Thread
+import math
 import mgr_database
 import mgr_plot
 import numpy as np
 from calendar import timegm
 
-class Day:
-    def __init__(self):
-        pass
+class Dday:
+    def __init__(self, datestring):
+        self.hours = []
+        self.label = datestring
+        for i in range(0, 24):
+            h = Dhours(i)
+            self.hours.append(h)
 
-class D_Hours:
-    def __init__(self):
-        pass
+class Dhours:
+    def __init__(self, label):
+        self.label = label
+        self.minutes = []
+        for i in range(0, 60):
+            m = Dmins(i)
+            self.minutes.append(m)
 
 
-class D_Mins:
-    def __init__(self):
-        pass
+class Dmins:
+    def __init__(self, label):
+        self.label = label
+        self.datavalue = []
+
+
+def posix2utc(posixtime, timeformat):
+    # print(posixtime)
+    # utctime = datetime.datetime.utcfromtimestamp(int(posixtime)).strftime(timeformat)
+    utctime = datetime.datetime.utcfromtimestamp(int(posixtime)).strftime(timeformat)
+    return utctime
 
 
 def utc2posix(utcstring, timeformat):
     utc_time = time.strptime(utcstring, timeformat)
     epoch_time = timegm(utc_time)
     return epoch_time
+
 
 utctime = "2023-02-20 00:00"
 starttime = utc2posix(utctime, '%Y-%m-%d %H:%M')
@@ -42,11 +60,26 @@ alt = 40
 result = mgr_database.qry_get_last_24hrs(actualstart, alt)
 result = np.array(result)
 
-for line in result:
-    print(line)
+start = int(result[0][1])
+end = int(result[len(result) - 1][1])
+day = 60 * 60 * 24
+# duration = (end - start) / day
+# duration = math.ceil(duration)
+
+days = []
+for i in range(start, end, day):
+    dd = Dday(i)
+    days.append(dd)
+
+for row in result:
+    psx = int(row[1])
+    hr = posix2utc(psx, '%H')
+    mn = posix2utc(psx, '%M')
+    idx
 
 
-mgr_plot.wrapper(result, k.comport)
+
+# mgr_plot.wrapper(result, k.comport)
 
 
 
