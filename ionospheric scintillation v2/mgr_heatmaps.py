876c918461c8fbf379815218be802956f0885bbb
vmalkin <vaughnmalkin@gmail.com>
2023-04-03 22:20:46 +1200

    Refactor of GPS satellite processor

--- ionospheric scintillation v2/mgr_heatmaps.py
+++ ionospheric scintillation v2/mgr_heatmaps.py
@@ -71,7 +71,8 @@ def stackplot(displaydata, timestamps, label_day, comport):
     fig.update_layout(font=dict(size=16, color="#f0f0f0"), title_font_size=18, )
 
     for item in displaydata:
-        fig.add_trace(go.Scatter(x=timestamps, y=item))
+        fig.add_trace(go.Scatter(x=timestamps, y=item, mode='lines'))
+
     fig.write_image("stackplot.jpg")
 
 
@@ -80,8 +81,14 @@ def heatmap(displaydata, timestamps, label_day, comport):
     gridcolour = "#303030"
     width = 1500
 
-    data = go.Heatmap(x=timestamps, y=label_day, colorscale = 'electric')
-    fig = go.Figure(data)
+    # # CReate individual heatmap traces
+    # data = []
+    # for item in displaydata:
+    #     trace = go.Heatmap(x=timestamps, y=label_day, z=item)
+    #     data.append(trace)
+
+    data = go.Heatmap(x=timestamps, y=label_day, z=displaydata, colorscale="electric")
+    fig = go.Figure(data=data)
 
     title = "Heatmap, SNR for " + comport
     fig.update_layout(width=width, title=title,
@@ -96,24 +103,12 @@ def heatmap(displaydata, timestamps, label_day, comport):
     fig.update_layout(plot_bgcolor=papercolour,
                       paper_bgcolor=papercolour)
 
-    for item in displaydata:
-        print(item)
-        fig.add_trace(go.Heatmap(x=timestamps, y=label_day, z=item, colorscale = 'electric'))
+    fig.show()
 
-    fig.write_image("heatmap.jpg")
+    # fig.write_image("heatmap.jpg")
 
 
 def wrapper(result, comport):
-    # utctime = "2023-02-20 00:00"
-    # starttime = utc2posix(utctime, '%Y-%m-%d %H:%M')
-    # day = 60 * 60 * 24 * 14
-    # actualstart = starttime - day
-    #
-    # alt = 40
-    # # The result of the query gets passed into all plotting functions
-    # result = mgr_database.qry_get_last_24hrs(actualstart, alt)
-    # result = np.array(result)
-
     start = int(result[0][1])
     end = int(result[len(result) - 1][1])
     day = 60 * 60 * 24
@@ -127,26 +122,14 @@ def wrapper(result, comport):
 
     # Populate the day objects with data
     for row in result:
-        psx = int(row[1])
+        posix = int(row[1])
         data = float(row[5])
-        hr = int(posix2utc(psx, '%H'))
-        mn = int(posix2utc(psx, '%M'))
-        idx = int(math.floor((psx - start) / day))
+        hr = int(posix2utc(posix, '%H'))
+        mn = int(posix2utc(posix, '%M'))
+        idx = int(math.floor((posix - start) / day))
         days[idx].hours[hr].minutes[mn].datavalue.append(data)
         # print(days[idx].hours[hr].minutes[mn].get_average())
 
-    displaydata = []
-    timestamps = []
-    for i in range(0, 24):
-        for j in range(0, 60):
-            k = j
-            if k < 10:
-                k = '0' + str(k)
-            else:
-                k = str(k)
-
-            timestring = str(i) + ":" + str(k)
-            timestamps.append(timestring)
 
     # Create the day lables. the y axis of the heatmap
     daylabels = []
@@ -154,21 +137,42 @@ def wrapper(result, comport):
         daylabels.append(posix2utc(dd.label, "%Y-%m-%d"))
 
     # Create the arrays of each days minute data
+    displaydata = []
     for dd in days:
         temparray = []
-        for i in range(0, 1440):
-            temparray.append(None)
-
         for hour in dd.hours:
             for minute in hour.minutes:
-                idx = int(hour.label) * int(minute.label)
                 data = minute.get_average()
-                temparray[idx] = data
-                ts = str(hour.label) + ":" + str(minute.label)
-                timestamps.append(ts)
+                temparray.append(data)
         displaydata.append(temparray)
 
+    # Make the array of timestamps
+    timestamps = []
+    for i in range(0, 24):
+        if i < 10:
+            hh = "0" +  str(i)
+        else:
+            hh = str(i)
+        for j in range(0, 60):
+            if j < 10:
+                mm = "0" + str(j)
+            else:
+                mm = str(j)
+            ts = hh + ":" + mm
+            timestamps.append(ts)
+
+    # for dd in displaydata:
+    #     print(len(dd))
+
     stackplot(displaydata, timestamps, daylabels, comport)
-    # heatmap(displaydata, timestamps, daylabels, comport)
+    heatmap(displaydata, timestamps, daylabels, comport)
+
+    with open("data.csv", "w") as d:
+        for item in result:
+            dd = posix2utc(item[1], "%Y-%m-%d %H:%M:%S")
+            da = item[5]
+            dp = dd + "," + da + "\n"
+            d.write(dp)
+    d.close()
 
 # mgr_plot.wrapper(result, k.comport)
