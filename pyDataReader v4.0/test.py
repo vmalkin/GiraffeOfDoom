0c7b396a2b5890bede03e646b5d95715cce1c4b6
vmalkin <vaughnmalkin@gmail.com>
2023-05-29 20:08:43 +1200

    took space out of project folder name kaleido issue

--- pyDataReader v4.0/test.py
+++
@@ -1,9 +0,0 @@
-import mgr_plot_diffs
-import constants as k
-
-print("*** dhdt: Start")
-# unprocessed magnetogram/data
-mgr_plot_diffs.wrapper(k.database, k.publish_dir)
-print("*** dhdt: Finish")
-
-
