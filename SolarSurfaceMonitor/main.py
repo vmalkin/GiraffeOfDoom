cd1749904677dcc5a098f5712afd496aff51d68f
Vaughn Malkin <vaughnmalkin@gmail.com>
2023-06-08 13:32:21 +1200

    Update main.py

--- SolarSurfaceMonitor/main.py
+++ SolarSurfaceMonitor/main.py
@@ -1,6 +1,9 @@
+#  fix opencv error in windows with:
+#  https://stackoverflow.com/questions/63869389/error-could-not-build-wheels-for-opencv-python-which-use-pep-517-and-cannot-b
+
 import cv2
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    pass
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
