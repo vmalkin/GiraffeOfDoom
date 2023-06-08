b899ba117f21441e44741c95efd4e74db7a5576d
Vaughn Malkin <vaughnmalkin@gmail.com>
2023-06-09 10:28:23 +1200

    fixed completion indicator for loading database

--- pyDataLogger_10/plot_from_csv.py
+++ pyDataLogger_10/plot_from_csv.py
@@ -13,43 +13,43 @@ database = "temp.db"
 filename = "dr01_24hr.csv"
 publish_dir = k.publish_dir
 
-# gpsdb = sqlite3.connect(database)
-# # gpsdb = sqlite3.connect(":memory:")
-# db = gpsdb.cursor()
-# db.execute('drop table if exists data;')
-# db.execute('create table data ('
-#            'posixtime text,'
-#            'datavalue real'
-#            ');')
-# gpsdb.commit()
-# db.close()
-#
-# dataarray = []
-# with open(filename, "r") as dd:
-#     counter = 0
-#     for row in dd:
-#         if counter > 0:
-#             for row in dd:
-#                 data = row.replace("\n", "")
-#                 datasplit = data.split(",")
-#                 posixtime = standard_stuff.utc2posix(datasplit[0], '%Y-%m-%d %H:%M:%S')
-#                 reading = datasplit[1]
-#                 dp = [int(posixtime) , float(reading)]
-#                 dataarray.append(dp)
-#
-#         counter = counter + 1
-#
-# db = sqlite3.connect(database)
-# cursor = db.cursor()
-# for i in range(0, len(dataarray)):
-#     cursor.execute("insert into data (posixtime, datavalue) values (?,?);", [dataarray[i][0], dataarray[i][1]] )
-#     print("Inputting data: ", i, len(dataarray))
-#     # if i == 200:
-#     #     break
-#     db.commit()
-# db.close()
+gpsdb = sqlite3.connect(database)
+# gpsdb = sqlite3.connect(":memory:")
+db = gpsdb.cursor()
+db.execute('drop table if exists data;')
+db.execute('create table data ('
+           'posixtime text,'
+           'datavalue real'
+           ');')
+gpsdb.commit()
+db.close()
 
-# mgr_plot_diurnal.wrapper(database, 0, publish_dir)
-# mgr_plot_diffs.wrapper(database, 0, publish_dir)
-# mgr_plot_detrended.wrapper(database, 0, publish_dir)
+dataarray = []
+with open(filename, "r") as dd:
+    counter = 0
+    for row in dd:
+        if counter > 0:
+            for row in dd:
+                data = row.replace("\n", "")
+                datasplit = data.split(",")
+                posixtime = standard_stuff.utc2posix(datasplit[0], '%Y-%m-%d %H:%M:%S')
+                reading = datasplit[1]
+                dp = [int(posixtime) , float(reading)]
+                dataarray.append(dp)
+
+        counter = counter + 1
+
+db = sqlite3.connect(database)
+cursor = db.cursor()
+for i in range(0, len(dataarray)):
+    cursor.execute("insert into data (posixtime, datavalue) values (?,?);", [dataarray[i][0], dataarray[i][1]] )
+    if i / 1000 == 0:
+        pcent = round((i / 1000), 2)
+        print("Completion: ", pcent)
+    db.commit()
+db.close()
+
+mgr_plot_diurnal.wrapper(database, 0, publish_dir)
+mgr_plot_diffs.wrapper(database, 0, publish_dir)
+mgr_plot_detrended.wrapper(database, 0, publish_dir)
 mgr_emd.wrapper(database, 0, publish_dir)
