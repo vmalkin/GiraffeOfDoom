609966a735d46abefb6e0ef09cd539fa156742b1
vmalkin <vaughnmalkin@gmail.com>
2023-06-10 20:41:27 +1200

    image difference experimentation

--- SolarSurfaceMonitor/main.py
+++ SolarSurfaceMonitor/main.py
@@ -2,15 +2,16 @@
 #  https://stackoverflow.com/questions/63869389/error-could-not-build-wheels-for-opencv-python-which-use-pep-517-and-cannot-b
 
 import cv2 as cv
+import numpy as np
 
 if __name__ == '__main__':
     img1 = cv.imread("or_suvi-l2-ci171_g18_s20230525T224000Z_e20230525T224400Z_v1-0-1.png", cv.IMREAD_GRAYSCALE)
     img2 = cv.imread("or_suvi-l2-ci171_g18_s20230525T224400Z_e20230525T224800Z_v1-0-1.png", cv.IMREAD_GRAYSCALE)
 
     imgd = img2 - img1
-
-    cv.imshow('unchanged image', imgd)
-    cv.waitKey(0)
-    cv.destroyAllwindows()
+    cv.imwrite("diff.jpg", imgd)
+    # cv.imshow('unchanged image', imgd)
+    # cv.waitKey(0)
+    # cv.destroyAllwindows()
 
 
