b7872123d47814c341a8c26fb95babfb1bd16578
vmalkin <vaughnmalkin@gmail.com>
2023-06-27 16:43:50 +1200

    image difference experimentation

--- SolarSurfaceMonitor/mgr_diffs_2.py
+++ SolarSurfaceMonitor/mgr_diffs_2.py
@@ -17,15 +17,25 @@ def utc2posix(utcstring, timeformat):
     epoch_time = timegm(utc_time)
     return epoch_time
 
-def create_label(image, text):
+def create_label(image, timestamp):
     # width, height, channels = image.shape
     width, height = image.shape
+    label_height = int(height / 10)
+    font_height = int(height / 12)
+    cv2.rectangle(image, (0,0), (width,label_height), (0, 0, 0), -1)
+    cv2.rectangle(image, (0, height - label_height), (width, height), (0, 0, 0), -1)
+
     font = cv2.FONT_HERSHEY_SIMPLEX
     font_size = 1
     # colours are blue, green, red in opencv
     font_color = (255, 255, 255)
     font_thickness = 1
-    cv2.putText(image, text, (100, height - 100), font, font_size, font_color, font_thickness, cv2.LINE_AA)
+    label0 = "GOES SUVI image processed at DunedinAurora.NZ Magnetic Observatory"
+    label1 = "Image time: " + timestamp
+    label2 = "Images courtesy of NOAA."
+    cv2.putText(image, label0, (0, 50), font, font_size, font_color, font_thickness, cv2.LINE_AA)
+    cv2.putText(image, label1, (0, 100), font, font_size, font_color, font_thickness, cv2.LINE_AA)
+    cv2.putText(image, label2, (0, height - 80), font, font_size, font_color, font_thickness, cv2.LINE_AA)
     return image
 
 def image_read_fromfile(name):
@@ -85,7 +95,7 @@ def wrapper(filepathlist, diffstore, pathsep):
             img_new = image_translate(img_new, 1)
 
             img_diff = cv2.addWeighted(img_old, 0.5, img_new, 0.5, 0)
-            img_diff = cv2.medianBlur(img_diff, 3)
+            img_diff = cv2.medianBlur(img_diff, 5)
 
             clahe = cv2.createCLAHE(clipLimit=20, tileGridSize=(10, 10))
             img_diff = clahe.apply(img_diff)
