fd22c15627b72074ee8f2189263756a63460be3e
vmalkin <vaughnmalkin@gmail.com>
2023-07-02 22:18:48 +1200

    added new plotter file for stacked plots

--- ionosphere_gps/mgr_plot_v2.py
+++ ionosphere_gps/mgr_plot_v2.py
@@ -12,11 +12,13 @@ median_half_window = int(readings_per_minute * 2)
 
 
 class Day:
-    def __init__(self):
+    def __init__(self, posixdate):
+        self.posixstart = posixdate
         self.lat = self.create_datastore()
         self.long = self.create_datastore()
         self.alt = self.create_datastore()
         self.hdop = self.create_datastore()
+        self.plot_null = None
 
     def create_datastore(self):
         t = []
@@ -24,9 +26,37 @@ class Day:
             t.append([])
         return t
 
-    def report_length(self, array):
-        length = len(array)
-        print("Length is: ", length)
+    def avg_mins(self, array):
+        returnarray = []
+        for item in array:
+            if len(item) == 0:
+                returnarray.append(self.plot_null)
+            else:
+                avg = mean(item)
+                returnarray.append(avg)
+        return returnarray
+
+
+def plot(gpsdata, timestamps, label, pencolour):
+    papercolour = "#d0d0d0"
+    gridcolour = "#c0c0c0"
+    width = 1500
+    height = 550
+
+    data = go.Scatter(x=timestamps, y=gpsdata, mode='lines',
+                                     line=dict(color=pencolour, width=1))
+    fig =  go.Figure(data)
+
+    title = label
+    fig.update_layout(width=width, height=height, title=title,
+                      xaxis_title="Date/time UTC<br><sub>http://DunedinAurora.nz</sub>")
+    fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor=gridcolour, nticks=24, tickangle=50)
+    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor=gridcolour)
+    fig.update_layout(font=dict(size=16, color="#202020"), title_font_size=18, )
+    fig.update_layout(plot_bgcolor=papercolour,
+                      paper_bgcolor=papercolour)
+    savefile = label + ".jpg"
+    fig.write_image(savefile)
 
 
 def wrapper(db_data, label):
@@ -40,22 +70,31 @@ def wrapper(db_data, label):
     numsats = 4
     hdop = 6
     alt = 6
-    current_day = None
 
+    current_day = None
     day_stack = []
+    d = Day(0)
+
     for gpsdata in db_data:
-        index_day = standard_stuff.posix2utc(gpsdata[psxtime], "%d")
+        index_day = standard_stuff.posix2utc(gpsdata[psxtime], "%Y-%m-%d")
         if index_day != current_day:
             current_day = index_day
-            d = Day()
-        index_minute = int(standard_stuff.posix2utc(gpsdata[psxtime], "%M"))
-        d.lat[index_minute].append(gpsdata[lat])
-        d.long[index_minute].append(gpsdata[long])
-        d.alt[index_minute].append(gpsdata[alt])
-        d.hdop[index_minute].append(gpsdata[hdop])
-    #     index_hour = standard_stuff.posix2utc(gpsdata[posixtime], "%H")
-    #     index_minute = standard_stuff.posix2utc(gpsdata[posixtime], "%M")
-    for day in day_stack:
-        day.report_length(day.lat)
+            day_stack.append(d)
+            # set the posix date for the Day object to be the start of the UTC day
+            psxstart = standard_stuff.utc2posix(index_day, "%Y-%m-%d")
+            d = Day(psxstart)
+
+        else:
+            index_minute = int((int(gpsdata[psxtime]) - d.posixstart) / 60)
+            d.lat[index_minute].append(float(gpsdata[lat]))
+            d.long[index_minute].append(float(gpsdata[long]))
+            d.alt[index_minute].append(float(gpsdata[alt]))
+            d.hdop[index_minute].append(float(gpsdata[hdop]))
+    day_stack.pop(0)
+
+    plotdata = day_stack[0].avg_mins(day_stack[0].lat)
+
+    plot(plotdata, None, "test", "#ff0000")
+
 
 
