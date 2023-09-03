b13654f5b882e4022012674f61895d6ef2f6e8b4
vmalkin <vaughnmalkin@gmail.com>
2023-09-03 13:39:00 +1200

    created simple plotter

--- CoronalHoleSuite_v4/mgr_simple_plot.py
+++ CoronalHoleSuite_v4/mgr_simple_plot.py
@@ -1,2 +1,9 @@
 # THis will generate a simple line plot of solar wind speed for the last 3 carrington rotations
 # and highlight repeating events.
+
+import common_data as k
+import sqlite3
+
+def wrapper():
+    pass
+
