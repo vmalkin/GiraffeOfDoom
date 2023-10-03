203f5a94e2c314eefebc027fecd7843ccbd80cce
Vaughn Malkin <vaughnmalkin@gmail.com>
2023-10-03 14:22:03 +1300

    added scratch file

--- CoronalHoleSuite_v4/test.py
+++ CoronalHoleSuite_v4/test.py
@@ -1,6 +1,6 @@
-data = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+data = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17, 18, 19 ,20, 21]
 
-step = 5
+step = 6
 lower = 0
 upper = step
 step_multiple = 1
@@ -9,16 +9,19 @@ tmp = []
 
 for i in range(0, len(data)):
     if i >= lower:
-        if i <= upper:
+        if i < upper:
             tmp.append(data[i])
 
-    if i > upper:
+    if i >= upper:
         step_multiple = step_multiple + 1
         lower = upper
         upper = step_multiple * step
         returnlist.append(tmp)
         tmp = []
         tmp.append(data[i])
+
+    if i == (len(data) - 1):
+        returnlist.append(tmp)
 
 
 print(returnlist)
