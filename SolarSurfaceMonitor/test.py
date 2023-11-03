118be76b12881b8e3af2dfd3a4a0f82ba680701a
vmalkin <vaughnmalkin@gmail.com>
2023-11-04 12:51:23 +1300

    create combined image diffs analyser

--- SolarSurfaceMonitor/test.py
+++ SolarSurfaceMonitor/test.py
@@ -1,29 +1,33 @@
-# use "pip install opencv-python" if cv2 is not found
-import cv2
+import numpy as np
+import os
 import glob
+import time
+import datetime
+from calendar import timegm
+import cv2
+import mgr_mp4 as make_anim
 
-# Get a list of PNG images on the "test_images" folder
-images = glob.glob("combined/*.png")
-# You can also use the following two lines to get PNG files in a folder.
-# import os
-# images = [os.path.join("../test_images", file) for file in os.listdir("../test_images") if file.endswith(".png")]
-# Sort images by name. Optional step.
-images = sorted(images)
-# Define codec and create a VideoWriter object
-# cv2.VideoWriter_fourcc(*"mp4v") or cv2.VideoWriter_fourcc("m", "p", "4", "v")
-fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-video = cv2.VideoWriter(
-    filename="output.mp4", fourcc=fourcc, fps=30.0, frameSize=(430, 430)
-)
+# file path seperator / or \ ???
+pathsep = os.sep
 
-# Read each image and write it to the video
-for image in images:
-    # read the image using OpenCV
-    frame = cv2.imread(image)
-    # Optional step to resize the input image to the dimension stated in the
-    # VideoWriter object above
-    frame = cv2.resize(frame, dsize=(430, 430))
-    video.write(frame)
+def local_file_list_build(directory):
+    # Builds and returns a list of files contained in the directory.
+    # List is sorted into A --> Z order
+    dirlisting = []
+    path = directory + pathsep + "*.*"
+    for name in glob.glob(path):
+        name = os.path.normpath(name)
+        # seperator = os.path.sep
+        # n = name.split(seperator)
+        # nn = n[1]
+        dirlisting.append(name)
+    dirlisting.sort()
+    return dirlisting
 
-# Exit the video writer
-video.release()
+
+
+folder = 'diffs_g'
+img_files = local_file_list_build(folder)
+# a day is roughly 100 images
+img_files = img_files[-360:]
+make_anim.wrapper(img_files, 'diffs_191A')
