5c3739c71059ed6682cf2904837431857129fa68
vmalkin <vaughnmalkin@gmail.com>
2023-11-20 20:43:23 +1300

    multicolour difference images and animation

--- SolarSurfaceMonitor/main.py
+++ SolarSurfaceMonitor/main.py
@@ -6,6 +6,7 @@ import mgr_diffs_2 as diffs
 import mgr_mp4 as make_anim
 import mgr_gif as make_gif
 import mgr_multicolour_v2 as multicolour
+import mgr_multicolour_diff as multidiff
 
 suvidata = {
     '171': {
@@ -160,8 +161,36 @@ if __name__ == '__main__':
         multicolour.wrapper(multifilelist, 'combined')
 
         # create multispectral difference images
+        # Files for each datetime, regardless of the wavelength, have the same name
+        files_blue = local_file_list_build('diffs_b')
+        files_blue = files_blue[-360:]
+        files_green = local_file_list_build('diffs_g')
+        files_green = files_green[-360:]
+        files_red = local_file_list_build('diffs_r')
+        files_red = files_red[-360:]
 
+        multifilelist = []
+        for file_b in files_blue:
+            tmp = []
+            tmp.append(file_b)
+            b = file_b.split(pathsep)
+            start_b = b[1]
 
+            for file_g in files_green:
+                g = file_g.split(pathsep)
+                start_g = g[1]
+                if start_g == start_b:
+                    tmp.append(file_g)
+
+            for file_r in files_red:
+                r = file_r.split(pathsep)
+                start_r = r[1]
+                if start_r == start_b:
+                    tmp.append(file_r)
+            if len(tmp) == 3:
+                multifilelist.append(tmp)
+
+        multidiff.wrapper(multifilelist, 'combined_diffs')
 
         # Make animations
         folder = 'diffs_g'
@@ -209,6 +238,12 @@ if __name__ == '__main__':
         # a day is roughly 360 images
         img_files = img_files[-360:]
         make_anim.wrapper(img_files, '3_colour')
+
+        folder = 'combined_diffs'
+        img_files = local_file_list_build(folder)
+        # a day is roughly 360 images
+        img_files = img_files[-360:]
+        make_anim.wrapper(img_files, '3_clr_diffs')
 
         # folder = 'combined_diffs'
         # img_files = local_file_list_build(folder)
---
+++ SolarSurfaceMonitor/mgr_multicolour_diff.py
@@ -0,0 +1,78 @@
+import os
+import cv2
+import datetime
+import time
+from calendar import timegm
+import re
+
+pathsep = os.sep
+
+
+def create_label(image, timestamp):
+    # width, height, channels = image.shape
+    width, height, depth = image.shape
+    label_height = int(height / 10)
+    font_height = int(height / 12)
+    cv2.rectangle(image, (0,0), (width,label_height), (0, 0, 0), -1)
+    cv2.rectangle(image, (0, height - label_height), (width, height), (0, 0, 0), -1)
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_size = 1
+    # colours are blue, green, red in opencv
+    font_color = (255, 255, 255)
+    font_thickness = 1
+    label0 = "GOES SUVI 171A, 195A, 284A multispectral image."
+    label1 = "Image time: " + timestamp
+    label2 = "Images courtesy of NOAA. Processed at DunedinAurora.NZ"
+    cv2.putText(image, label0, (0, 50), font, font_size, font_color, font_thickness, cv2.LINE_AA)
+    cv2.putText(image, label1, (0, 100), font, font_size, font_color, font_thickness, cv2.LINE_AA)
+    cv2.putText(image, label2, (0, height - 80), font, font_size, font_color, font_thickness, cv2.LINE_AA)
+    return image
+
+
+def posix2utc(posixtime, timeformat):
+    # '%Y-%m-%d %H:%M'
+    utctime = datetime.datetime.utcfromtimestamp(int(posixtime)).strftime(timeformat)
+    return utctime
+
+
+def utc2posix(utcstring, timeformat):
+    utc_time = time.strptime(utcstring, timeformat)
+    epoch_time = timegm(utc_time)
+    return epoch_time
+
+
+
+
+def wrapper(multifilelist, save_folder):
+    print('*** BEGIN multicolour diffs processing ', save_folder)
+
+    if os.path.exists(save_folder) is False:
+        os.makedirs(save_folder)
+
+    for item in multifilelist:
+        files = item
+        # print(files)
+        if len(files) == 3:
+            # splitvalue = '_' + '|' +  str(pathsep)
+            # print(splitvalue)
+
+            t = files[0].split(pathsep)
+
+            tt = t[1].split('_')
+            timestamp = tt[0]
+            filename = timestamp + '_clr.png'
+
+            timestamp = utc2posix(timestamp, '%Y%m%dT%H%M%S')
+            timestamp = posix2utc(timestamp, '%Y-%m-%d %H:%M')
+
+            b = cv2.imread(files[0], 0)
+            r = cv2.imread(files[1], 0)
+            g = cv2.imread(files[2], 0)
+            colour_img = cv2.merge([b, g, r])
+            colour_img = create_label(colour_img, timestamp)
+            fc = save_folder + pathsep + str(filename)
+            cv2.imwrite(fc, colour_img)
+
+
+    print('*** END multicolour diffs processing')
