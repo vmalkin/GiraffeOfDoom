474464da1c07dd0bae6b0f2a6611645ce8910c80
vmalkin <vaughnmalkin@gmail.com>
2023-11-24 17:02:22 +1300

    experimenting with video codecs for web

--- SolarSurfaceMonitor/mgr_mp4.py
+++ SolarSurfaceMonitor/mgr_mp4.py
@@ -14,15 +14,20 @@ def wrapper(filelist, name):
     #
     # width = j[0]
     # height = j[1]
-    # filename = name + ".mp4"
+    filename1 = name + ".mp4"
     filename = name + ".webm"
     # Define codec and create a VideoWriter object
     # cv2.VideoWriter_fourcc(*"mp4v") or cv2.VideoWriter_fourcc("m", "p", "4", "v")
-    # fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    fourcc1 = cv2.VideoWriter_fourcc(*"mp4v")
     fourcc = cv2.VideoWriter_fourcc('V', 'P', '8', '0')
     video = cv2.VideoWriter(
         # filename=filename, fourcc=fourcc, fps=10.0, frameSize=(width, height)
         filename=filename, fourcc=fourcc, fps=10.0, frameSize=(640, 640)
+    )
+
+    video1 = cv2.VideoWriter(
+        # filename=filename, fourcc=fourcc, fps=10.0, frameSize=(width, height)
+        filename=filename1, fourcc=fourcc1, fps=10.0, frameSize=(640, 640)
     )
 
     # Read each image and write it to the video
@@ -33,10 +38,12 @@ def wrapper(filelist, name):
         # VideoWriter object above
         try:
             frame = cv2.resize(frame, dsize=(640, 640))
+            video1.write(frame)
             video.write(frame)
         except cv2.error:
             print('!!! Unable to resize video frame')
 
     # Exit the video writer
+    video1.release()
     video.release()
     print('*** End movie creation: ', name)
