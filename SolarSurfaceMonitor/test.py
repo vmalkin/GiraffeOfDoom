edd8bb9814218a0a634845f2daeef30d74630dcd
vmalkin <vaughnmalkin@gmail.com>
2023-12-01 20:09:36 +1300

    bash script editing

--- SolarSurfaceMonitor/test.py
+++ SolarSurfaceMonitor/test.py
@@ -23,7 +23,7 @@ def getfilename(pathname):
     return pp[0]
 
 
-folder = 'diffs_g'
+folder = 'diffs_b'
 img_files = local_file_list_build(folder)
 # a day is roughly 360 images
 img_files = img_files[-360:]
@@ -33,30 +33,33 @@ for item in img_files:
     tmp = []
     img = cv2.imread(item)
     result = np.histogram(img, bins=5, range=(0, 256))
-    # result[0] is histogram, result[1] is datatype, result[2] are bin labels
+    # result[0] is histogram, result[1] are bin labels
     histgm = (result[0])
+
     tmp.append(getfilename(item))
-    tmp.append(histgm[0])
-    tmp.append(histgm[4])
+    for item in histgm:
+        tmp.append(item)
+    # tmp.append(histgm)
     returnarray.append(tmp)
+# print(returnarray)
 
-returnarray = np.array(returnarray)
-# returnarray = np.array(returnarray)
-
-# numpy.ndarray.astype
-pixels_black = returnarray[:,1]
-pixels_white = returnarray[:,2]
-
+# rr = np.array(returnarray)
+# dates = rr[:,1]
+# print(dates)
 # std_wh = np.std(pixels_white)
 # std_bl = np.std(pixels_black)
 # avg_wh = np.average(pixels_white)
 # avg_bl = np.average(pixels_black)
-# with open('histograms.csv', 'w') as h:
-#     for line in returnarray:
-#         d = line[0] + ',' + str(line[1]) + ',' + str(line[2]) + str(average1) + str(average2) + str(std1) + str(std2)
-#         d = line + '\n'
-#         h.write(str(d))
-# h.close()
+
+with open('histograms.csv', 'w') as h:
+    for line in returnarray:
+        t = ''
+        for item in line:
+            t = t + ',' + str(item)
+        # d = line[0] + ',' + str(line[1]) + ',' + str(line[2])
+        d = t + '\n'
+        h.write(str(d))
+h.close()
 
 
 
