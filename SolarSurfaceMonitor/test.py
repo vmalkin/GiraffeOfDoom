4ff79ec1a2ab4dd1e84382f0f17fa6be0598fc4d
Vaughn Malkin <vaughnmalkin@gmail.com>
2023-12-08 12:53:18 +1300

    refine histogram analysis to the disc itself, not the corona

--- SolarSurfaceMonitor/test.py
+++ SolarSurfaceMonitor/test.py
@@ -6,6 +6,22 @@ import numpy as np
 # file path seperator / or \ ???
 pathsep = os.sep
 
+
+def create_mask(image):
+    # Mask off the outer corona and only focus on the image of the sun
+    diameter_ratio = 380 / 1280
+    width, height, colourdepth = image.shape
+    solar_diameter = int(diameter_ratio * width)
+    mask = np.zeros((width, height), dtype=np.int8)
+    x_offset = 0
+    y_offset = 0
+    x_centre = int(width / 2) + x_offset
+    y_centre = int(height / 2) + y_offset
+    cv2.circle(mask, (x_centre, y_centre), solar_diameter, (255,255,255), -1)
+    image = cv2.bitwise_and(image, image, mask=mask)
+    return image
+
+
 def local_file_list_build(directory):
     # Builds and returns a list of files contained in the directory.
     # List is sorted into A --> Z order
@@ -32,6 +48,9 @@ returnarray = []
 for item in img_files:
     tmp = []
     img = cv2.imread(item)
+    # Mask off the outer corona - we're only interested in the solar disc
+    img = create_mask(img)
+
     result = np.histogram(img, bins=5, range=(0, 256))
     # result[0] is histogram, result[1] are bin labels
     histgm = (result[0])
@@ -42,7 +61,7 @@ for item in img_files:
     # for item in histgm:
     #     tmp.append(item)
     returnarray.append(tmp)
-# print(returnarray)
+
 
 px_white = []
 px_black = []
@@ -56,23 +75,21 @@ avg_white = np.average(px_white)
 std_white = np.std(px_white)
 avg_black = np.average(px_black)
 std_black = np.std(px_black)
-for i in range(0, len(dates)):
-    print(dates[i], px_white[i], px_black[i], avg_white, avg_black)
 
-# with open('histograms.csv', 'w') as h:
-#     for line in returnarray:
-#         t = ''
-#         for item in line:
-#             if len(t) == 0:
-#                 t = t + str(item)
-#             else:
-#                 t = t + ',' + str(item)
-#
-#         d = t + '\n'
-#         h.write(str(d))
-# h.close()
+with open('histograms.csv', 'w') as h:
+    for i in range(0, len(dates)):
+        dt = dates[i]
+        if px_white[i] > (avg_white + std_white):
+            cme_wh = 1
+        else:
+            cme_wh = 0
 
+        if px_black[i] > (avg_black + std_black):
+            cme_bl = 1
+        else:
+            cme_bl = 0
 
+        line = dt + ',' + str(cme_wh) + ',' + str(cme_bl) + '\n'
+        h.write(line)
+h.close()
 
-
-# make_anim.wrapper(img_files, '3_clr_diffs')
