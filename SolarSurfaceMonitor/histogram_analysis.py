39fbc7026a90f8ef7665d11d5da0ec5bca9ce5f3
Vaughn Malkin <vaughnmalkin@gmail.com>
2023-12-14 10:36:14 +1300

    refine histogram analysis to the disc itself, not the corona

--- SolarSurfaceMonitor/histogram_analysis.py
+++ SolarSurfaceMonitor/histogram_analysis.py
@@ -2,7 +2,7 @@ import glob
 import os
 import cv2
 import numpy as np
-
+from plotly import graph_objects as go
 # file path seperator / or \ ???
 pathsep = os.sep
 
@@ -39,45 +39,69 @@ def getfilename(pathname):
     pp = p[1].split('_')
     return pp[0]
 
+
+def plot(event_data):
+    fig = go.Figure()
+    for data in event_data:
+        # Each data is is an array [datetime, data1, data2]
+        dt = []
+        d0 = []
+        d1 = []
+        for entries in data:
+            dt.append(entries[0])
+            d0.append(entries[1])
+            d1.append(entries[2])
+        fig.add_bar(x=dt, y=d0)
+        fig.add_bar(x=dt, y=d1)
+        # Force y axis to show at least 10
+        fig.update_yaxes(range=[10, None])
+    fig.update_layout(barmode='group')
+    fig.show()
+
 def wrapper():
     print('*** Begin Histogram Analysis')
-    folder = 'diffs_g'
-    img_files = local_file_list_build(folder)
-    # a day is roughly 360 images
-    img_files = img_files[-360:]
 
-    returnarray = []
-    for item in img_files:
-        tmp = []
-        img = cv2.imread(item)
-        # Mask off the outer corona - we're only interested in the solar disc
-        img = create_mask(img)
+    # Supply a list of sub folders with diffs images in them to analyse
+    folders = ['diffs_g', 'diffs_b', 'diffs_r']
+    solar_surface_events = []
 
-        result = np.histogram(img, bins=5, range=(0, 256))
-        # result[0] is histogram, result[1] are bin labels
-        histgm = (result[0])
+    for folder in folders:
+        img_files = local_file_list_build(folder)
+        # a day is roughly 360 images
+        img_files = img_files[-360:]
 
-        tmp.append(getfilename(item))
-        tmp.append(histgm[0])
-        tmp.append(histgm[4])
-        # for item in histgm:
-        #     tmp.append(item)
-        returnarray.append(tmp)
+        returnarray = []
+        for item in img_files:
+            tmp = []
+            img = cv2.imread(item)
+            # Mask off the outer corona - we're only interested in the solar disc
+            img = create_mask(img)
 
-    px_white = []
-    px_black = []
-    dates = []
-    for item in returnarray:
-        dates.append(item[0])
-        px_white.append(item[1])
-        px_black.append(item[2])
+            result = np.histogram(img, bins=5, range=(0, 256))
+            # result[0] is histogram, result[1] are bin labels
+            histgm = (result[0])
 
-    avg_white = np.average(px_white)
-    std_white = np.std(px_white)
-    avg_black = np.average(px_black)
-    std_black = np.std(px_black)
+            tmp.append(getfilename(item))
+            tmp.append(histgm[0])
+            tmp.append(histgm[4])
+            # for item in histgm:
+            #     tmp.append(item)
+            returnarray.append(tmp)
 
-    with open('histograms.csv', 'w') as h:
+        px_white = []
+        px_black = []
+        dates = []
+        for item in returnarray:
+            dates.append(item[0])
+            px_white.append(item[1])
+            px_black.append(item[2])
+
+        avg_white = np.average(px_white)
+        std_white = np.std(px_white)
+        avg_black = np.average(px_black)
+        std_black = np.std(px_black)
+
+        events = []
         for i in range(0, len(dates)):
             dt = dates[i]
             # href=''
@@ -91,7 +115,9 @@ def wrapper():
             else:
                 cme_bl = 0
 
-            line = dt + ',' + str(cme_wh) + ',' + str(cme_bl) + '\n'
-            h.write(line)
-    h.close()
+            line = [dt, cme_wh, cme_bl]
+            events.append(line)
+        solar_surface_events.append(events)
+
+    plot(solar_surface_events)
     print('*** End Histogram Analysis')
