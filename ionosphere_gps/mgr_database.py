48387be5a3076c833bcad07e3df83ab6d72a4b47
vmalkin <vaughnmalkin@gmail.com>
2024-02-26 19:16:41 +1300

    experimenting with SNR plots

--- ionosphere_gps/mgr_database.py
+++ ionosphere_gps/mgr_database.py
@@ -7,61 +7,61 @@ def db_create():
     # create database!
     gpsdb = sqlite3.connect(k.sat_database)
     db = gpsdb.cursor()
-    db.execute('drop table if exists constellation;')
-    db.execute('drop table if exists satellites;')
+    # db.execute('drop table if exists constellation;')
+    # db.execute('drop table if exists satellites;')
     db.execute('drop table if exists observations;')
-    db.execute('drop table if exists gga;')
+    # db.execute('drop table if exists gga;')
 
-    db.execute('create table constellation ('
-               'constellation_id text primary key'
-               ');')
-
-    db.execute('create table satellite('
-               'sat_id text primary key,'
-               'constellation_id text,'
-               'foreign key (constellation_id) references constellation(constellation_id)'
-               ');')
+    # db.execute('create table constellation ('
+    #            'constellation_id text primary key'
+    #            ');')
+    #
+    # db.execute('create table satellite('
+    #            'sat_id text primary key,'
+    #            'constellation_id text,'
+    #            'foreign key (constellation_id) references constellation(constellation_id)'
+    #            ');')
 
     db.execute('create table observations ('
+               'constellation text,'
                'sat_id text,'
                'posixtime integer,'
                'alt integer,'
                'az integer,'
-               'snr integer,'
-               'foreign key (sat_id) references satellite(sat_id)'
+               'snr integer'
                ');')
 
-    db.execute('create table gga ('
-               'constellation_id text,'
-               'posixtime integer,'
-               'lat real,'
-               'long real,'
-               'sats_used integer,'
-               'hdop real,'
-               'foreign key (constellation_id) references constellation(constellation_id)'
-               ');')
+    # db.execute('create table gga ('
+    #            'constellation_id text,'
+    #            'posixtime integer,'
+    #            'lat real,'
+    #            'long real,'
+    #            'sats_used integer,'
+    #            'hdop real,'
+    #            'foreign key (constellation_id) references constellation(constellation_id)'
+    #            ');')
 
     gpsdb.commit()
     db.close()
 
 
-def db_initialise():
-    # populate database with initial values
-    gpsdb = sqlite3.connect(k.sat_database)
-    db = gpsdb.cursor()
-
-    values = ['gps']
-    db.execute('insert into constellation(constellation_id) values (?);', values)
-
-    # for GPS satellites, usually only numbered 1 to 32
-    for i in range(1, 33):
-        id_num = convert_sat_id(i, 'GP')
-        values = [id_num, 'gps']
-        db.execute('insert into satellite(sat_id, constellation_id) values (?, ?);', values)
-    # for GLONASS satellites?
-
-    gpsdb.commit()
-    db.close()
+# def db_initialise():
+#     # populate database with initial values
+#     gpsdb = sqlite3.connect(k.sat_database)
+#     db = gpsdb.cursor()
+#
+#     values = ['gps']
+#     db.execute('insert into constellation(constellation_id) values (?);', values)
+#
+#     # for GPS satellites, usually only numbered 1 to 32
+#     for i in range(1, 33):
+#         id_num = convert_sat_id(i, 'GP')
+#         values = [id_num, 'gps']
+#         db.execute('insert into satellite(sat_id, constellation_id) values (?, ?);', values)
+#     # for GLONASS satellites?
+#
+#     gpsdb.commit()
+#     db.close()
 
 
 def db_gpgsv_add(gsvdata):
@@ -77,8 +77,8 @@ def db_gpgsv_add(gsvdata):
         az = item[3]
         snr = item[4]
         values = [sat_id, posixtime, alt, az, snr]
-        db.execute('insert into observations(sat_id, posixtime, alt, az, snr) '
-                   'values (?, ?, ?, ?, ?);', values)
+        db.execute('insert into observations(constellation, sat_id, posixtime, alt, az, snr) '
+                   'values (?, ?, ?, ?, ?, ?);', values)
     gpsdb.commit()
     db.close()
 
