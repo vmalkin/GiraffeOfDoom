9c079b1ba05542bef955e1d38aa004d1f5ded6fa
vmalkin <vaughnmalkin@gmail.com>
2024-03-10 21:27:27 +1300

    refactor in goes secondary data

--- helios_v1.0/process_histogram_analysis.py
+++ helios_v1.0/process_histogram_analysis.py
@@ -86,157 +86,218 @@ if __name__ == '__main__':
     print('*** Begin Histogram Analysis')
 
     # Supply a list of sub folders with diffs images in them to analyse
-    data = global_config.goes_dict
+    goes_dict = global_config.goes_dict
+    for sat in goes_dict:
+        solar_surface_events = []
+        for key in goes_dict[sat]['wavelengths']:
+            diffs = goes_dict[sat]['wavelengths'][key]['diffs']
+            img_files = local_file_list_build(diffs)
+            # a day is roughly 360 images
+            img_files = img_files[-360:]
 
+            returnarray = []
+            for item in img_files:
+                tmp = []
+                img = cv2.imread(item)
+                # Mask off the outer corona - we're only interested in the solar disc
+                img = create_mask(img)
+                # CReate a histogram of the image being processed
+                result = np.histogram(img, bins=5, range=(0, 256))
+                # result[0] is histogram, result[1] are bin labels
+                histgm = (result[0])
+                tmp.append(getfilename(item))
+                tmp.append(histgm[0])
+                tmp.append(histgm[4])
+                # for item in histgm:
+                #     tmp.append(item)
+                returnarray.append(tmp)
 
+            px_white = []
+            px_black = []
+            dates = []
+            for item in returnarray:
+                dates.append(item[0])
+                px_white.append(item[1])
+                px_black.append(item[2])
 
+            # Get simple statistics of the average, and standard deviations for all-black and all-white
+            # pixels for the current day. Use this to determine of any single image is above average, this will
+            # be our simple indicator of rapid change in the image caused by CME or similar events on the sun's surface
+            # It is possible that we might need a better treatment of this? We might want to store STD and AVG over
+            # a period of time and use the median values of those to evaluate individual images?
+            avg_white = np.average(px_white)
+            std_white = np.std(px_white)
+            avg_black = np.average(px_black)
+            std_black = np.std(px_black)
 
-    folder = 2
-    folders_x = [data[0][folder], data[1][folder], data[2][folder]]
-    folders_y = [data[3][folder], data[4][folder], data[5][folder]]
+            events = []
+            for i in range(0, len(dates)):
+                dt = dates[i]
+                # href=''
+                if px_white[i] > (avg_white + std_white):
+                    cme_wh = round(((px_white[i] - avg_white) / std_white), 3)
+                else:
+                    cme_wh = 0
 
-    solar_surface_events_x = []
-    solar_surface_events_y = []
+                if px_black[i] > (avg_black + std_black):
+                    cme_bl = round(((px_black[i] - avg_black) / std_black), 3)
+                else:
+                    cme_bl = 0
 
-    for folder in folders_x:
-        img_files = local_file_list_build(folder)
-        # a day is roughly 360 images
-        img_files = img_files[-360:]
+                line = [dt, cme_wh, cme_bl]
+                events.append(line)
+            solar_surface_events.append(events)
 
-        returnarray = []
-        for item in img_files:
-            tmp = []
-            img = cv2.imread(item)
-            # Mask off the outer corona - we're only interested in the solar disc
-            img = create_mask(img)
+        plot(solar_surface_events, 'GOES Primary')
+        print('*** End Histogram Analysis')
 
-            # cv2.imshow('image', img)
-            # # Waits for a keystroke
-            # cv2.waitKey(0)
-            # # Destroys all the windows created
-            # cv2.destroyAllwindows()
-
-            # CReate a histogram of the image being processed
-            result = np.histogram(img, bins=5, range=(0, 256))
-
-            # result[0] is histogram, result[1] are bin labels
-            histgm = (result[0])
-
-            # CReate an array of the image file name, and the count of all-black and all-white pixels
-
-            tmp.append(getfilename(item))
-            tmp.append(histgm[0])
-            tmp.append(histgm[4])
-            # for item in histgm:
-            #     tmp.append(item)
-            returnarray.append(tmp)
-
-        px_white = []
-        px_black = []
-        dates = []
-        for item in returnarray:
-            dates.append(item[0])
-            px_white.append(item[1])
-            px_black.append(item[2])
-
-        # Get simple statistics of the average, and standard deviations for all-black and all-white
-        # pixels for the current day. Use this to determine of any single image is above average, this will
-        # be our simple indicator of rapid change in the image caused by CME or similar events on the sun's surface
-        # It is possible that we might need a better treatment of this? We might want to store STD and AVG over
-        # a period of time and use the median values of those to evaluate individual images?
-        avg_white = np.average(px_white)
-        std_white = np.std(px_white)
-        avg_black = np.average(px_black)
-        std_black = np.std(px_black)
-
-        events = []
-        for i in range(0, len(dates)):
-            dt = dates[i]
-            # href=''
-            if px_white[i] > (avg_white + std_white):
-                cme_wh = round(((px_white[i] - avg_white) / std_white), 3)
-            else:
-                cme_wh = 0
-
-            if px_black[i] > (avg_black + std_black):
-                cme_bl = round(((px_black[i] - avg_black) / std_black), 3)
-            else:
-                cme_bl = 0
-
-            line = [dt, cme_wh, cme_bl]
-            events.append(line)
-        solar_surface_events_x.append(events)
-
-
-    for folder in folders_y:
-        img_files = local_file_list_build(folder)
-        # a day is roughly 360 images
-        img_files = img_files[-360:]
-
-        returnarray = []
-        for item in img_files:
-            tmp = []
-            img = cv2.imread(item)
-            # Mask off the outer corona - we're only interested in the solar disc
-            img = create_mask(img)
-
-            # cv2.imshow('image', img)
-            # # Waits for a keystroke
-            # cv2.waitKey(0)
-            # # Destroys all the windows created
-            # cv2.destroyAllwindows()
-
-            # CReate a histogram of the image being processed
-            result = np.histogram(img, bins=5, range=(0, 256))
-
-            # result[0] is histogram, result[1] are bin labels
-            histgm = (result[0])
-
-            # CReate an array of the image file name, and the count of all-black and all-white pixels
-
-            tmp.append(getfilename(item))
-            tmp.append(histgm[0])
-            tmp.append(histgm[4])
-            # for item in histgm:
-            #     tmp.append(item)
-            returnarray.append(tmp)
-
-        px_white = []
-        px_black = []
-        dates = []
-        for item in returnarray:
-            dates.append(item[0])
-            px_white.append(item[1])
-            px_black.append(item[2])
-
-        # Get simple statistics of the average, and standard deviations for all-black and all-white
-        # pixels for the current day. Use this to determine of any single image is above average, this will
-        # be our simple indicator of rapid change in the image caused by CME or similar events on the sun's surface
-        # It is possible that we might need a better treatment of this? We might want to store STD and AVG over
-        # a period of time and use the median values of those to evaluate individual images?
-        avg_white = np.average(px_white)
-        std_white = np.std(px_white)
-        avg_black = np.average(px_black)
-        std_black = np.std(px_black)
-
-        events = []
-        for i in range(0, len(dates)):
-            dt = dates[i]
-            # href=''
-            if px_white[i] > (avg_white + std_white):
-                cme_wh = round(((px_white[i] - avg_white) / std_white), 3)
-            else:
-                cme_wh = 0
-
-            if px_black[i] > (avg_black + std_black):
-                cme_bl = round(((px_black[i] - avg_black) / std_black), 3)
-            else:
-                cme_bl = 0
-
-            line = [dt, cme_wh, cme_bl]
-            events.append(line)
-        solar_surface_events_y.append(events)
-
-    plot(solar_surface_events_x, 'GOES Primary')
-    plot(solar_surface_events_y, 'GOES Secondary')
-    print('*** End Histogram Analysis')
+    #
+    # folder = 2
+    # folders_x = [data[0][folder], data[1][folder], data[2][folder]]
+    # folders_y = [data[3][folder], data[4][folder], data[5][folder]]
+    #
+    # solar_surface_events_x = []
+    # solar_surface_events_y = []
+    #
+    # for folder in folders_x:
+    #     img_files = local_file_list_build(folder)
+    #     # a day is roughly 360 images
+    #     img_files = img_files[-360:]
+    #
+    #     returnarray = []
+    #     for item in img_files:
+    #         tmp = []
+    #         img = cv2.imread(item)
+    #         # Mask off the outer corona - we're only interested in the solar disc
+    #         img = create_mask(img)
+    #
+    #         # cv2.imshow('image', img)
+    #         # # Waits for a keystroke
+    #         # cv2.waitKey(0)
+    #         # # Destroys all the windows created
+    #         # cv2.destroyAllwindows()
+    #
+    #         # CReate a histogram of the image being processed
+    #         result = np.histogram(img, bins=5, range=(0, 256))
+    #
+    #         # result[0] is histogram, result[1] are bin labels
+    #         histgm = (result[0])
+    #
+    #         # CReate an array of the image file name, and the count of all-black and all-white pixels
+    #
+    #         tmp.append(getfilename(item))
+    #         tmp.append(histgm[0])
+    #         tmp.append(histgm[4])
+    #         # for item in histgm:
+    #         #     tmp.append(item)
+    #         returnarray.append(tmp)
+    #
+    #     px_white = []
+    #     px_black = []
+    #     dates = []
+    #     for item in returnarray:
+    #         dates.append(item[0])
+    #         px_white.append(item[1])
+    #         px_black.append(item[2])
+    #
+    #     # Get simple statistics of the average, and standard deviations for all-black and all-white
+    #     # pixels for the current day. Use this to determine of any single image is above average, this will
+    #     # be our simple indicator of rapid change in the image caused by CME or similar events on the sun's surface
+    #     # It is possible that we might need a better treatment of this? We might want to store STD and AVG over
+    #     # a period of time and use the median values of those to evaluate individual images?
+    #     avg_white = np.average(px_white)
+    #     std_white = np.std(px_white)
+    #     avg_black = np.average(px_black)
+    #     std_black = np.std(px_black)
+    #
+    #     events = []
+    #     for i in range(0, len(dates)):
+    #         dt = dates[i]
+    #         # href=''
+    #         if px_white[i] > (avg_white + std_white):
+    #             cme_wh = round(((px_white[i] - avg_white) / std_white), 3)
+    #         else:
+    #             cme_wh = 0
+    #
+    #         if px_black[i] > (avg_black + std_black):
+    #             cme_bl = round(((px_black[i] - avg_black) / std_black), 3)
+    #         else:
+    #             cme_bl = 0
+    #
+    #         line = [dt, cme_wh, cme_bl]
+    #         events.append(line)
+    #     solar_surface_events_x.append(events)
+    #
+    #
+    # for folder in folders_y:
+    #     img_files = local_file_list_build(folder)
+    #     # a day is roughly 360 images
+    #     img_files = img_files[-360:]
+    #
+    #     returnarray = []
+    #     for item in img_files:
+    #         tmp = []
+    #         img = cv2.imread(item)
+    #         # Mask off the outer corona - we're only interested in the solar disc
+    #         img = create_mask(img)
+    #
+    #         # cv2.imshow('image', img)
+    #         # # Waits for a keystroke
+    #         # cv2.waitKey(0)
+    #         # # Destroys all the windows created
+    #         # cv2.destroyAllwindows()
+    #
+    #         # CReate a histogram of the image being processed
+    #         result = np.histogram(img, bins=5, range=(0, 256))
+    #
+    #         # result[0] is histogram, result[1] are bin labels
+    #         histgm = (result[0])
+    #
+    #         # CReate an array of the image file name, and the count of all-black and all-white pixels
+    #
+    #         tmp.append(getfilename(item))
+    #         tmp.append(histgm[0])
+    #         tmp.append(histgm[4])
+    #         # for item in histgm:
+    #         #     tmp.append(item)
+    #         returnarray.append(tmp)
+    #
+    #     px_white = []
+    #     px_black = []
+    #     dates = []
+    #     for item in returnarray:
+    #         dates.append(item[0])
+    #         px_white.append(item[1])
+    #         px_black.append(item[2])
+    #
+    #     # Get simple statistics of the average, and standard deviations for all-black and all-white
+    #     # pixels for the current day. Use this to determine of any single image is above average, this will
+    #     # be our simple indicator of rapid change in the image caused by CME or similar events on the sun's surface
+    #     # It is possible that we might need a better treatment of this? We might want to store STD and AVG over
+    #     # a period of time and use the median values of those to evaluate individual images?
+    #     avg_white = np.average(px_white)
+    #     std_white = np.std(px_white)
+    #     avg_black = np.average(px_black)
+    #     std_black = np.std(px_black)
+    #
+    #     events = []
+    #     for i in range(0, len(dates)):
+    #         dt = dates[i]
+    #         # href=''
+    #         if px_white[i] > (avg_white + std_white):
+    #             cme_wh = round(((px_white[i] - avg_white) / std_white), 3)
+    #         else:
+    #             cme_wh = 0
+    #
+    #         if px_black[i] > (avg_black + std_black):
+    #             cme_bl = round(((px_black[i] - avg_black) / std_black), 3)
+    #         else:
+    #             cme_bl = 0
+    #
+    #         line = [dt, cme_wh, cme_bl]
+    #         events.append(line)
+    #     solar_surface_events_y.append(events)
+    #
+    # plot(solar_surface_events_x, 'GOES Primary')
+    # plot(solar_surface_events_y, 'GOES Secondary')
+    # print('*** End Histogram Analysis')
--- helios_v1.0/test.py
+++ helios_v1.0/test.py
@@ -5,9 +5,8 @@ from global_config import goes_dict
 #     print(goes_dict[sat]['false_diffs'])
 
 for sat in goes_dict:
-    # for key in goes_dict[sat]['wavelengths']:
-    #     # # print(goes_dict[sat]['wavelengths'][key]['url'])
-    #     # diffs = goes_dict[sat]['wavelengths'][key]['diffs']
-    #     # store = goes_dict[sat]['wavelengths'][key]['store']
-    #     # print( sat, key, diffs, store)
-    print(goes_dict[sat]['false_colour'])
+    for key in goes_dict[sat]['wavelengths']:
+        diffs = goes_dict[sat]['wavelengths'][key]['diffs']
+        store = goes_dict[sat]['wavelengths'][key]['store']
+        print( sat, key, diffs, store)
+    # print(goes_dict[sat]['false_colour'])
