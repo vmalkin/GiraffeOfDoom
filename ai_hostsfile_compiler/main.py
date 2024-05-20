bf6bdb145fc3e9a2c785490a2a80c426ca3de1c8
Vaughn Malkin <vaughnmalkin@gmail.com>
2024-05-21 11:55:18 +1200

    add get_data method

--- ai_hostsfile_compiler/main.py
+++ ai_hostsfile_compiler/main.py
@@ -1,7 +1,7 @@
 import requests
 
-
-def do_get_data(self):
+datasource = 'https://github.com/mahseema/awesome-ai-tools'
+def do_get_data(datasource):
     """Get data"""
     result = "fail"
     try:
@@ -14,31 +14,33 @@ def do_get_data(self):
         webdata = webdata.content.decode('utf-8')
         webdata = webdata.split('\n')
 
-        # the first line is just header data
-        webdata.pop(0)
+        # # the first line is just header data
+        # webdata.pop(0)
         # convert datetime to posix values
         for row in webdata:
-            try:
-                r = row.split(',')
-                # print(row)
-                try:
-                    posix_dt = self.utc2posix(r[0])
-                    value = round(float(r[1]), 3)
-                    dp = str(posix_dt) + "," + str(value)
-                    self.mag_data.append(dp)
-                except Exception:
-                    print("WARNING: unable to parse time")
-                    logging.warning(station_id + " WARNING: unable to parse a time value: " + str(posix_dt))
-            except IndexError:
-                logging.warning(station_id + " WARNING: list index out of range")
-                result = "fail"
-    else:
-        logging.error(station_id + " ERROR: Could not get data from URL")
-        result = "fail"
+            print(row)
+            # try:
+            #     r = row.split(',')
+            #     # print(row)
+            #     try:
+            #         posix_dt = self.utc2posix(r[0])
+            #         value = round(float(r[1]), 3)
+            #         dp = str(posix_dt) + "," + str(value)
+            #         self.mag_data.append(dp)
+            #     except Exception:
+            #         print("WARNING: unable to parse time")
+            #         logging.warning(station_id + " WARNING: unable to parse a time value: " + str(posix_dt))
+            # except IndexError:
+            #     logging.warning(station_id + " WARNING: list index out of range")
+            #     result = "fail"
+    # else:
+    #     logging.error(station_id + " ERROR: Could not get data from URL")
+    #     result = "fail"
 
-    if len(self.mag_data) > 2:
-        result = "success"
-    # print(self.mag_data)
+    # if len(self.mag_data) > 2:
+    #     result = "success"
+    # # print(self.mag_data)
     return result
 
-if __name__ ==
+if __name__ == "__main__":
+    do_get_data(datasource)
