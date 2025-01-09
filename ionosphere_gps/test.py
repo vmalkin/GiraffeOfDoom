6b682f185ef59f424f96f65c84ac073da5e4cd94
vmalkin <vaughnmalkin@gmail.com>
2025-01-09 21:55:06 +1300

    experimenting with SNR plots

--- ionosphere_gps/test.py
+++ ionosphere_gps/test.py
@@ -10,8 +10,8 @@ now = int(time.time())
 print('Plot last 24 hour GPS tracks')
 last_6_hours = now - (60 * 60 * 24)
 query_result = mgr_database.db_get_gsv(last_6_hours, 1)
-# savefile = k.dir_images + os.sep + "simple_altaz.jpg"
-# mgr_matplot.plot_alt_az(query_result, savefile)
+savefile = k.dir_images + os.sep + "simple_altaz.jpg"
+mgr_matplot.plot_alt_az(query_result, savefile)
 
 savefile = k.dir_images + os.sep + "simple_polar.jpg"
 mgr_matplot.plot_polar_positions(query_result, savefile)
