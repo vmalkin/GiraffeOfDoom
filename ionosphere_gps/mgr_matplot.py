ac9c618b52db984cfca5af567f9e2dfbe26f54db
vmalkin <vaughnmalkin@gmail.com>
2025-01-11 10:44:32 +1300

    experimenting with SNR plots

--- ionosphere_gps/mgr_matplot.py
+++ ionosphere_gps/mgr_matplot.py
@@ -1,46 +1,91 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-plt.style.use('_mpl-gallery')
 
-def plot_alt_az(queryresult, savefile):
-    fig, ax = plt.subplots(layout="constrained", figsize=(9,3), dpi=140)
-    # datetime = []
-    altitude = []
-    azimuth = []
-    for item in queryresult:
-        # datetime.append(item[2])
-        alt = item[3]
-        azi = item[4]
 
-        if item[3] == '':
-            alt = np.nan
-        else:
-            alt = int(alt)
-            # print("Null altitude value")
-        if item[4] == '':
-            azi = np.nan
-        else:
-            azi = int(azi)
-            # print("Null azimuth value")
+# def plot_alt_az(queryresult, savefile):
+#     fig, ax = plt.subplots(layout="constrained", figsize=(15,5), dpi=140)
+#     # datetime = []
+#     altitude = []
+#     azimuth = []
+#     signalnoise = []
+#     for item in queryresult:
+#         # datetime.append(item[2])
+#         alt = item[3]
+#         azi = item[4]
+#         snr = item[5]
+#
+#         if item[3] == '':
+#             alt = np.nan
+#         else:
+#             alt = int(alt)
+#             # print("Null altitude value")
+#         if item[4] == '':
+#             azi = np.nan
+#         else:
+#             azi = int(azi)
+#             # print("Null azimuth value")
+#         if item[5] == '':
+#             snr = np.nan
+#         else:
+#             snr = int(snr)
+#
+#         altitude.append(alt)
+#         azimuth.append(azi)
+#         signalnoise.append(snr)
+#
+#     ax.scatter(azimuth, altitude, s=50, c=signalnoise, cmap='afmhot', alpha=0.5)
+#     ax.set(xlim=(0, 360), ylim=(0, 90))
+#     ax.set_title("GPS Tracks - 25 hours")
+#     plt.savefig(savefile)
+#     # plt.show()
+#
+# def plot_polar_positions(queryresult, savefile):
+#     # Polar plot Theta axis is in radians!
+#     altitude = []
+#     azimuth = []
+#     for item in queryresult:
+#         # datetime.append(item[2])
+#         alt = item[3]
+#         azi = item[4]
+#
+#         if item[3] == '':
+#             alt = np.nan
+#         else:
+#             alt = int(alt)
+#             # print("Null altitude value")
+#         if item[4] == '':
+#             azi = np.nan
+#         else:
+#             azi = azi * (np.pi / 180)
+#             # print("Null azimuth value")
+#
+#         altitude.append(alt)
+#         azimuth.append(azi)
+#
+#     fig = plt.figure(figsize=(6, 6), dpi=140)
+#     ax = fig.add_subplot(projection='polar')
+#
+#     ax.scatter(azimuth, altitude, s=6, c="seagreen")
+#     ax.set_rmax(0)
+#     ax.set_rmin(90)
+#     ax.set_theta_zero_location("N")
+#     ax.set_theta_direction(-1)
+#     ax.set_title("GPS Tracks - 24 hours")
+#     # plt.show()
+#     plt.savefig(savefile)
 
-        altitude.append(alt)
-        azimuth.append(azi)
-
-    ax.scatter(azimuth, altitude, s=3, c="seagreen")
-    ax.set(xlim=(0, 360), ylim=(0, 90))
-    ax.set_title("GPS Tracks")
-    plt.savefig(savefile)
-    # plt.show()
-
-def plot_polar_positions(queryresult, savefile):
+def plot_polar_noise(queryresult, savefile):
+    # plt.style.use('_mpl-gallery')
     # Polar plot Theta axis is in radians!
     altitude = []
     azimuth = []
+    signalnoise = []
     for item in queryresult:
         # datetime.append(item[2])
         alt = item[3]
         azi = item[4]
+        snr = item[5]
 
         if item[3] == '':
             alt = np.nan
@@ -50,20 +95,28 @@ def plot_polar_positions(queryresult, savefile):
         if item[4] == '':
             azi = np.nan
         else:
+            # azimuths have to be in radians, even tho the graph shows them as degrees
             azi = azi * (np.pi / 180)
             # print("Null azimuth value")
+        if item[5] == '':
+            snr = np.nan
+        else:
+            snr = int(snr)
+            # print("Null altitude value")
 
         altitude.append(alt)
         azimuth.append(azi)
+        signalnoise.append(snr)
 
-    fig = plt.figure(figsize=(6, 6))
+    fig = plt.figure(layout="constrained", figsize=(6, 6), dpi=140)
     ax = fig.add_subplot(projection='polar')
 
-    ax.scatter(azimuth, altitude, s=3, c="seagreen")
+
+    ax.scatter(azimuth, altitude, s=10, c=signalnoise, cmap='Blues', alpha=0.5)
     ax.set_rmax(0)
     ax.set_rmin(90)
-    ax.set_theta_zero_location("N")
-    ax.set_theta_direction(-1)
-    ax.set_title("GPS Tracks")
+    ax.set_theta_zero_location("S")
+    ax.set_theta_direction(1)
+    ax.set_title("GPS SNR Tracks - 24 hours")
     # plt.show()
     plt.savefig(savefile)
--- ionosphere_gps/test.py
+++ ionosphere_gps/test.py
@@ -8,11 +8,14 @@ print("***************************** Start Query Processor")
 now = int(time.time())
 
 print('Plot last 24 hour GPS tracks')
-last_6_hours = now - (60 * 60 * 24)
-query_result = mgr_database.db_get_gsv(last_6_hours, 10)
-savefile = k.dir_images + os.sep + "simple_altaz.jpg"
-mgr_matplot.plot_alt_az(query_result, savefile)
+last_6_hours = now - (60 * 60 * 6)
+query_result = mgr_database.db_get_gsv(last_6_hours, 1)
+# savefile = k.dir_images + os.sep + "simple_altaz.png"
+# mgr_matplot.plot_alt_az(query_result, savefile)
+#
+# savefile = k.dir_images + os.sep + "simple_polar.png"
+# mgr_matplot.plot_polar_positions(query_result, savefile)
 
-savefile = k.dir_images + os.sep + "simple_polar.jpg"
-mgr_matplot.plot_polar_positions(query_result, savefile)
+savefile = k.dir_images + os.sep + "simple_polar_snr.png"
+mgr_matplot.plot_polar_noise(query_result, savefile)
 print("******************************* End Query Processor")
