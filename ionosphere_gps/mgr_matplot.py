63fca38b0e1a9afbdb806d17b2e46792d4ef62df
vmalkin <vaughnmalkin@gmail.com>
2025-01-11 10:54:57 +1300

    experimenting with SNR plots

--- ionosphere_gps/mgr_matplot.py
+++ ionosphere_gps/mgr_matplot.py
@@ -1,8 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-
 # def plot_alt_az(queryresult, savefile):
 #     fig, ax = plt.subplots(layout="constrained", figsize=(15,5), dpi=140)
 #     # datetime = []
@@ -39,41 +37,7 @@ import numpy as np
 #     ax.set_title("GPS Tracks - 25 hours")
 #     plt.savefig(savefile)
 #     # plt.show()
-#
-# def plot_polar_positions(queryresult, savefile):
-#     # Polar plot Theta axis is in radians!
-#     altitude = []
-#     azimuth = []
-#     for item in queryresult:
-#         # datetime.append(item[2])
-#         alt = item[3]
-#         azi = item[4]
-#
-#         if item[3] == '':
-#             alt = np.nan
-#         else:
-#             alt = int(alt)
-#             # print("Null altitude value")
-#         if item[4] == '':
-#             azi = np.nan
-#         else:
-#             azi = azi * (np.pi / 180)
-#             # print("Null azimuth value")
-#
-#         altitude.append(alt)
-#         azimuth.append(azi)
-#
-#     fig = plt.figure(figsize=(6, 6), dpi=140)
-#     ax = fig.add_subplot(projection='polar')
-#
-#     ax.scatter(azimuth, altitude, s=6, c="seagreen")
-#     ax.set_rmax(0)
-#     ax.set_rmin(90)
-#     ax.set_theta_zero_location("N")
-#     ax.set_theta_direction(-1)
-#     ax.set_title("GPS Tracks - 24 hours")
-#     # plt.show()
-#     plt.savefig(savefile)
+
 
 def plot_polar_noise(queryresult, savefile):
     # plt.style.use('_mpl-gallery')
