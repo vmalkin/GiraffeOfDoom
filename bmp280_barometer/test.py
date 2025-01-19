d21f19cbf36e3b91a26991c8747fab8bff9c0c32
vmalkin <vaughnmalkin@gmail.com>
2025-01-19 21:34:44 +1300

    Precision Barometer plotter

--- bmp280_barometer/test.py
+++ bmp280_barometer/test.py
@@ -5,12 +5,8 @@ import os
 import constants as k
 
 now = int(time.time())
-timeinterval = now - (60 * 60 * 24 * 1)
-query_result = mgr_database.db_get_gsv(timeinterval, 1)
-
-print("Plot avg SNR")
 timeinterval = now - (60 * 60 * 24)
-# queryresult = mgr_database.db_get_grouped_snr(timeinterval)
-savefile = k.dir_images + os.sep + "simple_time_snr.png"
-mgr_matplot.plot_polar_noise(query_result, savefile)
+queryresult = mgr_database.db_get_pressure(timeinterval)
+savefile = k.dir_images + os.sep + "pressure.png"
+mgr_matplot.plot_time_snr(queryresult, savefile)
 
