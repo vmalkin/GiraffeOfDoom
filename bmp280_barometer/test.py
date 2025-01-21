3a751c1bb6ca5895e5e2b0231a88ceea9c70f5ef
Vaughn Malkin <vaughnmalkin@gmail.com>
2025-01-21 13:59:23 +1300

    updated rolling average to a single pass instead of recursive loops!

--- bmp280_barometer/test.py
+++ bmp280_barometer/test.py
@@ -5,7 +5,7 @@ import os
 import constants as k
 
 now = int(time.time())
-timeinterval = now - (60 * 60 * 24 * 7)
+timeinterval = now - (60 * 60 * 24)
 queryresult = mgr_database.db_get_pressure(timeinterval)
 savefile = k.dir_images + os.sep + "long_term.png"
 mgr_matplot.plot_time_data(queryresult, savefile)
@@ -13,7 +13,7 @@ mgr_matplot.plot_time_data(queryresult, savefile)
 # savefile = k.dir_images + os.sep + "dxdt.png"
 # mgr_matplot.plot_time_dxdt(queryresult, savefile)
 
-# savefile = k.dir_images + os.sep + "detrended.png"
-# halfwindow = 60 * 60 * 2
-# mgr_matplot.plot_detrended(queryresult, halfwindow, savefile)
+savefile = k.dir_images + os.sep + "detrended.png"
+halfwindow = 60 * 5
+mgr_matplot.plot_detrended(queryresult, halfwindow, savefile)
 
