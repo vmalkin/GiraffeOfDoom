ec1fac3534d0a1a17073f14f1a5273269517eda1
Vaughn Malkin <vaughnmalkin@gmail.com>
2025-01-31 11:14:25 +1300

    adjusted tick spacing and arguments passed in to plotting functions to give more flexibility

--- bmp280_barometer/test.py
+++ bmp280_barometer/test.py
@@ -3,23 +3,16 @@ import mgr_matplot
 import time
 import os
 import constants as k
-import standard_stuff
 
+print("Plot pressure seven days")
 now = int(time.time())
 timeinterval = now - (60 * 60 * 24 * 7)
 queryresult = mgr_database.db_get_pressure(timeinterval)
 savefile = k.dir_images + os.sep + "7days_pressure.png"
-print("Plot pressure seven days")
 readings_per_minute = 1
 decimation = int(60 / readings_per_minute)
 readings_per_tick = readings_per_minute * 60 * 12
 mgr_matplot.plot_time_data(queryresult, decimation, readings_per_tick, "Seven Days",savefile)
-
-# print("Plot dx-dt seven days")
-# savefile = k.dir_images + os.sep + "7days_dxdt.png"
-# decimation = 60
-# hour_tick_interval = 3
-# mgr_matplot.plot_time_dxdt(queryresult, decimation, hour_tick_interval, "Seven Days dx-dt",savefile)
 
 print("Plot pressure one day")
 timeinterval = now - (60 * 60 * 24)
@@ -30,11 +23,11 @@ decimation = int(60 / readings_per_minute)
 readings_per_tick = readings_per_minute * 60 * 1
 mgr_matplot.plot_time_data(queryresult, decimation, readings_per_tick,"One Day", savefile)
 
+print("Plot standard deviation one day")
 readings_per_minute = 30
 decimation = int(60 / readings_per_minute)
 readings_per_tick = readings_per_minute * 60 * 1
 halfwindow = int(60 / decimation) * 5
-print("Plot standard deviation one day")
 savefile = k.dir_images + os.sep + "stats.png"
 mgr_matplot.plot_stats(queryresult, decimation, readings_per_tick, halfwindow, savefile)
 
