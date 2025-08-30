9506ddcff801e523e460facee70a1b5da4a900b6
vmalkin <vaughnmalkin@gmail.com>
2025-08-30 12:50:44 +1200

    adjusting plotter for new data format from database

--- pyDataLogger_2025_08/mgr_matplot.py
+++ pyDataLogger_2025_08/mgr_matplot.py
@@ -24,37 +24,37 @@ def plot_time_data(utcdates, maindata, readings_per_tick, ymin, ymax, texttitle,
     plt.close()
 
 
-def plot_hourly_array(utcdates, sensor_data, save_path):
-    # Data is about 5 readings per second. Calculate how many readings for an hour
-    window = 5 * 60 * 60
-    index_start = 0
-    index_end = window
-
-    for i in range(0, 50):
-        hour_data = sensor_data[index_start: index_end]
-        hour_utc = utcdates[index_start: index_end]
-        try:
-            plt.style.use('Solarize_Light2')
-            fig, ax = plt.subplots(layout="constrained", figsize=(16, 8), dpi=140)
-            ax.plot(hour_utc, hour_data, c=ink_colour, linewidth=1)
-            ax.xaxis.set_major_locator(ticker.MultipleLocator(3000))
-            ax.set_ylim([400, 500])
-            # ax.set_xlim([0, 0.3])
-            plt.xticks(rotation=45)
-
-            pt = time.time()
-            ut = standard_stuff.posix2utc(pt, '%Y-%m-%d %H:%M.%f')
-            texttitle = hour_data[0]
-            plot_title = str(texttitle) + " (Arbitrary Units) - " + ut
-            savefile = save_path + os.sep + f"{i:03}" + ".png"
-            ax.set_title(plot_title)
-            plt.savefig(savefile)
-            # increment the indices for slicing the array
-            index_start = index_end
-            index_end = index_end + window
-            plt.close()
-        except:
-            print("Finished")
+# def plot_hourly_array(utcdates, sensor_data, save_path):
+#     # Data is about 5 readings per second. Calculate how many readings for an hour
+#     window = 5 * 60 * 60
+#     index_start = 0
+#     index_end = window
+#
+#     for i in range(0, 50):
+#         hour_data = sensor_data[index_start: index_end]
+#         hour_utc = utcdates[index_start: index_end]
+#         try:
+#             plt.style.use('Solarize_Light2')
+#             fig, ax = plt.subplots(layout="constrained", figsize=(16, 8), dpi=140)
+#             ax.plot(hour_utc, hour_data, c=ink_colour, linewidth=1)
+#             ax.xaxis.set_major_locator(ticker.MultipleLocator(3000))
+#             ax.set_ylim([400, 500])
+#             # ax.set_xlim([0, 0.3])
+#             plt.xticks(rotation=45)
+#
+#             pt = time.time()
+#             ut = standard_stuff.posix2utc(pt, '%Y-%m-%d %H:%M.%f')
+#             texttitle = hour_data[0]
+#             plot_title = str(texttitle) + " (Arbitrary Units) - " + ut
+#             savefile = save_path + os.sep + f"{i:03}" + ".png"
+#             ax.set_title(plot_title)
+#             plt.savefig(savefile)
+#             # increment the indices for slicing the array
+#             index_start = index_end
+#             index_end = index_end + window
+#             plt.close()
+#         except:
+#             print("Finished")
 
 
 def plot_spectrum(data, datetimes, plotfrequency, savefile):
