ce311d29b7922023d71eff5e92d6133abfa5b22d
vmalkin <vaughnmalkin@gmail.com>
2025-09-05 16:49:34 +1200

    Fixing datetime bug in plots

--- pyDataLogger_2025_08/plot_seismos_v2.py
+++ pyDataLogger_2025_08/plot_seismos_v2.py
@@ -96,19 +96,19 @@ plot_press = plot_press[avgwindow:-avgwindow]
 ticks = 120
 ymin = 451.5
 ymax = 453
-df = "%d %H:%M"
+df = "%d  %H:%M"
 title = "Tiltmeter One Day"
 savefile = k.dir_images + os.sep + "one_day.png"
 mgr_matplot.plot_time_data(df, plot_utc, smoothe_seismo,  ticks,ymin, ymax, title, savefile)
 ymin = None
 ymax = None
-df = "%d %H:%M"
+df = "%d  %H:%M"
 title = "Pressure One Day"
 savefile = k.dir_images + os.sep + "one_press.png"
 mgr_matplot.plot_time_data(df, plot_utc, plot_press,  ticks,ymin, ymax, title, savefile)
 ymin = None
 ymax = None
-df = "%d %H:%M"
+df = "%d  %H:%M"
 title = "Temperature One Day"
 savefile = k.dir_images + os.sep + "one_temp.png"
 mgr_matplot.plot_time_data(df, plot_utc, plot_temp,  ticks,ymin, ymax, title, savefile)
@@ -158,27 +158,62 @@ plot_press = plot_press[avgwindow:-avgwindow]
 ticks = 240
 ymin = 451.5
 ymax = 453
-df = "%d %H:%M"
+df = "%d  %H:%M"
 title = "Tiltmeter One Week"
 savefile = k.dir_images + os.sep + "seven_day.png"
 mgr_matplot.plot_time_data(df, plot_utc, smoothe_seismo,  ticks,ymin, ymax, title, savefile)
 ymin = None
 ymax = None
-df = "%d %H:%M"
+df = "%d  %H:%M"
 title = "Pressure One Week"
 savefile = k.dir_images + os.sep + "seven_press.png"
 mgr_matplot.plot_time_data(df, plot_utc, plot_press,  ticks,ymin, ymax, title, savefile)
 ymin = None
 ymax = None
-df = "%d %H:%M"
+df = "%d  %H:%M"
 title = "Temperature One Week"
 savefile = k.dir_images + os.sep + "seven_temp.png"
 mgr_matplot.plot_time_data(df, plot_utc, plot_temp,  ticks,ymin, ymax, title, savefile)
 
-# df = "%d %H:%M"
-# title = "7 Day Spectrogram of Tilt Readings"
-# savefile = k.dir_images + os.sep + "spectrum_seven.png"
-# mgr_matplot.plot_spectrum(df, plot_seismo, plot_utc, 1, -70, 0, title, savefile)
+df = "%d  %H:%M"
+title = "7 Day Spectrogram of Tilt Readings"
+savefile = k.dir_images + os.sep + "spectrum_seven.png"
+mgr_matplot.plot_spectrum(df, smoothe_seismo, plot_utc, 1, -140, -15, title, savefile)
+
+print("Tiltmeter - One Day dx/dt")
+aggregate_array = result_1d
+aggregate_array.pop(0)
+plot_utc = []
+plot_seismo = []
+
+for i in range(1, len(aggregate_array)):
+    tim = aggregate_array[i][0]
+    tim = datetime.datetime.fromtimestamp(tim)  # datetime object
+    siz = aggregate_array[i][1]
+    plot_utc.append(tim)
+    plot_seismo.append(siz)
+
+# Convert distance readings to rate of change.
+# This is similar to traditional seismograph display
+dxdt = []
+for i in range(1, len(plot_seismo)):
+    dx = plot_seismo[i] - plot_seismo[i - 1]
+    dxdt.append(dx)
+plot_utc.pop(0)
+
+avgwindow = 120
+smoothe_seismo = standard_stuff.filter_average(dxdt, avgwindow)
+plot_utc = plot_utc[avgwindow:-avgwindow]
+smoothe_seismo = standard_stuff.filter_average(smoothe_seismo, avgwindow)
+plot_utc = plot_utc[avgwindow:-avgwindow]
+
+ticks = 30
+ymin = -0.005
+ymax = 0.005
+df = "%d  %H:%M"
+title = "Tiltmeter - One Day dx/dt"
+savefile = k.dir_images + os.sep + "dxdt.png"
+mgr_matplot.plot_time_data(df, plot_utc, smoothe_seismo, ticks, ymin, ymax, title, savefile)
 
 timefinish = time.time()
 print(f"Elapsed seconds to process: {timefinish - time_end}")
