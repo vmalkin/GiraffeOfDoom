dc8e5989c548f5337d4808af01ccd0a88d11b0e2
vmalkin <vaughnmalkin@gmail.com>
2025-09-06 14:28:43 +1200

    Fixing datetime bug in plots

--- pyDataLogger_2025_08/mgr_matplot.py
+++ pyDataLogger_2025_08/mgr_matplot.py
@@ -1,37 +1,61 @@
 import time
 import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
 import matplotlib.dates as mdates
-import numpy as np
 import standard_stuff
-import os
 
-ink_colour = "#7a3f16"
+ink_colour = ["#7a3f16", "red", "green"]
+
+def plot_multi(dateformatstring, dateobjects, dataarrays, readings_per_tick, texttitle, savefile):
+    # utcdates should be datetime objects, not POSIX floats
+    plt.style.use('Solarize_Light2')
+    fig, ax1 = plt.subplots(layout="constrained", figsize=(16, 8), dpi=140)
+    # Subplots with separate y axes
+    ax1.plot(dateobjects, dataarrays[0], c=ink_colour[0], linewidth=1)
+    ax1.set_ylabel("Tiltmeter. Arbitrary Units.", color=ink_colour[0])
+    ax1.tick_params(axis='y', colors=ink_colour[0])
+    ax1.set_ylim([451.75, 453])
+
+    ax2 = ax1.twinx()
+    ax2.plot(dateobjects, dataarrays[1], c=ink_colour[1], linewidth=1)
+    ax2.set_ylabel("Pressure. Pa.", color=ink_colour[1])
+    ax2.tick_params(axis='y', colors=ink_colour[1])
+    ax2.set_ylim([96000, 102000])
+    ax2.spines['right'].set_position(('outward', 60))
+
+    ax3 = ax1.twinx()
+    ax3.plot(dateobjects, dataarrays[2], c=ink_colour[2], linewidth=1)
+    ax3.set_ylabel("Temperature. Deg C.", color=ink_colour[2])
+    ax3.tick_params(axis='y', colors=ink_colour[2])
+    ax3.set_ylim([8, 20])
+
+    # Use proper date formatter + locator
+    ax1.xaxis.set_major_formatter(mdates.DateFormatter(dateformatstring))
+    ax1.xaxis.set_major_locator(mdates.MinuteLocator(interval=readings_per_tick))
+    plt.setp(ax1.get_xticklabels(), rotation=45)  # safer than plt.xticks
+    plot_title = texttitle + " - " + standard_stuff.posix2utc(time.time(), '%Y-%m-%d %H:%M')
+    ax1.set_title(plot_title)
+    plt.savefig(savefile)
+    plt.close()
+
 
 def plot_time_data(dateformatstring, utcdates, maindata, readings_per_tick, ymin, ymax, texttitle, savefile):
     plt.style.use('Solarize_Light2')
     fig, ax = plt.subplots(layout="constrained", figsize=(16, 8), dpi=140)
-
     # utcdates should be datetime objects, not POSIX floats
     ax.plot(utcdates, maindata, c=ink_colour, linewidth=1)
-
     # Use proper date formatter + locator
     ax.xaxis.set_major_formatter(mdates.DateFormatter(dateformatstring))
     ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=readings_per_tick))
-
     plt.setp(ax.get_xticklabels(), rotation=45)  # safer than plt.xticks
     ax.set_ylim([ymin, ymax])
-
     plot_title = texttitle + " - " + standard_stuff.posix2utc(time.time(), '%Y-%m-%d %H:%M')
     ax.set_title(plot_title)
-
     plt.savefig(savefile)
     plt.close()
 
 
 def plot_spectrum(datetimeformat, data, datetimes, plotfrequency, minv, maxv, plottitle, savefile):
     frequency = 1 / plotfrequency
-
     plt.figure(layout="constrained", figsize=(15, 5))
     Pxx, freqs, bins, im = plt.specgram(data, NFFT=128, noverlap=32, detrend='mean', Fs=frequency, cmap='inferno', vmin=minv, vmax=maxv)
     # Pxx, freqs, bins, im = plt.specgram(data, NFFT=128, noverlap=32, detrend='mean', Fs=frequency, cmap='inferno')
