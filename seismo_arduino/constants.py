508a323706d8ec455eb0ebfd9f0b0bb458c28868
vmalkin <vaughnmalkin@gmail.com>
2025-09-30 22:21:45 +1300

    change of default com port

--- seismo_arduino/constants.py
+++ seismo_arduino/constants.py
@@ -1,9 +1,9 @@
 # Comm port parameters - uncomment and change one of the portNames depending on your OS
-comport = 'Com6' # Windows
+# comport = 'Com6' # Windows
 # portName = '/dev/tty.usbserial-A702O0K9' #MacOS
 # portName = "/dev/cu.usbmodem1421"
 # comport = '/dev/ttyUSB0'
-# comport = "/dev/ttyACM0"
+comport = "/dev/ttyACM0"
 # comport = "/dev/ttyACM1"
 baudrate = 57600
 bytesize = 8
