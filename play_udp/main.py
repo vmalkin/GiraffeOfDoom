d5618180989f8c2c0ced1cdca477e43731c723af
Vaughn Malkin <vaughnmalkin@gmail.com>
2025-10-29 15:54:10 +1300

    playing with UDP to send array of data

--- play_udp/main.py
+++ play_udp/main.py
@@ -2,15 +2,26 @@ from enum import Enum
 
 
 class State(Enum):
-    GET_DATA = 0
-    PARSE_DATA = 1
-    ADD_DATA = 2
-    ERROR = 3
-    SETUP = 4
+    SETUP = 0
+    GET_DATA = 1
+    VERIFY_DATA = 2
+    ADD_DATA = 3
+    ERROR = 4
 
-currentstate = State.SETUP
 
+current_state = State.SETUP
+current_message = None
 
 if __name__ == "__main__":
+    # Set up non-blocking timing.
+    while True:
+
+# check_create_udp_socket()
+# check_create_folders()
+# check_create_db()
+# get_data()
+# verify_data()
+# add_data()
+
 
 
