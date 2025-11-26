ffd6d03115ab683c8e39b261d0f2e4dfecec1386
Vaughn Malkin <vaughnmalkin@gmail.com>
2025-11-26 13:09:25 +1300

    rationalising the plotting across separate files

---
+++ seismo_arduino/plot_master.py
@@ -0,0 +1,243 @@
+import mgr_database
+import time
+import constants as k
+import plotter_phaseportrait
+
+time_end = time.time()
+time_start_7d = time_end - (60 * 60 * 24 * 7)
+
+# result_total = mgr_database.db_data_get_all()
+result_7d = mgr_database.db_data_get(time_start_7d)
+result_1d = result_7d[-86400 * int(1 / k.sensor_reading_frequency):]
+
+plotter_phaseportrait.wrapper(result_1d)
+
+timefinish = time.time()
+print(f"Elapsed minutes to process: {(timefinish - time_end) / 60}")
+
+# try:
+#     print("Barometric Spectrogram - Past 24 hours")
+#     window = 10
+#     aggregate_array = class_aggregator.aggregate_data(window, result_1d)
+#     aggregate_array.pop(0)
+#
+#     plot_utc = []
+#     plot_press = []
+#     for i in range(1, len(aggregate_array)):
+#         tim = aggregate_array[i].get_avg_posix()
+#         tim = datetime.fromtimestamp(tim, tz=timezone.utc)  # datetime object
+#         prs = aggregate_array[i].get_data_avg(aggregate_array[i].data_pressure)
+#         plot_utc.append(tim)
+#         plot_press.append(prs)
+#
+#     df = "%d %H:%M"
+#     title = "Spectrogram of Barometric Pressure"
+#     savefile = k.dir_images + os.sep + "spectrum_press.png"
+#     tick = 60 * 60
+#     mgr_matplot.plot_spectrum(df, tick, plot_press, plot_utc, 1, 0, 30, title, savefile)
+# except:
+#     pass
+#
+# # =============================================================================================================
+# print("Tiltmeter - 24, hourly plots")
+# aggregate_array = result_1d
+# aggregate_array.pop(0)
+# plot_utc = []
+# plot_seismo = []
+#
+# for i in range(1, len(aggregate_array)):
+#     tim = aggregate_array[i][0]
+#     tim = datetime.fromtimestamp(tim, tz=timezone.utc)  # datetime object
+#     siz = aggregate_array[i][1]
+#     plot_utc.append(tim)
+#     plot_seismo.append(siz)
+#
+# # Convert distance readings to rate of change.
+# # This is similar to traditional seismograph display
+# dxdt = []
+# for i in range(1, len(plot_seismo)):
+#     # dx = plot_seismo[i]
+#     dx = plot_seismo[i] - plot_seismo[i - 1]
+#     dxdt.append(dx)
+# plot_utc.pop(0)
+#
+# avgwindow = 10 * 3
+# smoothe_dx = standard_stuff.filter_average(dxdt, avgwindow)
+# plot_utc = plot_utc[avgwindow:-avgwindow]
+# smoothe_dx = standard_stuff.filter_average(smoothe_dx, avgwindow)
+# plot_utc = plot_utc[avgwindow:-avgwindow]
+#
+# smoothe_seismo = standard_stuff.filter_average(plot_seismo, avgwindow)
+# smoothe_seismo = standard_stuff.filter_average(smoothe_seismo, avgwindow)
+# smoothe_seismo.pop(0)
+#
+# ticks = 20
+# df = "%d  %H:%M"
+# title = "Tiltmeter One Day dx/dt"
+# savefolder = k.dir_images
+# mgr_matplot.plot_dual_hourly(df, plot_utc, smoothe_seismo, smoothe_dx, title, savefolder)
+#
+# title = "Phase Plot. "
+# savefolder = "phaseimages"
+# mgr_matplot.plot_pseudo_phase(df, plot_utc, smoothe_seismo, smoothe_dx, title, savefolder)
+#
+# # =============================================================================================================
+# print("Tiltmeter - 1 Day")
+# aggregate_array = result_1d
+# aggregate_array.pop(0)
+# plot_utc = []
+# plot_seismo = []
+# plot_temp = []
+# plot_press = []
+# wrapper = []
+#
+# for i in range(1, len(aggregate_array)):
+#     tim = aggregate_array[i][0]
+#     tim = datetime.fromtimestamp(tim, tz=timezone.utc)  # datetime object
+#     siz = aggregate_array[i][1]
+#     tmp = aggregate_array[i][2]
+#     prs = aggregate_array[i][3]
+#     plot_utc.append(tim)
+#     plot_seismo.append(siz)
+#     plot_temp.append(tmp)
+#     plot_press.append(prs)
+#
+# avgwindow = 20
+# smoothe_seismo = standard_stuff.filter_average(plot_seismo, avgwindow)
+# plot_utc = plot_utc[avgwindow:-avgwindow]
+# plot_temp = plot_temp[avgwindow:-avgwindow]
+# plot_press = plot_press[avgwindow:-avgwindow]
+# smoothe_seismo = standard_stuff.filter_average(smoothe_seismo, avgwindow)
+# plot_utc = plot_utc[avgwindow:-avgwindow]
+# plot_temp = plot_temp[avgwindow:-avgwindow]
+# plot_press = plot_press[avgwindow:-avgwindow]
+# wrapper.append(smoothe_seismo)
+# wrapper.append(plot_press)
+# wrapper.append(plot_temp)
+#
+# ticks = 20
+# df = "%d  %H:%M"
+# title = "Tiltmeter One Day"
+# savefile = k.dir_images + os.sep + "one_day.png"
+# mgr_matplot.plot_multi(df, plot_utc, wrapper, ticks, title, savefile)
+#
+#
+# # =============================================================================================================
+# # A special instance here where we will decimate the volume of data
+# print("Tiltmeter - 7 Days")
+# # decimate data for this.
+# window = 10
+# aggregate_array = class_aggregator.aggregate_data(window, result_7d)
+# aggregate_array.pop(0)
+#
+# wrapper = []
+# plot_utc = []
+# plot_seismo = []
+# plot_temp = []
+# plot_press = []
+#
+# for i in range(1, len(aggregate_array)):
+#     tim = aggregate_array[i].get_avg_posix()
+#     tim = datetime.fromtimestamp(tim, tz=timezone.utc)  # datetime object
+#     siz = aggregate_array[i].get_data_avg(aggregate_array[i].data_seismo)
+#     tmp = aggregate_array[i].get_data_avg(aggregate_array[i].data_temperature)
+#     prs = aggregate_array[i].get_data_avg(aggregate_array[i].data_pressure)
+#     plot_utc.append(tim)
+#     plot_seismo.append(siz)
+#     plot_temp.append(tmp)
+#     plot_press.append(prs)
+#
+# # # =============================================================================================================
+# # # Scatterplots are here to use the 7 day data that's already been processed. No need to duplicate things.
+# # print("Tiltmeter - Scatterplots")
+# # sct_seis = standard_stuff.filter_median(plot_seismo, 2)
+# # sct_press = standard_stuff.filter_median(plot_press, 2)
+# # sct_temp = standard_stuff.filter_median(plot_temp, 2)
+# # savefile = k.dir_images + os.sep + "sctr_tilt_temp.png"
+# # mgr_matplot.plot_scatterplot(sct_temp, sct_seis, "7 Day Tilt vs Temperature", savefile)
+# # savefile = k.dir_images + os.sep + "sctr_tilt_press.png"
+# # mgr_matplot.plot_scatterplot(sct_press, sct_seis, "7 Day Tilt vs Air Pressure", savefile)
+# # savefile = k.dir_images + os.sep + "sctr_temp_press.png"
+# # mgr_matplot.plot_scatterplot(sct_press, sct_temp, "7 Day Air Temp vs Air Pressure", savefile)
+#
+# avgwindow = 40
+# smoothe_seismo = standard_stuff.filter_average(plot_seismo, avgwindow)
+# plot_utc = plot_utc[avgwindow:-avgwindow]
+# plot_temp = plot_temp[avgwindow:-avgwindow]
+# plot_press = plot_press[avgwindow:-avgwindow]
+# smoothe_seismo = standard_stuff.filter_average(smoothe_seismo, avgwindow)
+# plot_utc = plot_utc[avgwindow:-avgwindow]
+# plot_temp = plot_temp[avgwindow:-avgwindow]
+# plot_press = plot_press[avgwindow:-avgwindow]
+# wrapper.append(smoothe_seismo)
+# wrapper.append(plot_press)
+# wrapper.append(plot_temp)
+#
+# ticks = 240
+# df = "%d  %H:%M"
+# title = "Tiltmeter One Week"
+# savefile = k.dir_images + os.sep + "seven_day.png"
+# mgr_matplot.plot_multi(df, plot_utc, wrapper, ticks, title, savefile)
+#
+# # =============================================================================================================
+# # Spectrogram of seismic readings
+# print("Seismic Spectrogram")
+# aggregate_array = result_1d
+# aggregate_array.pop(0)
+# plot_utc = []
+# plot_seismo = []
+# plot_temp = []
+# plot_press = []
+# wrapper = []
+#
+# for i in range(1, len(aggregate_array)):
+#     tim = aggregate_array[i][0]
+#     tim = datetime.fromtimestamp(tim, tz=timezone.utc)  # datetime object
+#     siz = aggregate_array[i][1]
+#     tmp = aggregate_array[i][2]
+#     prs = aggregate_array[i][3]
+#     plot_utc.append(tim)
+#     plot_seismo.append(siz)
+#     plot_temp.append(tmp)
+#     plot_press.append(prs)
+# spectrum_utc = plot_utc
+# spectrum_seismo = plot_seismo
+# df = "%d %H:%M"
+# title = "Spectrogram of Tilt Readings"
+# savefile = k.dir_images + os.sep + "spectrum_seismo.png"
+# tick = 60 * 10 * 60
+# mgr_matplot.plot_spectrum(df, tick, spectrum_seismo, spectrum_utc, 1, -10, 20, title, savefile)
+#
+# # =============================================================================================================
+# # Empirical Mode Decomposition
+# print("Empirical Mode Decomposition")
+# aggregate_array = result_1d
+# aggregate_array.pop(0)
+# plot_utc = []
+# plot_seismo = []
+# wrapperdata = []
+#
+# for i in range(1, len(aggregate_array)):
+#     tim = aggregate_array[i][0]
+#     tim = datetime.fromtimestamp(tim, tz=timezone.utc)  # datetime object
+#     siz = aggregate_array[i][1]
+#     plot_utc.append(tim)
+#     plot_seismo.append(siz)
+#
+# wrapperdata.append(plot_utc)
+# wrapperdata.append(plot_seismo)
+# savefile = k.dir_images + os.sep + "imf.png"
+# df = "%d  %H:%M"
+# mgr_emd.wrapper(wrapperdata, savefile, df)
+# # # =============================================================================================================
+# # # Statistical analysis to identify events that exceed standrd deviation thresholds, and anything that
+# # # triggers strong oscillations at the pendulums fundamental frequency. Display as a 24 hour graph.
+# #
+# # # =============================================================================================================
+# # print("FFT - 1 Days")
+# # fft_sevendays.wrapper(result_1d)
+# # print("FFT - ALL DATA!!")
+# # fft_entire_data.wrapper(result_total)
+#
+# timefinish = time.time()
+# print(f"Elapsed minutes to process: {(timefinish - time_end) / 60}")
---
+++ seismo_arduino/plotter_phaseportrait.py
@@ -0,0 +1,98 @@
+from datetime import timezone, datetime
+import standard_stuff
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+ink_colour = ["#7a3f16", "green", "red", "#ffffff"]
+plotstyle = 'bmh'
+
+
+def try_create_directory(directory):
+    if os.path.isdir(directory) is False:
+        print("Creating image file directory...")
+        try:
+            os.makedirs(directory)
+            print("Directory created.")
+        except:
+            if not os.path.isdir(directory):
+                print("Unable to create directory")
+
+def plot_pseudo_phase(datetimeformat, plot_utc, plot_seismo, dxdt, title, savefolder):
+    try_create_directory(savefolder)
+    # the size of an hour is plot frequency multiplied by seconds/min and mins/hr
+    hour_slice = 10 * 60 * 10
+    sz_avg = np.mean(plot_seismo)
+    sz_max = max(plot_seismo)
+    sz_min = min(plot_seismo)
+    xmax = sz_avg + 1.1 * (sz_max - sz_avg)
+    xmin = sz_avg - 1.1 * (sz_avg - sz_min)
+
+    dx_avg = np.mean(dxdt)
+    dx_max = max(dxdt)
+    dx_min = min(dxdt)
+    ymax = dx_avg + 1.1 * (dx_max - dx_avg)
+    ymin = dx_avg - 1.1 * (dx_avg - dx_min)
+
+    for i in range(0, len(plot_seismo), hour_slice):
+        array_start = i
+        array_end = i + hour_slice
+        seismo_data = plot_seismo[array_start:array_end]
+        diff_data = dxdt[array_start:array_end]
+        chart_times = plot_utc[array_start:array_end]
+        begintime = chart_times[0].strftime(datetimeformat)
+        endtime = chart_times[len(chart_times) - 1].strftime(datetimeformat)
+
+        plt.style.use(plotstyle)
+        fig, ax = plt.subplots(layout="constrained", figsize=(8, 8), dpi=140)
+        ax.set_ylim([ymin, ymax])
+        ax.set_xlim([xmin, xmax])
+        # utcdates should be datetime objects, not POSIX floats
+        # ax.scatter(seismo_data, diff_data, c=ink_colour[0], s=3)
+        ax.plot(seismo_data, diff_data, c=ink_colour[0], linewidth=1)
+
+        plot_title = title + " - " + begintime + " - " + endtime
+        fig.suptitle(plot_title)
+        savefile = savefolder + os.sep + "pp_" + str(i) + ".png"
+        plt.savefig(savefile)
+        plt.close()
+
+
+def wrapper(data):
+    print("Tiltmeter - 24, hourly plots")
+    aggregate_array = data
+    aggregate_array.pop(0)
+    plot_utc = []
+    plot_seismo = []
+
+    for i in range(1, len(aggregate_array)):
+        tim = aggregate_array[i][0]
+        tim = datetime.fromtimestamp(tim, tz=timezone.utc)  # datetime object
+        siz = aggregate_array[i][1]
+        plot_utc.append(tim)
+        plot_seismo.append(siz)
+
+    # Convert distance readings to rate of change.
+    # This is similar to traditional seismograph display
+    dxdt = []
+    for i in range(1, len(plot_seismo)):
+        # dx = plot_seismo[i]
+        dx = plot_seismo[i] - plot_seismo[i - 1]
+        dxdt.append(dx)
+    plot_utc.pop(0)
+
+    avgwindow = 10 * 3
+    smoothe_dx = standard_stuff.filter_average(dxdt, avgwindow)
+    plot_utc = plot_utc[avgwindow:-avgwindow]
+    smoothe_dx = standard_stuff.filter_average(smoothe_dx, avgwindow)
+    plot_utc = plot_utc[avgwindow:-avgwindow]
+
+    smoothe_seismo = standard_stuff.filter_average(plot_seismo, avgwindow)
+    smoothe_seismo = standard_stuff.filter_average(smoothe_seismo, avgwindow)
+    smoothe_seismo.pop(0)
+
+    ticks = 20
+    df = "%d  %H:%M"
+    title = "Phase Plot. "
+    savefolder = "phaseimages"
+    plot_pseudo_phase(df, plot_utc, smoothe_seismo, smoothe_dx, title, savefolder)
