b1f911fc066822cd1aa93d87313625bcd451bc20
vmalkin <vaughnmalkin@gmail.com>
2025-11-27 08:16:35 +1300

    tweak series line thicknesses in plots

--- seismo_arduino/plotter_combo1day.py
+++ seismo_arduino/plotter_combo1day.py
@@ -27,7 +27,7 @@ def plot_multi(dateformatstring, dateobjects, dataarrays, readings_per_tick, tex
     plt.style.use(plotstyle)
     fig, ax1 = plt.subplots(layout="constrained", figsize=(16, 8), dpi=140)
     # Subplots with separate y axes
-    ax1.plot(dateobjects, dataarrays[0], c=ink_colour[0], linewidth=1)
+    ax1.plot(dateobjects, dataarrays[0], c=ink_colour[0], linewidth=2)
     ax1.set_ylabel("Tiltmeter. Arbitrary Units.", color=ink_colour[0])
     ax1.tick_params(axis='y', colors=ink_colour[0])
 
@@ -39,7 +39,7 @@ def plot_multi(dateformatstring, dateobjects, dataarrays, readings_per_tick, tex
     ax1.set_ylim([ymin, ymax])
 
     ax2 = ax1.twinx()
-    ax2.plot(dateobjects, dataarrays[1], c=ink_colour[1], linewidth=1)
+    ax2.plot(dateobjects, dataarrays[1], c=ink_colour[1], linewidth=2)
     ax2.set_ylabel("Pressure. Pa.", color=ink_colour[1])
     ax2.tick_params(axis='y', colors=ink_colour[1])
     maxv = max(dataarrays[1])
@@ -49,7 +49,7 @@ def plot_multi(dateformatstring, dateobjects, dataarrays, readings_per_tick, tex
     ax2.yaxis.grid(False)
 
     ax3 = ax1.twinx()
-    ax3.plot(dateobjects, dataarrays[2], c=ink_colour[2], linewidth=1)
+    ax3.plot(dateobjects, dataarrays[2], c=ink_colour[2], linewidth=2)
     ax3.set_ylabel("Temperature. Deg C.", color=ink_colour[2])
     ax3.tick_params(axis='y', colors=ink_colour[2])
     # maxv = 18
