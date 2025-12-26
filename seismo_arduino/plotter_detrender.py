e3732207953c870408ba2deeded1cd4c5b39d70a
vmalkin <vaughnmalkin@gmail.com>
2025-12-26 22:19:05 +1300

    Investigate detrending to remove long-term changes and highlight short term changes.

--- seismo_arduino/plotter_detrender.py
+++ seismo_arduino/plotter_detrender.py
@@ -5,25 +5,23 @@ import matplotlib.dates as mdates
 from matplotlib.ticker import AutoMinorLocator
 import os
 import constants as k
+import standard_stuff
 
 
 def plot_data(data, dates, filename, dateformatstring):
     ink_colour = ["#7a3f16", "green", "red", "#ffffff"]
     plotstyle = 'bmh'
-
     plt.style.use(plotstyle)
-
-    fig, ax = plt.subplots( layout="constrained", figsize=(14, 7), dpi=140)
+    fig, ax = plt.subplots( layout="constrained", figsize=(200, 7), dpi=140)
     title = "Empirical Mode Decomposion - Tilt data. "
 
     ax.plot(dates, data, c=ink_colour[0], linewidth=1)
-    ax.xaxis.set_major_formatter(mdates.DateFormatter(dateformatstring))
+    ax.set_ylim([-2, 2])
+    # Use proper date formatter + locator
     ax.grid(which='major', axis='x', linestyle='solid', c='black', visible='True', zorder=5)
     ax.grid(which='minor', axis='x', linestyle='dotted', c='black', visible='True', zorder=5)
-
-    # Use proper date formatter + locator
     ax.xaxis.set_major_formatter(mdates.DateFormatter(dateformatstring))
-    ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=4))
+    ax.xaxis.set_minor_locator(AutoMinorLocator(6))
     plt.setp(ax.get_xticklabels(), rotation=90)  # safer than plt.xticks
 
     savefile = filename
@@ -47,7 +45,8 @@ def wrapper(data):
     plot_utc = []
     plot_seismo = []
     readings_per_sec = 10
-    detrend_half_window = readings_per_sec * 60 * 5
+    # Current detrending by an hour
+    detrend_half_window = readings_per_sec * 60 * 30
     end_index = len(data_seismo) - detrend_half_window
 
     for i in range(0, len(data_seismo)):
@@ -57,8 +56,17 @@ def wrapper(data):
             dt = data_utc[i]
             plot_seismo.append(dd)
             plot_utc.append(dt)
-            if i % 100 == 0:
-                print(f'{i} / {len(data_seismo)}')
+            if i % 100000 == 0:
+                print(f'Detrend {int(i / len(data_seismo) * 100)}% completed...')
+
+    # Some smoothing is required here
+    avgwindow = 10 * 3
+    plot_seismo = standard_stuff.filter_average(plot_seismo, avgwindow)
+    plot_utc = plot_utc[avgwindow:-avgwindow]
+    plot_seismo = standard_stuff.filter_average(plot_seismo, avgwindow)
+    plot_utc = plot_utc[avgwindow:-avgwindow]
+
+    print(f'Detrend completed!')
     df = "%d  %H:%M"
     savefile = k.dir_images['images'] + os.sep + "detrended.png"
     plot_data(plot_seismo, plot_utc, savefile, df)
