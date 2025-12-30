7f73f2b7ce36660884bd77b67ed2a5ab7f67561b
vmalkin <vaughnmalkin@gmail.com>
2025-12-30 13:56:16 +1300

    Looking at normalisation and preprocessing of data

--- seismo_arduino/plotter_detrender.py
+++ seismo_arduino/plotter_detrender.py
@@ -3,10 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from matplotlib.ticker import AutoMinorLocator
-import os
-import constants as k
-import standard_stuff
-import class_aggregator
+# import os
+# import constants as k
+# import standard_stuff
+# import class_aggregator
+
+nullvalue = np.nan
+
+
+class DecimatedData:
+    def __init__(self):
+        self.posixtime = []
+        self.seismo = []
+        self.temperature = []
+        self.pressure = []
 
 
 def plot_data(data, dates, filename, dateformatstring):
@@ -32,50 +42,95 @@ def plot_data(data, dates, filename, dateformatstring):
 
 def wrapper(data):
     print("*** Detrending.")
-    # decimate the data
-    window = 10
-    aggregate_array = class_aggregator.aggregate_data(window, data)
-    aggregate_array.pop(0)
-
     raw_utc = []
     raw_seismo = []
-
-    for i in range(1, len(aggregate_array)):
-        tim = aggregate_array[i].get_avg_posix()
-        tim = datetime.fromtimestamp(tim, tz=timezone.utc)  # datetime object
-        siz = aggregate_array[i].get_data_avg(aggregate_array[i].data_seismo)
+    raw_temperature = []
+    raw_pressure = []
+    # data in format posixtime, tiltdata, temperature, pressure
+    for i in range(0, len(data)):
+        tim = data[i][0]
+        siz = data[i][1]
+        tmp = data[i][2]
+        prs = data[i][3]
         raw_utc.append(tim)
         raw_seismo.append(siz)
+        raw_temperature.append(tmp)
+        raw_pressure.append(prs)
+
+    # de-mean the data
+    demean_seismo = []
+    mean_value = np.nanmean(raw_seismo)
+    for item in raw_seismo:
+        dd = item - mean_value
+        demean_seismo.append(dd)
+
+    demean_temperature = []
+    mean_value = np.nanmean(raw_temperature)
+    for item in raw_temperature:
+        dd = item - mean_value
+        demean_temperature.append(dd)
+
+    demean_pressure = []
+    mean_value = np.nanmean(raw_pressure)
+    for item in raw_pressure:
+        dd = item - mean_value
+        demean_pressure.append(dd)
 
 
-    plot_utc = []
-    plot_seismo = []
-    readings_per_sec = 1
-    # Current detrending by an hour
-    detrend_half_window = readings_per_sec * 60 * 5
-    end_index = len(raw_seismo) - detrend_half_window
+    # Perform mean / z-score normalisation
+    d_mu = np.mean(demean_seismo)
+    d_sigma = np.std(demean_seismo)
+    z_score_seismo = []
+    for item in demean_seismo:
+        z_score = (item - d_mu) / d_sigma
+        z_score_seismo.append(z_score)
 
-    for i in range(0, len(raw_seismo)):
-        if detrend_half_window < i < end_index:
-            window_data = raw_seismo[i - detrend_half_window: i + detrend_half_window]
-            dd = raw_seismo[i] - np.nanmean(window_data)
-            dt = raw_utc[i]
-            plot_seismo.append(dd)
-            plot_utc.append(dt)
-            if i % 100000 == 0:
-                print(f'Detrend {int(i / len(raw_seismo) * 100)}% completed...')
+    d_mu = np.mean(demean_temperature)
+    d_sigma = np.std(demean_temperature)
+    z_score_temperature = []
+    for item in demean_temperature:
+        z_score = (item - d_mu) / d_sigma
+        z_score_temperature.append(z_score)
 
-    # Some smoothing is required here
-    avgwindow = 2
-    plot_seismo = standard_stuff.filter_average(plot_seismo, avgwindow)
-    plot_utc = plot_utc[avgwindow:-avgwindow]
-    plot_seismo = standard_stuff.filter_average(plot_seismo, avgwindow)
-    plot_utc = plot_utc[avgwindow:-avgwindow]
+    d_mu = np.mean(demean_pressure)
+    d_sigma = np.std(demean_pressure)
+    z_score_pressure = []
+    for item in demean_pressure:
+        z_score = (item - d_mu) / d_sigma
+        z_score_pressure.append(z_score)
+
+    print(f'length utc = {len(raw_utc)}.\nlength z_score = {len(z_score_seismo)}.\nlength z_score_temperature = {len(z_score_temperature)}.')
+
+    # decimate data to plot it.
+    decimate_array = []
+    decimate_half_window = 5
+    end_index = len(z_score_seismo) - decimate_half_window
+
+    if len(z_score_seismo) > decimate_half_window:
+        for i in range(0, len(z_score_seismo)):
+            if decimate_half_window < i < end_index:
+                px_data = raw_utc[i - decimate_half_window: i + decimate_half_window]
+                sz_data = z_score_seismo[i - decimate_half_window: i + decimate_half_window]
+                tm_data = z_score_temperature[i - decimate_half_window: i + decimate_half_window]
+                pr_data = z_score_pressure[i - decimate_half_window: i + decimate_half_window]
+                d = DecimatedData()
+                d.posixtime = px_data
+                d.seismo = sz_data
+                d.temperature = tm_data
+                d.pressure = pr_data
+                decimate_array.append(d)
+
+            else:
+                decimate_array.append(nullvalue)
+    else:
+        for _ in data:
+            decimate_array.append(nullvalue)
+
 
     print(f'Detrend completed!')
-    df = "%d  %H:%M"
-    savefile = k.dir_images['images'] + os.sep + "detrended.png"
-    plot_data(plot_seismo, plot_utc, savefile, df)
+    # df = "%d  %H:%M"
+    # savefile = k.dir_images['images'] + os.sep + "detrended.png"
+    # plot_data(demean_seismo, raw_utc, savefile, df)
 
 
 
