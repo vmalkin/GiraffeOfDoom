51a828c1a98eb3922fde7fa323463c9f6f86aa5e
vmalkin <vaughnmalkin@gmail.com>
2026-01-02 21:26:50 +1300

    Looking at normalisation and preprocessing of data

--- seismo_arduino/plotter_z_score.py
+++ seismo_arduino/plotter_z_score.py
@@ -27,48 +27,89 @@ class DecimatedData:
         return returnvalue
 
 
-def plot_multi(dateformatstring, dateobjects, dataarrays, readings_per_tick, texttitle, savefile):
+def plot_multi(dateformatstring, dateobjects, data_dm, data_roll, data_zs, readings_per_tick, texttitle, savefile):
     # utcdates should be datetime objects, not POSIX floats
     ink_colour = ["#7a3f16", "green", "red", "#ffffff"]
     plotstyle = 'bmh'
     plt.style.use(plotstyle)
     # fig, ax1 = plt.subplots(nrows=2, layout="constrained", figsize=(16, 8), dpi=140)
-    fig, (ax_top, ax_bottom) = plt.subplots(
-        2, 1, figsize=(16, 8), dpi=140, sharex=True, height_ratios=[2, 1]
+    fig, (ax_demean, ax_rollmean, ax_zscore) = plt.subplots(
+        3, 1,
+        sharex=True,
+        figsize=(17, 12),
+        layout="constrained",
+        height_ratios=[1, 1, 1],
     )
 
+    # --- De-meaned seismo data ---
+    ax_demean.plot(dateobjects, data_dm, c='blue', linewidth=1)
+    ax_demean.set_ylabel("(Arb))", color='blue')
+    ax_demean.tick_params(axis='y', colors='blue')
+    title = "De-meaned seismic data."
+    ax_demean.set_title(f'{title}')
+    ax_demean.grid(which='major', axis='x', linestyle='solid', visible='True')
+    ax_demean.grid(which='minor', axis='x', linestyle='dotted', visible='True')
+    ax_demean.grid(which='major', axis='y', linestyle='solid', visible='True')
+
+    # --- Rolling mean seismo data ---
+    ax_rollmean.plot(dateobjects, data_roll, c='blue', linewidth=1)
+    ax_rollmean.set_ylabel("(Arb))", color='blue')
+    ax_rollmean.tick_params(axis='y', colors='blue')
+    title = "Rolling Mean."
+    ax_rollmean.set_title(f'{title}')
+    ax_rollmean.grid(which='major', axis='x', linestyle='solid', visible='True')
+    ax_rollmean.grid(which='minor', axis='x', linestyle='dotted', visible='True')
+    ax_rollmean.grid(which='major', axis='y', linestyle='solid', visible='True')
+
+    # --- Z-Score seismo data ---
+    ax_zscore.plot(dateobjects, data_zs, c='blue', linewidth=1)
+    ax_zscore.set_ylabel("(Arb))", color='blue')
+    ax_zscore.tick_params(axis='y', colors='blue')
+    title = "Z-Score."
+    ax_zscore.set_title(f'{title}')
+    ax_zscore.grid(which='major', axis='x', linestyle='solid', visible='True')
+    ax_zscore.grid(which='minor', axis='x', linestyle='dotted', visible='True')
+    ax_zscore.grid(which='major', axis='y', linestyle='solid', visible='True')
+
+    ax_zscore.xaxis.set_major_formatter(mdates.DateFormatter(dateformatstring))
+    fig.autofmt_xdate()
+    ax_zscore.xaxis.set_minor_locator(AutoMinorLocator(6))
+
+    if savefile is not None:
+        fig.savefig(savefile)
+
     # Subplots with separate y axes
-    ax_top.plot(dateobjects, dataarrays[0], c=ink_colour[0], linewidth=1)
-    ax_top.set_ylabel("Tiltmeter (Arb). StdDev.", color=ink_colour[0])
-    ax_top.tick_params(axis='y', colors=ink_colour[0])
-
-    ax_top2 = ax_top.twinx()
-    ax_top2.plot(dateobjects, dataarrays[1], c=ink_colour[1], linewidth=1)
-    ax_top2.set_ylabel("Pressure (Pa). StdDev.", color=ink_colour[1])
-    ax_top2.tick_params(axis='y', colors=ink_colour[1])
-    ax_top2.spines['right'].set_position(('outward', 60))
-    ax_top2.yaxis.grid(False)
-
-    ax_top3 = ax_top.twinx()
-    ax_top3.plot(dateobjects, dataarrays[2], c=ink_colour[2], linewidth=1)
-    ax_top3.set_ylabel("Temperature (°C). StdDev.", color=ink_colour[2])
-    ax_top3.tick_params(axis='y', colors=ink_colour[2])
-    ax_top3.yaxis.grid(False)
-
-    noise_colour = '#505050'
-    ax_bottom.plot(dateobjects, dataarrays[3], c=noise_colour, linewidth=0.8)
-    ax_bottom.set_ylabel("Tilt Noise. (Arb).", color=noise_colour)
-    ax_bottom.tick_params(axis='y', colors=noise_colour)
-    ax_bottom.set_ylim([3, 15])
-    ax_bottom.yaxis.grid(False)
+    # ax_top.plot(dateobjects, dataarrays[0], c=ink_colour[0], linewidth=1)
+    # ax_top.set_ylabel("Tiltmeter (Arb). StdDev.", color=ink_colour[0])
+    # ax_top.tick_params(axis='y', colors=ink_colour[0])
+    #
+    # ax_top2 = ax_top.twinx()
+    # ax_top2.plot(dateobjects, dataarrays[1], c=ink_colour[1], linewidth=1)
+    # ax_top2.set_ylabel("Pressure (Pa). StdDev.", color=ink_colour[1])
+    # ax_top2.tick_params(axis='y', colors=ink_colour[1])
+    # ax_top2.spines['right'].set_position(('outward', 60))
+    # ax_top2.yaxis.grid(False)
+    #
+    # ax_top3 = ax_top.twinx()
+    # ax_top3.plot(dateobjects, dataarrays[2], c=ink_colour[2], linewidth=1)
+    # ax_top3.set_ylabel("Temperature (°C). StdDev.", color=ink_colour[2])
+    # ax_top3.tick_params(axis='y', colors=ink_colour[2])
+    # ax_top3.yaxis.grid(False)
+    #
+    # noise_colour = '#505050'
+    # ax_bottom.plot(dateobjects, dataarrays[3], c=noise_colour, linewidth=0.8)
+    # ax_bottom.set_ylabel("Tilt Noise. (Arb).", color=noise_colour)
+    # ax_bottom.tick_params(axis='y', colors=noise_colour)
+    # ax_bottom.set_ylim([3, 15])
+    # ax_bottom.yaxis.grid(False)
 
     # Use proper date formatter + locator
-    ax_top.xaxis.set_major_formatter(mdates.DateFormatter(dateformatstring))
-    ax_top.xaxis.set_major_locator(mdates.MinuteLocator(interval=readings_per_tick))
-    plt.setp(ax_bottom.get_xticklabels(), rotation=90)  # safer than plt.xticks
-    plot_title = texttitle
-    ax_top.set_title(plot_title)
-    plt.tight_layout()
+    ax_zscore.xaxis.set_major_formatter(mdates.DateFormatter(dateformatstring))
+    ax_zscore.xaxis.set_major_locator(mdates.MinuteLocator(interval=readings_per_tick))
+    plt.setp(ax_zscore.get_xticklabels(), rotation=90)  # safer than plt.xticks
+    # plot_title = texttitle
+    # ax_top.set_title(plot_title)
+    # plt.tight_layout()
     plt.savefig(savefile)
     plt.close()
 
@@ -128,7 +169,8 @@ def rolling_mean(dataarray, halfwindow):
 
 def wrapper(data):
     print("*** Detrend started.")
-    # Our window should relate to real phenomena based on detected events. An hour or so is often used for seismic events
+    # Our window should relate to real phenomena based on detected events. An hour or so is often used
+    # for seismic events
     half_window = 10 * 60 * 30
     raw_utc = []
     raw_seismo = []
@@ -186,47 +228,46 @@ def wrapper(data):
             if i % 10000 == 0:
                 print(f'Decimation: {i} / {len(z_score_seismo)} completed')
 
-    # # ================================================================================
-    # # Finally, plot data!
-    # print('--- Finally, plot data!...')
-    # df = "%d  %H:%M"
-    # # tim = datetime.fromtimestamp(tim, tz=timezone.utc)  # datetime object
-    # plot_dates = []
-    # datawrapper = []
-    # plot_seismo = []
-    # plot_temperature = []
-    # plot_pressure = []
-    # plot_noise = []
-    #
-    # for item in decimate_array:
-    #     duration = item.posixtime[-1] - item.posixtime[0]
-    #     if duration < ((2 * decimate_half_window + 1) * 0.1):
-    #         time_object = np.nanmean(item.posixtime)
-    #         time_object = datetime.fromtimestamp(time_object, tz=timezone.utc)
-    #         seismo_current = np.nanmean(item.seismo)
-    #         temperature_current = np.nanmean(item.temperature)
-    #         pressure_current = np.nanmean(item.pressure)
-    #         noise = item.get_noise()
-    #
-    #         plot_dates.append(time_object)
-    #         plot_seismo.append(seismo_current)
-    #         plot_temperature.append(temperature_current)
-    #         plot_pressure.append(pressure_current)
-    #         plot_noise.append(noise)
-    #
-    # datawrapper.append(plot_seismo)
-    # datawrapper.append(plot_pressure)
-    # datawrapper.append(plot_temperature)
-    # datawrapper.append(plot_noise)
-    #
-    # plottitle = f'De-meaned, Z-score Normalised Data. Decimation half window: {decimate_half_window}.'
-    # savefile = k.dir_images['images'] + os.sep + "detrended.png"
-    # plot_multi(dateformatstring=df,
-    #            dateobjects=plot_dates,
-    #            dataarrays=datawrapper,
-    #            readings_per_tick=60,
-    #            texttitle=plottitle,
-    #            savefile=savefile)
+    # ================================================================================
+    # Finally, plot data!
+    print('--- Finally, plot data!...')
+    df = "%d  %H:%M"
+    # tim = datetime.fromtimestamp(tim, tz=timezone.utc)  # datetime object
+    plot_dates = []
+    plot_seismo = []
+    plot_demean = []
+    plot_rollingmean = []
+    plot_zscore = []
+
+
+    for item in decimate_array:
+        duration = item.posixtime[-1] - item.posixtime[0]
+        if duration < ((2 * decimate_half_window + 1) * 0.1):
+            time_object = np.nanmean(item.posixtime)
+            time_object = datetime.fromtimestamp(time_object, tz=timezone.utc)
+            seismo_current = np.nanmean(item.seismo)
+            demean_current = np.nanmean(item.demean)
+            rolling_current = np.nanmean(item.rollingmean)
+            zscore_current = np.nanmean(item.zscore)
+
+            plot_dates.append(time_object)
+            plot_seismo.append(seismo_current)
+            plot_demean.append(demean_current)
+            plot_rollingmean.append(rolling_current)
+            plot_zscore.append(zscore_current)
+
+
+    plottitle = f'De-meaned, Z-score Normalised Data. Decimation half window: {decimate_half_window}.'
+    savefile = k.dir_images['images'] + os.sep + "detrended.png"
+
+    plot_multi(dateformatstring=df,
+               dateobjects=plot_dates,
+               data_dm=plot_demean,
+               data_roll=plot_rollingmean,
+               data_zs=plot_zscore,
+               readings_per_tick=60,
+               texttitle=plottitle,
+               savefile=savefile)
 
     print(f'*** Detrend completed!')
 
