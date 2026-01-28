15c366823a59035296bcde2ee20154afece1d40a
vmalkin <vaughnmalkin@gmail.com>
2026-01-28 21:19:02 +1300

    test openCV

--- test/main.py
+++ test/main.py
@@ -2,11 +2,15 @@ import cv2
 
 cam  = cv2.VideoCapture(0)
 
-ret, frame = cam.read()
+ret, frame1 = cam.read()
+frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
 
 while True:
-    ret, frame = cam.read()
-    cv2.imshow("Captured", frame)
+    ret, frame2 = cam.read()
+    frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+    diff = cv2.absdiff(frame1, frame2)
+    cv2.imshow("Captured", diff)
+    frame1 = frame2.copy()
     if cv2.waitKey(1) == ord('q'):
         cv2.destroyWindow("Captured")
         break
