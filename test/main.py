6d486277eb44133fdba8e8a5352143c46382f010
vmalkin <vaughnmalkin@gmail.com>
2026-01-29 08:55:43 +1300

    test openCV

--- test/main.py
+++ test/main.py
@@ -1,15 +1,39 @@
 import cv2
+import numpy as np
+
+# Optical Resolution (True) 	1280 x 960 1.2MP
+# Image Capture (4:3 SD) 	320x240, 640x480 1.2 MP, 3.0 MP
+# Image Capture (16:9 W) 	360p, 480p, 720p
+# Video Capture (4:3 SD) 	320x240, 640x480, 800x600
+# Video Capture (16:9 W) 	360p, 480p, 720p,
+# Frame Rate (max) 	30fps @ 640x480
 
 cam  = cv2.VideoCapture(0)
-
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
 ret, frame1 = cam.read()
-frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+print(f'Image dimensions: {frame1.shape}')
+# new_image = np.zeros(frame1.shape, frame1.dtype)
 
 while True:
     ret, frame2 = cam.read()
-    frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
     diff = cv2.absdiff(frame1, frame2)
-    cv2.imshow("Captured", diff)
+    diff = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    # Adjust the brightness and contrast
+    # g(i,j)=α⋅f(i,j)+β
+    # control Contrast by 1.5
+    alpha = 1.5
+    # control brightness by 50
+    beta = 20
+    diff = cv2.convertScaleAbs(diff, alpha=alpha, beta=beta)
+
+    # # Create the sharpening kernel
+    # kernel = np.array([[0, -1, 0], [-1, 2, -1], [0, -1, 0]])
+    # # Sharpen the image
+    # diff = cv2.filter2D(diff, -1, kernel)
+
+    cv2.imshow("Cam", frame2)
+    cv2.imshow("Diff Image", diff)
     frame1 = frame2.copy()
     if cv2.waitKey(1) == ord('q'):
         cv2.destroyWindow("Captured")
