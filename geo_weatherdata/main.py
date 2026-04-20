b80f3f3293555d3ad9db06c1beef38c4def2cd0e
vmalkin <vaughnmalkin@gmail.com>
2026-04-20 20:20:26 +1200

    added database manager

--- geo_weatherdata/main.py
+++ geo_weatherdata/main.py
@@ -6,6 +6,36 @@ from collections import deque
 
 buffer_length = (24 * 60 * 60 * k.sensor_reading_frequency) + 100
 
+def add_data(collection, current_posixtime, csv_line):
+    # Will need to add a try except here. Just return the collection
+    # if there's a problem.
+    try:
+        seismodata = csv_line[0]
+        temperature = csv_line[1]
+        pressure = csv_line[2]
+        # Test for seismic data, temp and pressure as floats, otherwise do not use this data.
+        if test_isnumber(temperature):
+            if test_isnumber(pressure):
+                dp = [current_posixtime, seismodata, temperature, pressure]
+                collection.append(dp)
+    except:
+        print(f"Unable to parse data to add to collection: {csv_line}")
+    return collection
+
+
+def test_isnumber(numbertotest):
+    # Data is ONLY ever a float
+    if isinstance(numbertotest, float):
+        return True
+    # If it's a float cast as a string
+    if isinstance(numbertotest, str):
+        try:
+            float(numbertotest)
+            return True
+        except ValueError:
+            return False
+    return False
+
 
 def try_create_directory(directory):
     if os.path.isdir(directory) is False:
@@ -21,6 +51,10 @@ if __name__ == "__main__":
     # initial setup, create database, save folders.
     for key, value in k.dir_saves.items():
         try_create_directory(value)
+
+    if not os.path.isfile(k.sat_database):
+        print("No database file, initialising")
+        mgr_database.db_create()
 
     com = mgr_comport.SerialManager(k.comport, k.baudrate, k.bytesize, k.parity, k.stopbits, k.timeout,
                                     k.xonxoff,
---
+++ geo_weatherdata/mgr_database.py
@@ -0,0 +1,68 @@
+import sqlite3
+import constants as k
+
+
+def db_create():
+    # create database!
+    gpsdb = sqlite3.connect(k.sat_database)
+    db = gpsdb.cursor()
+
+    db.execute('drop table if exists observations;')
+
+    db.execute('create table observations ('
+               'posixtime real,'
+               'temperature real,'
+               'pressure real'
+               ');')
+
+    gpsdb.commit()
+    db.close()
+
+
+def db_data_add(gsvdata):
+    # this method expects an array with each element in the array being:
+    # [1737274820, '21.05', '99740.46'] (posixtime, temperature, pressure)
+    try:
+        gpsdb = sqlite3.connect(k.sat_database, timeout=10)
+        db = gpsdb.cursor()
+        for item in gsvdata:
+            posixtime = item[0]
+            seismodata = item[1]
+            temperature = item[2]
+            pressure = item[3]
+            values = [posixtime, seismodata, temperature, pressure]
+            db.execute('insert into observations(posixtime, temperature, pressure) '
+                       'values (?, ?, ?);', values)
+        gpsdb.commit()
+        db.close()
+    except sqlite3.OperationalError:
+        print(f'Database data insert FAILED - database locked')
+
+
+def db_data_get(timestart):
+    returnarray = []
+    values = [timestart]
+    try:
+        gpsdb = sqlite3.connect(k.sat_database, timeout=10)
+        db = gpsdb.cursor()
+        result = db.execute('select * from observations where posixtime > ?;', values)
+        for item in result:
+            returnarray.append(item)
+        db.close()
+    except sqlite3.OperationalError:
+        print(f'Database select query FAILED - database locked')
+    return returnarray
+
+
+def db_data_get_all():
+    returnarray = []
+    try:
+        gpsdb = sqlite3.connect(k.sat_database, timeout=10)
+        db = gpsdb.cursor()
+        result = db.execute('select * from observations;')
+        for item in result:
+            returnarray.append(item)
+        db.close()
+    except sqlite3.OperationalError:
+        print(f'Database get all FAILED - database locked')
+    return returnarray
