52b9c68eadfbd4dadfa8e8dc2318d7237db0ec5b
Vaughn Malkin <vaughnmalkin@gmail.com>
2026-04-22 11:02:56 +1200

    Added save data thread

--- geo_weatherdata/constants.py
+++ geo_weatherdata/constants.py
@@ -1,12 +1,12 @@
 # from numpy import nan
 # Comm port parameters - uncomment and change one of the portNames depending on your OS
-# comport = 'COM5' # Windows
+comport = 'COM5' # Windows
 # portName = '/dev/tty.usbserial-A702O0K9' #MacOS
 # portName = "/dev/cu.usbmodem1421"
 # comport = '/dev/ttyUSB0'
-comport = "/dev/ttyACM0"
+# comport = "/dev/ttyACM0"
 # comport = "/dev/ttyACM1"
-baudrate = 57600
+baudrate = 19200
 bytesize = 8
 parity = 'N'
 stopbits = 1
