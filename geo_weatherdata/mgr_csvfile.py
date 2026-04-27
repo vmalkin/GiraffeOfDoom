56405a33444680a942972672ef758cf868327955
vmalkin <vaughnmalkin@gmail.com>
2026-04-27 15:27:25 +1200

    added csv backup for yesterdays data

--- geo_weatherdata/mgr_csvfile.py
+++ geo_weatherdata/mgr_csvfile.py
@@ -1,8 +1,22 @@
-import constants as k
-import standard_stuff
-import gzip
-import mgr_database
+import os
 import time
+import constants as k
 
-def csv_save(current_savefile):
-   pass
+import mgr_database
+import standard_stuff
+
+def csv_save():
+    # Save data from the previous 48 to 24 hours. We will be backing up yesterday's data to CSV file. THis saves
+    # the nuisance of accounting for dual dates and serves the purpose of a backup well enough.
+    t = time.time()
+    utc_end_time = standard_stuff.posix2utc(t, '%Y-%m-%d')
+    psx_end_time = standard_stuff.utc2posix(utc_end_time)
+    psx_start_time = psx_end_time - 86400
+
+    csv_backup = f"{k.dir_saves['logs']}/{utc_end_time}.csv"
+    if not os.path.exists(csv_backup):
+        # get data from database
+        with open(csv_backup, 'w', newline='') as c:
+            pass
+    c.close()
+
