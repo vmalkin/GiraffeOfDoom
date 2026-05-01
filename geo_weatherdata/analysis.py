a65b7948b8f3ce16ffec1f108272763d2f5269b5
vmalkin <vaughnmalkin@gmail.com>
2026-05-01 12:41:04 +1200

    Setting up code for simple plot of single data value

--- geo_weatherdata/analysis.py
+++ geo_weatherdata/analysis.py
@@ -2,21 +2,27 @@ import mgr_database
 import standard_stuff
 import time
 import matplotlib.pyplot as plt
+from datetime import datetime, timezone
 import matplotlib.dates as mdates
 import numpy as np
 
 
 if __name__ == "__main__":
     end_time = time.time()
-    # print("Start time:", standard_stuff.posix2utc(1777442234.41846, timeformat='%Y-%m-%d %H:%M:%S'))
     start_time = end_time - 86400
-
     data = mgr_database.db_data_get(start_time, end_time)
     print(f"Data length: {len(data)}")
+
+    # process data, times for plotting.
+    plot_prs = []
+    plot_temp = []
+    plot_utc = []
     for psx, temp, prs in data:
-        print(f"{standard_stuff.posix2utc(psx, timeformat='%Y-%m-%d %H:%M:%S')},{temp},{prs}")
-        # print(f"{psx},{temp},{prs}")
+        plot_prs.append(prs)
+        plot_temp.append(temp)
+        tim = datetime.fromtimestamp(psx, tz=timezone.utc)  # datetime object
+        plot_utc.append(tim)
 
 
-def plot_multi(dateformatstring, dateobjects, singledataarray, tickinterval, plotcolour, plottitle, savefile):
-    pass
+def plot_singledata(dateformatstring, dateobjects, singledataarray, tickinterval, plotcolour, plottitle, savefile):
+    fig, ax = plt.subplots(layout="constrained", figsize=(8, 8), dpi=140)
