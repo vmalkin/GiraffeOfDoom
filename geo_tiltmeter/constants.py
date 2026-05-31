2eb1e3062047e4118a1f901e98266160b66ddbae
vmalkin <vaughnmalkin@gmail.com>
2026-05-31 13:37:23 +1200

    Set up for reading from camera, rather than COM port

--- geo_tiltmeter/constants.py
+++ geo_tiltmeter/constants.py
@@ -4,27 +4,27 @@
 # portName = '/dev/tty.usbserial-A702O0K9' #MacOS
 # portName = "/dev/cu.usbmodem1421"
 # comport = '/dev/ttyUSB0'
-comport = "/dev/ttyACM0"
+# comport = "/dev/ttyACM0"
 # comport = "/dev/ttyACM1"
-baudrate = 57600
-bytesize = 8
-parity = 'N'
-stopbits = 1
-timeout = 60
-xonxoff = False
-rtscts = True
-writeTimeout = None
-dsrdtr = False
-interCharTimeout = None
+# baudrate = 57600
+# bytesize = 8
+# parity = 'N'
+# stopbits = 1
+# timeout = 60
+# xonxoff = False
+# rtscts = True
+# writeTimeout = None
+# dsrdtr = False
+# interCharTimeout = None
 
 timeformat = '%Y-%m-%d %H:%M:%S'
-database = 'weather_data.db'
+database = 'tiltmeter.db'
 
-sensor = "BMP280"
+sensor = "Logitech_C270"
 # How many times a second the sensor reports data to the logger
-sensor_reading_frequency = 1
+sensor_reading_frequency = 30
 # Buffer length is for 30 mins plus 10%
-buffer_length = int((30 * 60 * sensor_reading_frequency) * 1.1)
+buffer_length = int((sensor_reading_frequency * 60 * 30) * 1.1)
 
 dir_saves = {
     'logs': 'logfiles'
