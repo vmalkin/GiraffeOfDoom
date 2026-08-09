053fad276a0159f422c3f8991fbd8320606a3f39
vmalkin <vaughnmalkin@gmail.com>
2026-08-09 20:19:22 +1200

    edit plot size

--- DunedinAurora/dnacore/chart_spark_goes_primary.py
+++ DunedinAurora/dnacore/chart_spark_goes_primary.py
@@ -49,7 +49,7 @@ def posix2utc(posixtime, timeformat):
 def plot(hours, data, colours):
     maxaxis = 8 * median_sigma + median_mean
     plt.style.use('dark_background')
-    fig, ax = plt.subplots(figsize=(3, 8), layout="constrained")
+    fig, ax = plt.subplots(figsize=(3, 9), layout="constrained")
     ax.barh(hours, data, align='center', color=colours)
     ax.yaxis.set_inverted(False)  # arrange data from top to bottom
     ax.set_xlabel('Sigma')
