4dfac57f7b88df72d0c216376373e27618bca743
vmalkin <vaughnmalkin@gmail.com>
2026-08-09 15:52:32 +1200

    removed unused files

--- dnacore05/test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-datasource = "https://services.swpc.noaa.gov/products/geospace/propagated-solar-wind-1-hour.json"
-webdata = requests.get(datasource, timeout=10).json()
-
-for i in range(1, len(webdata)):
-    print(webdata[i])
