9ecc703e3ab9c8567293e3fcdef22b55e70e12ca
Vaughn Malkin <vaughnmalkin@gmail.com>
2026-09-24 13:26:53 +1200

    set up for saving csv data from database

--- geo_tiltmeter/mgr_csvfile.py
+++ geo_tiltmeter/mgr_csvfile.py
@@ -6,6 +6,8 @@ import standard_stuff
 
 def csv_save(parseddata):
     pass
+
+
     # Save data from the previous 48 to 24 hours. We will be backing up yesterday's data to CSV file. THis saves
     # the nuisance of accounting for dual dates and serves the purpose of a backup well enough.
 
